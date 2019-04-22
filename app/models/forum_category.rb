8698bd4e398271592b765b2b8fe8969935b4d377
Kira <kira+git@e621.net>
2019-04-21 20:28:11 -0700

    Support for access locked forum categories

---
+++ app/models/forum_category.rb
@@ -0,0 +1,22 @@
+class ForumCategory < ApplicationRecord
+  has_many :forum_topics, -> { order(id: :desc) }
+  validates_uniqueness_of :name, case_sensitive: false
+
+  after_destroy :reassign_topics
+
+  def reassign_topics
+    ForumTopic.where(category: @cat.id).update_all(category_id: 0)
+  end
+
+  def can_create_within?(user = CurrentUser.user)
+    user.level >= can_create
+  end
+
+  def self.reverse_mapping
+    order(:cat_order).all.map { |rec| [rec.name, rec.id] }
+  end
+
+  def self.ordered_categories
+    order(:cat_order)
+  end
+end
