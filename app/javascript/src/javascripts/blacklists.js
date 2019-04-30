01418903aa6efa474d351091235fa5c8200d69d7
Kira <kira+git@e621.net>
2019-04-30 06:46:27 -0700

    Enable blacklist code to accept post data as objects
    This sets up support of using the blacklist code as a utility to
    support blacklisting avatars and dtext thumbnails before inserting
    them into the DOM.

--- app/javascript/src/javascripts/blacklists.js
+++ app/javascript/src/javascripts/blacklists.js
@@ -5,7 +5,7 @@ let Blacklist = {};
 
 Blacklist.entries = [];
 
-Blacklist.parse_entry = function(string) {
+Blacklist.parse_entry = function (string) {
   var entry = {
     "tags": string,
     "require": [],
@@ -16,7 +16,7 @@ Blacklist.parse_entry = function(string) {
     "min_score": null
   };
   var matches = string.match(/\S+/g) || [];
-  $.each(matches, function(i, tag) {
+  $.each(matches, function (i, tag) {
     if (tag.charAt(0) === '-') {
       entry.exclude.push(tag.slice(1));
     } else if (tag.charAt(0) === '~') {
@@ -31,19 +31,19 @@ Blacklist.parse_entry = function(string) {
   return entry;
 }
 
-Blacklist.parse_entries = function() {
+Blacklist.parse_entries = function () {
   var entries = (Utility.meta("blacklisted-tags") || "nozomiisthebestlovelive").replace(/(rating:[qes])\w+/ig, "$1").toLowerCase().split(/,/);
   entries = entries.filter(e => e.trim() !== "");
 
-  $.each(entries, function(i, tags) {
+  $.each(entries, function (i, tags) {
     var entry = Blacklist.parse_entry(tags);
     Blacklist.entries.push(entry);
   });
 }
 
-Blacklist.toggle_entry = function(e) {
+Blacklist.toggle_entry = function (e) {
   var tags = $(e.target).text();
-  var match = $.grep(Blacklist.entries, function(entry, i) {
+  var match = $.grep(Blacklist.entries, function (entry, i) {
     return entry.tags === tags;
   })[0];
   if (match) {
@@ -58,8 +58,8 @@ Blacklist.toggle_entry = function(e) {
   e.preventDefault();
 }
 
-Blacklist.update_sidebar = function() {
-  $.each(this.entries, function(i, entry) {
+Blacklist.update_sidebar = function () {
+  $.each(this.entries, function (i, entry) {
     if (entry.hits === 0) {
       return;
     }
@@ -84,7 +84,7 @@ Blacklist.update_sidebar = function() {
   $("#blacklist-box").show();
 }
 
-Blacklist.initialize_disable_all_blacklists = function() {
+Blacklist.initialize_disable_all_blacklists = function () {
   if (Cookie.get("dab") === "1") {
     $("#re-enable-all-blacklists").show();
     $("#blacklist-list a:not(.blacklisted-active)").click();
@@ -93,7 +93,7 @@ Blacklist.initialize_disable_all_blacklists = function() {
     $("#disable-all-blacklists").show()
   }
 
-  $("#disable-all-blacklists").on("click.danbooru", function(e) {
+  $("#disable-all-blacklists").on("click.danbooru", function (e) {
     $("#disable-all-blacklists").hide();
     $("#re-enable-all-blacklists").show();
     Cookie.put("dab", "1");
@@ -101,7 +101,7 @@ Blacklist.initialize_disable_all_blacklists = function() {
     e.preventDefault();
   });
 
-  $("#re-enable-all-blacklists").on("click.danbooru", function(e) {
+  $("#re-enable-all-blacklists").on("click.danbooru", function (e) {
     $("#disable-all-blacklists").show();
     $("#re-enable-all-blacklists").hide();
     Cookie.put("dab", "0");
@@ -110,16 +110,16 @@ Blacklist.initialize_disable_all_blacklists = function() {
   });
 }
 
-Blacklist.apply = function() {
-  $.each(this.entries, function(i, entry) {
+Blacklist.apply = function () {
+  $.each(this.entries, function (i, entry) {
     entry.hits = 0;
   });
 
   var count = 0
 
-  $.each(this.posts(), function(i, post) {
+  $.each(this.posts(), function (i, post) {
     var post_count = 0;
-    $.each(Blacklist.entries, function(j, entry) {
+    $.each(Blacklist.entries, function (j, entry) {
       if (Blacklist.post_match(post, entry)) {
         entry.hits += 1;
         count += 1;
@@ -136,29 +136,41 @@ Blacklist.apply = function() {
   return count;
 }
 
-Blacklist.posts = function() {
-  return $(".post-preview, #image-container, #c-comments .post, .mod-queue-preview.post-preview");
+Blacklist.posts = function () {
+  return $(".post-preview, #image-container, #c-comments .post, .mod-queue-preview.post-preview, .user-avatar");
 }
 
-Blacklist.post_match = function(post, entry) {
+Blacklist.post_match = function (post, entry) {
   if (entry.disabled) {
     return false;
   }
 
   var $post = $(post);
-  var score = parseInt($post.attr("data-score"));
-  var score_test = entry.min_score === null || score < entry.min_score;
+  let post_data = {
+    id: $post.data('id'),
+    score: parseInt($post.data('score'), 10),
+    tags: $post.data('tags'),
+    rating: $post.data('rating'),
+    uploader_id: $post.data('uploader-id'),
+    user: $post.data('uploader'),
+    flags: $post.data('flags')
+  };
+  return Blacklist.post_match_object(post_data, entry);
+};
 
-  var tags = String($post.attr("data-tags")).match(/\S+/g) || [];
-  tags = tags.concat(String($post.attr("data-pools")).match(/\S+/g) || []);
-  tags.push("rating:" + $post.data("rating"));
-  if ($post.attr("data-uploader")) {
-    tags.push("user:" + $post.attr("data-uploader").toLowerCase());
-  }
-  tags.push("uploaderid:" + $post.attr("data-uploader-id"));
-  tags.push("toptaggerid:" + $post.attr("data-top-tagger"));
-  $.each(String($post.data("flags")).match(/\S+/g) || [], function(i, v) {
-    tags.push("status:" + v);
+Blacklist.post_match_object = function (post, entry) {
+  if (entry.disabled)
+    return false;
+
+  const score_test = entry.min_score === null || post.score < entry.min_score;
+  const tags = post.tags.match(/\S+/g) || [];
+  tags.push(`id:${post.id}`);
+  tags.push(`rating:${post.rating}`);
+  tags.push(`uploaderid:${post.uploader_id}`);
+  tags.push(`height:${post.height}`);
+  tags.push(`width:${post.width}`);
+  $.each(post.flags.match(/\S+/g) || [], function (i, ev) {
+    tags.push(`status:${v}`);
   });
 
   return (Utility.is_subset(tags, entry.require) && score_test)
@@ -166,7 +178,7 @@ Blacklist.post_match = function(post, entry) {
     && !Utility.intersect(tags, entry.exclude).length;
 }
 
-Blacklist.post_hide = function(post) {
+Blacklist.post_hide = function (post) {
   var $post = $(post);
   $post.addClass("blacklisted").addClass("blacklisted-active");
 
@@ -177,7 +189,7 @@ Blacklist.post_hide = function(post) {
   }
 }
 
-Blacklist.post_unhide = function(post) {
+Blacklist.post_unhide = function (post) {
   var $post = $(post);
   $post.addClass("blacklisted").removeClass("blacklisted-active");
 
@@ -187,7 +199,7 @@ Blacklist.post_unhide = function(post) {
   }
 }
 
-Blacklist.initialize_all = function() {
+Blacklist.initialize_all = function () {
   Blacklist.parse_entries();
 
   if (Blacklist.apply() > 0) {
@@ -196,7 +208,7 @@ Blacklist.initialize_all = function() {
   }
 }
 
-$(document).ready(function() {
+$(document).ready(function () {
   if ($("#blacklist-box").length === 0) {
     return;
   }
