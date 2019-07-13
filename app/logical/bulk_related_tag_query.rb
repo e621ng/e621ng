61d113f85a05d258166d604812d6413c5fffeb10
Kira <kira+git@e621.net>
2019-07-12 19:31:11 -0700

    Fix new uploader problems based on review

--- app/logical/bulk_related_tag_query.rb
+++ app/logical/bulk_related_tag_query.rb
@@ -27,25 +27,24 @@ class BulkRelatedTagQuery
   protected
 
   def related_tags
-    @tags = Tag.where(name: @query)
-    @tags = @tags.inject({}) do |all, x|
-      related = x.related_tag_array
+    @related_tags ||= Tag.where(name: @query).each_with_object({}) do |tag, hash|
+      related = tag.related_tag_array
       categories = Tag.categories_for(related.map(&:first))
-      all[x.name] = related.map { |x| [x[0], x[1].to_i, categories.fetch(x[0], -1)] }
-      all
+
+      hash[tag.name] = related.map {|name, count| [name, count.to_i, categories.fetch(name, -1)]}
     end
-    @tags
   end
 
   def related_tags_by_category
-    cat = Tag.categories.value_for(category)
-    @tags = @query.inject({}) do |all, tag|
-      related = RelatedTagCalculator.calculate_from_sample_to_array(tag, cat)
-      categories = Tag.categories_for(related.map(&:first))
-      Rails.logger.warn("[cats] #{related.inspect}")
-      all[tag] = related.map { |y| [y[0], y[1].to_i, categories.fetch(y[0], -1)] }
-      all
+    @related_tags_by_category ||= begin
+      cat = Tag.categories.value_for(category)
+
+      @query.each_with_object({}) do |tag, hash|
+        related = RelatedTagCalculator.calculate_from_sample_to_array(tag, cat)
+        categories = Tag.categories_for(related.map(&:first))
+
+        hash[tag] = related.map {|name, count| [name, count.to_i, categories.fetch(name, -1)]}
+      end
     end
-    @tags
   end
 end
