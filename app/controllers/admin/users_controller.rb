655dd886714a74f5e700a29c38fcaad76b4ef214
Kira <kira+git@e621.net>
2019-08-23 15:05:46 -0700

    Allow admins to edit users blacklists

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -11,5 +11,16 @@ module Admin
       @user.promote_to!(params[:user][:level], params[:user])
       redirect_to edit_admin_user_path(@user), :notice => "User updated"
     end
+
+    def edit_blacklist
+      @user = User.find(params[:id])
+    end
+
+    def update_blacklist
+      @user = User.find(params[:id])
+      @user.update_attributes!(params[:user].permit([:blacklisted_tags]))
+      ModAction.log(:user_blacklist_changed, {user_id: @user.id})
+      redirect_to edit_blacklist_admin_user_path(@user)
+    end
   end
 end
