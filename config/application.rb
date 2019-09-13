9c7b7a23d62aaf281bd235d379b87580ad73cdec
Kira <kira+git@e621.net>
2019-09-13 10:20:07 -0700

    Use rails 6.0 defaults

--- config/application.rb
+++ config/application.rb
@@ -11,10 +11,10 @@ require 'elasticsearch/rails/instrumentation'
 module Danbooru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.2
+    config.load_defaults '6.0'
     config.active_record.schema_format = :sql
     config.encoding = "utf-8"
-    config.filter_parameters += [:password]
+    config.filter_parameters += [:password, :password_hash, :api_key]
     config.assets.enabled = true
     config.assets.version = '1.0'
     config.autoload_paths += %W(#{config.root}/app/presenters #{config.root}/app/logical #{config.root}/app/mailers #{config.root}/app/indexes)
