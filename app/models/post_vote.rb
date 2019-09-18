bc43b857df1ad76a37a7c7869b201ec4c6393209
Kira <kira+git@e621.net>
2019-09-18 12:36:05 -0700

    Fix post voting, again
    More creator vs user nonsense.

--- app/models/post_vote.rb
+++ app/models/post_vote.rb
@@ -29,13 +29,13 @@ class PostVote < ApplicationRecord
   end
 
   def validate_user_can_vote
-    if creator.younger_than(3.days) && score == -1
-      errors.add(:creator, "must be 3 days old to downvote posts")
+    if user.younger_than(3.days) && score == -1
+      errors.add(:user, "must be 3 days old to downvote posts")
       return false
     end
-    allowed = creator.can_post_vote_with_reason
+    allowed = user.can_post_vote_with_reason
     if allowed != true
-      errors.add(:creator, User.throttle_reason(allowed))
+      errors.add(:user, User.throttle_reason(allowed))
       return false
     end
     true
