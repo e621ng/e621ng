d15acddca10590c1bc7780518ca02661536e9675
Kira <kira+git@e621.net>
2019-09-25 15:56:17 -0700

    Remove broken sitemap.xml

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -1,7 +1,7 @@
 class StaticController < ApplicationController
   def terms_of_service
   end
-  
+
   def accept_terms_of_service
     cookies.permanent[:accepted_tos] = "1"
     url = params[:url] if params[:url] && params[:url].start_with?("/")
@@ -17,11 +17,4 @@ class StaticController < ApplicationController
 
   def site_map
   end
-
-  def sitemap
-    @popular_search_service = PopularSearchService.new(Date.yesterday)
-    @post_set = PostSets::Popular.new(Date.yesterday.to_s, "week", limit: 200)
-    @posts = @post_set.posts
-    render layout: false
-  end  
 end
