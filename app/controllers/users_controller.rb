a186635420828ee162f2eb3621c98144918bd64b
Kira <kira+git@e621.net>
2019-09-27 21:16:35 -0700

    Allow editing per page for all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -115,7 +115,6 @@ class UsersController < ApplicationController
     permitted_params += [dmail_filter_attributes: %i[id words]]
     permitted_params += [:profile_about, :profile_artinfo, :email, :avatar_id] if CurrentUser.is_member? # Prevent editing when blocked
     permitted_params += [:name, :email] if context == :create
-    permitted_params << :level if CurrentUser.is_admin?
 
     params.require(:user).permit(permitted_params)
   end
