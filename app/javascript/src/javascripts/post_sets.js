b8a325941c73d1cffd767aaba4ac24eb08f1bc53
Kira <kira+git@e621.net>
2019-11-08 12:15:14 -0800

    Add 'add to set' option to posts page

--- app/javascript/src/javascripts/post_sets.js
+++ app/javascript/src/javascripts/post_sets.js
@@ -1,5 +1,7 @@
 import {SendQueue} from './send_queue'
 import Post from './posts.js.erb'
+import Pool from "./pools";
+import PostModeMenu from "./post_mode_menu";
 
 let PostSet = {};
 
@@ -38,5 +40,51 @@ PostSet.remove_post = function (set_id, post_id) {
     });
   });
 };
+
+PostSet.initialize_add_to_set_link = function() {
+  $("#add-to-set-dialog").dialog({autoOpen: false});
+
+  $("#set").on("click.danbooru", function(e) {
+    e.preventDefault();
+    PostSet.update_sets_menu();
+    $("#add-to-set-dialog").dialog("open");
+  });
+
+  $("#add-to-set-submit").on("click", function(e) {
+    e.preventDefault();
+    const post_id = $('#image-container').data('id');
+    PostSet.add_post($("#add-to-set-id").val(), post_id);
+    $('#add-to-set-dialog').dialog('close');
+  });
+};
+
+PostSet.update_sets_menu = function() {
+  const target = $('#add-to-set-id');
+  target.empty();
+  target.append($('<option>').text('Loading...'));
+  SendQueue.add(function() {
+    $.ajax({
+      type: "GET",
+      url: "/post_sets/for_select.json",
+    }).fail(function(data) {
+      $(window).trigger('danbooru:error', "Error getting sets list: " + data.message);
+    }).done(function(data) {
+      target.empty();
+      ['Owned', "Maintained"].forEach(function(v) {
+        let group = $('<optgroup>', {label: v});
+        data[v].forEach(function(gi) {
+          group.append($('<option>', {value: gi[1]}).text(gi[0]));
+        });
+        target.append(group);
+      });
+    });
+  });
+};
+
+$(function() {
+  if ($("#c-posts").length && $('#a-show').length) {
+    PostSet.initialize_add_to_set_link();
+  }
+});
 
 export default PostSet;
