b22a30b8e534a1cecda5d967afb0aeac08fcbebc
Kira <kira+git@e621.net>
2019-11-09 10:53:02 -0800

    Limit size of lists in admin dashboard

--- app/logical/admin_dashboard.rb
+++ app/logical/admin_dashboard.rb
@@ -1,10 +1,10 @@
 class AdminDashboard
   def tag_aliases
-    TagAlias.where(status: "pending").order("id desc")
+    TagAlias.where(status: "pending").order("id desc").limit(100)
   end
 
   def tag_implications
-    TagImplication.where(status: "pending").order("id desc")
+    TagImplication.where(status: "pending").order("id desc").limit(100)
   end
 
   def update_requests
