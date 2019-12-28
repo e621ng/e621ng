803e38f5cb42598d42658272ea43b22a188d393c
Kira <kira+git@e621.net>
2019-12-28 04:33:09 -0800

    Update gems
    And remove code that uses session.id

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,8 +1,4 @@
 module PostsHelper
-  def post_search_counts_enabled?
-    Danbooru.config.enable_post_search_counts && Danbooru.config.reportbooru_server.present? && Danbooru.config.reportbooru_key.present?
-  end
-
   def discover_mode?
     params[:tags] =~ /order:rank/
   end
@@ -19,43 +15,6 @@ module PostsHelper
     else
       nil
     end
-  end
-
-  def missed_post_search_count_js
-    return nil unless post_search_counts_enabled?
-
-    if params[:ms] == "1" && @post_set.post_count == 0 && @post_set.is_single_tag?
-      session_id = session.id
-      verifier = ActiveSupport::MessageVerifier.new(Danbooru.config.reportbooru_key, serializer: JSON, digest: "SHA256")
-      sig = verifier.generate("#{params[:tags]},#{session_id}")
-      return render("posts/partials/index/missed_search_count", sig: sig)
-    end
-  end
-
-  def post_search_count_js
-    return nil unless post_search_counts_enabled?
-
-    if action_name == "index" && params[:page].nil?
-      tags = Tag.scan_query(params[:tags]).sort.join(" ")
-
-      if tags.present?
-        key = "ps-#{tags}"
-        value = session.id
-        verifier = ActiveSupport::MessageVerifier.new(Danbooru.config.reportbooru_key, serializer: JSON, digest: "SHA256")
-        sig = verifier.generate("#{key},#{value}")
-        return render("posts/partials/index/search_count", sig: sig)
-      end
-    end
-
-    return nil
-  end
-
-  def post_view_count_js
-    return nil unless post_search_counts_enabled?
-
-    msg = "#{params[:id]},#{session.id}"
-    msg = ActiveSupport::MessageVerifier.new(Danbooru.config.reportbooru_key, serializer: JSON, digest: "SHA256").generate(msg)
-    return render("posts/partials/show/view_count", msg: msg)
   end
 
   def post_source_tag(source)
