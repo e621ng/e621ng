a1e21060c2e71a240d50f965d93db101420e3fb9
Kira <kira+git@e621.net>
2019-12-28 06:37:03 -0800

    Remove confusing tag count background style
    Users and staff keep confusing this for a bug and nobody
    understands what it is for or why it's there, even after being
    told what it is. So remove it and save code time and headache
    from everyone.

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -37,10 +37,4 @@ module TagsHelper
 
     html.html_safe
   end
-
-  def tag_post_count_style(tag)
-    @highest_post_count ||= Tag.highest_post_count
-    width_percent = Math.log([tag.post_count, 1].max, @highest_post_count) * 100
-    "background: linear-gradient(to left, rgba(0, 0, 0, 0.0) #{width_percent}%, rgba(0, 0, 0, 0.35) #{width_percent}%)"
-  end
 end
