8e117f4a2fbae21877c7e9716f75a55cf6183fac
Kira <kira+git@e621.net>
2020-01-16 10:12:33 -0800

    Fix post set migration

---
+++ script/e6_migration/000_drop_unused_tables.sql
@@ -0,0 +1,4 @@
+DROP TABLE IF EXISTS lol, restricted, table_data, servers, server_keys, stats, user_blacklisted_tags_backup, user_metadata;
+DROP TABLE IF EXISTS posts_tags, locked_post_tags;
+DROP TABLE IF EXISTS mod_menu, mod_queue_posts;
+DROP TABLE IF EXISTS tag_subscriptions;
