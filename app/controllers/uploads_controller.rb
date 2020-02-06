6c63d4bd83c8e0657485e17b61753ddaad258ec8
Kira <kira+git@e621.net>
2020-02-06 15:14:59 -0800

    Uploader and post_version fixes

--- app/controllers/uploads_controller.rb
+++ app/controllers/uploads_controller.rb
@@ -1,7 +1,10 @@
 class UploadsController < ApplicationController
   before_action :member_only
   before_action :janitor_only, only: [:index, :show]
-  respond_to :html, :json, :js
+  respond_to :html, :json
+  content_security_policy do |p|
+    p.img_src :self, :data, "*"
+  end
 
   def new
     if CurrentUser.can_upload_with_reason == :REJ_UPLOAD_NEWBIE
