8e355b6b89faad847c44d55c6b6377d71859bfad
Sasquire <onemillian@protonmail.com>
2020-03-04 17:00:04 -0500

    Fix issues with seeder script failing Fixes the problems described in issue 188

--- db/seeds.rb
+++ db/seeds.rb
@@ -18,6 +18,7 @@ puts "== Seeding database with sample content ==\n"
 admin = User.find_or_create_by!(name: "admin") do |user|
   user.created_at = 2.weeks.ago
   user.password = "e621test"
+  user.password_confirmation = "e621test"
   user.password_hash = ""
   user.email = "admin@e621.net"
   user.can_upload_free = true
@@ -25,8 +26,9 @@ admin = User.find_or_create_by!(name: "admin") do |user|
 end
 
 User.find_or_create_by!(name: Danbooru.config.system_user) do |user|
-  user.password_hash = ""
   user.password = "ae3n4oie2n3oi4en23oie4noienaorshtaioresnt"
+  user.password_confirmation = "ae3n4oie2n3oi4en23oie4noienaorshtaioresnt"
+  user.password_hash = ""
   user.email = "system@e621.net"
   user.can_upload_free = true
   user.level = User::Levels::ADMIN
