33524e19d5ecc372ef54ba98cb23ed3fda33d6b9
Kira <kira+git@e621.net>
2020-03-06 11:00:07 -0800

    Cookie update

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,4 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_danbooru_session'
+Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
