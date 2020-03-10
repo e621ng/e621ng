fc0cc8e9035eaed30acdfbaeb48cd89ad3a8464c
Kira <kira+git@e621.net>
2020-03-10 12:16:19 -0700

    Fix delete with given reason link

--- app/javascript/src/javascripts/mod_queue.js
+++ app/javascript/src/javascripts/mod_queue.js
@@ -27,11 +27,12 @@ $(function() {
   $(window).on("danbooru:modqueue_increment_processed", ModQueue.increment_processed);
   $(document).on("click.danbooru", ".quick-mod .detailed-rejection-link", ModQueue.detailed_rejection_dialog);
   $(".delete-with-reason-link").on('click', function(e) {
+    e.preventDefault();
     const post_id = $(e.target).attr('data-post-id');
     const prompt = $(e.target).data('prompt');
     const reason = $(e.target).data('reason');
 
-    if (confirm(`Delete post for being ${prompt}?`))
+    if (confirm(`Delete post for ${prompt}?`))
       Post.delete_with_reason(post_id, reason, true);
   });
 });
