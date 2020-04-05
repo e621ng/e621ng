5f68126bdb94c4dd4487e3a53ca330b03700e70f
Kira <kira+git@e621.net>
2020-04-05 08:13:51 -0700

    Deleted Posts: Try another fetch method that should be faster

--- app/controllers/deleted_posts_controller.rb
+++ app/controllers/deleted_posts_controller.rb
@@ -1,14 +1,20 @@
+require_relative '../logical/danbooru/paginator/elasticsearch_extensions'
 class DeletedPostsController < ApplicationController
   before_action :member_only
   respond_to :html
 
   def index
-    @posts = Post.where(is_deleted: true)
     if params[:user_id].present?
       @user = User.find(params[:user_id])
+      @posts = Post.where(is_deleted: true)
       @posts = @posts.where('posts.uploader_id = ?', @user.id)
+      @posts = @posts.includes(:uploader).includes(:flags).where('post_flags.id IS NOT NULL').order(Arel.sql('post_flags.created_at DESC')).paginate(params[:page])
+    else
+      @posts = PostFlag.where(is_deletion: true).includes(post: [:uploader, :flags]).order(id: :desc).paginate(params[:page])
+      new_opts = {mode: :numbered, per_page: @posts.records_per_page, total: @posts.total_count, current_page: params[:page] || 1}
+      @posts = ::Danbooru::Paginator::PaginatedArray.new(@posts.map {|f| f.post},
+                                                new_opts
+      )
     end
-
-    @posts = @posts.includes(:uploader).includes(:flags).where('post_flags.id IS NOT NULL').order(Arel.sql('post_flags.created_at DESC')).paginate(params[:page])
   end
 end
