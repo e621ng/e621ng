2cbaf803ec7375422605695ee55495a3a8b6ad75
Kira <kira+git@e621.net>
2020-04-16 17:46:22 -0700

    [Login] Make login reminder more vague in response

--- app/controllers/maintenance/user/login_reminders_controller.rb
+++ app/controllers/maintenance/user/login_reminders_controller.rb
@@ -8,10 +8,9 @@ module Maintenance
         @user = ::User.with_email(params[:user][:email]).first
         if @user
           LoginReminderMailer.notice(@user).deliver_now
-          flash[:notice] = "Email sent"
-        else
-          flash[:notice] = "Email address not found"
         end
+
+        flash[:notice] = "If your email was on file, an email has been sent your way. It should arrive within the next few minutes. Make sure to check your spam folder."
 
         redirect_to new_maintenance_user_login_reminder_path
       end
