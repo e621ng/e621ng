581881e58cb90f4aaa2ebbf4c33b05d124133380
Kira <kira+git@e621.net>
2020-04-24 23:48:21 -0700

    Remove reference to custom sorting support

--- app/logical/post_query_builder.rb
+++ app/logical/post_query_builder.rb
@@ -567,11 +567,6 @@ class PostQueryBuilder
     when "rank"
       relation = relation.order(Arel.sql("log(3, posts.score) + (extract(epoch from posts.created_at) - extract(epoch from timestamp '2005-05-24')) / 35000 DESC"))
 
-    when "custom"
-      if q[:post_id].present? && q[:post_id][0] == :in
-        relation = relation.find_ordered(q[:post_id][1])
-      end
-
     else
       relation = relation.order("posts.id DESC")
     end
