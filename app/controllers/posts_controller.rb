929d87c07086237b0e9042028459157bc21cf522
Kira <kira+git@e621.net>
2020-07-04 18:16:08 -0700

    [Posts] Allow priv to edit rating locks

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -172,7 +172,8 @@ private
       edit_reason
       has_embedded_notes
     ]
-    permitted_params += %i[is_rating_locked is_note_locked bg_color] if CurrentUser.is_janitor?
+    permitted_params += %i[is_rating_locked] if CurrentUser.is_privileged?
+    permitted_params += %i[is_note_locked bg_color] if CurrentUser.is_janitor?
     permitted_params += %i[is_status_locked locked_tags hide_from_anonymous hide_from_search_engines] if CurrentUser.is_admin?
 
     params.require(:post).permit(permitted_params)
