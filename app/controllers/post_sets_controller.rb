656719407ca663d06c85bf1f1160dd4877535db1
Kira <kira+git@e621.net>
2020-07-11 18:44:58 -0700

    [Sets] Order the results of sets by name

--- app/controllers/post_sets_controller.rb
+++ app/controllers/post_sets_controller.rb
@@ -108,8 +108,8 @@ class PostSetsController < ApplicationController
   end
 
   def for_select
-    owned = PostSet.owned(CurrentUser.user)
-    maintained = PostSet.active_maintainer(CurrentUser.user)
+    owned = PostSet.owned(CurrentUser.user).order(:name)
+    maintained = PostSet.active_maintainer(CurrentUser.user).order(:name)
 
     @for_select = {
         "Owned" => owned.map {|x| [x.name.tr("_", " ").truncate(35), x.id]},
