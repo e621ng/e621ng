ddf53378250ead24d94b5856aee13774021d2d0e
Kira <kira+git@e621.net>
2020-09-29 12:11:31 -0700

    Revert "[Flags] Fix flagging order and display"
    This reverts commit ef3b042357608f785bb78ece97b96567c3beddb3.
    
    This broken post deletions, somehow.

--- app/models/post.rb
+++ app/models/post.rb
@@ -49,7 +49,7 @@ class Post < ApplicationRecord
   belongs_to :parent, class_name: "Post", optional: true
   has_one :upload, :dependent => :destroy
   has_one :pixiv_ugoira_frame_data, :class_name => "PixivUgoiraFrameData", :dependent => :destroy
-  has_many :flags, -> {order('post_flags.id DESC')}, :class_name => "PostFlag", :dependent => :destroy
+  has_many :flags, :class_name => "PostFlag", :dependent => :destroy
   has_many :appeals, :class_name => "PostAppeal", :dependent => :destroy
   has_many :votes, :class_name => "PostVote", :dependent => :destroy
   has_many :notes, :dependent => :destroy
