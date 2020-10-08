37f10ea59dc18006eb3be756626d7a691d461c24
Kira <kira+git@e621.net>
2020-10-07 17:03:29 -0700

    [Error] Fix incorrect wording in throttle error template

--- app/models/user.rb
+++ app/models/user.rb
@@ -449,7 +449,7 @@ class User < ApplicationRecord
     def throttle_reason(reason)
       reasons = {
           REJ_NEWBIE: 'can not yet perform this action. User not old enough',
-          REJ_LIMITED: 'has reached the hourly limit for this action'
+          REJ_LIMITED: 'have reached the hourly limit for this action'
       }
       reasons.fetch(reason, 'unknown throttle reason, please report this as a bug')
     end
