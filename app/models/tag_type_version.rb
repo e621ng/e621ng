df6fb1bead3c603486047ac472a090e9d5239644
Earlopain <earlopain@protonmail.com>
2021-06-22 20:48:57 +0200

    [TagTypeVersions] Fix search by username
    Also use standard time formatting + numbered paginator

--- app/models/tag_type_version.rb
+++ app/models/tag_type_version.rb
@@ -8,20 +8,19 @@ class TagTypeVersion < ApplicationRecord
 
       if params[:tag].present?
         tag = Tag.find_by_normalized_name(params[:tag])
-        q = q.where(tag_id: tag.id) if tag
+        q = q.where(tag: tag)
       end
 
       if params[:user_id].present?
-        q = q.where('creator_id = ?', params[:user_id])
+        user = User.find_by_id(params[:user_id])
+        q = q.where(creator: user)
       end
       if params[:user_name].present?
-        name = User.find_by_name(params[:user_name])
-        q = q.where('creator_id = ? ', name.id) if id
+        user = User.find_by_name(params[:user_name])
+        q = q.where(creator: user)
       end
 
-      q = q.order(id: :desc)
-
-      q
+      q.order(id: :desc)
     end
   end
 
