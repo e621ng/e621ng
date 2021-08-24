d8b00bf94b5015a44d769a49effb90b0746817ec
Earlopain <earlopain@protonmail.com>
2021-08-24 19:22:01 +0200

    [Rails] Allow easy disable of the cache_store
    Setting it in the individual environment config does not work because it
    was overwritten by the cache_store.rb initializer

--- config/environments/staging.rb
+++ config/environments/staging.rb
@@ -54,9 +54,6 @@ Rails.application.configure do
   # Use a different logger for distributed setups.
   # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
 
-  # Use a different cache store in production.
-  # config.cache_store = :mem_cache_store
-
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
--- config/initializers/cache_store.rb
+++ config/initializers/cache_store.rb
@@ -1,41 +1,17 @@
-# Rails.application.configure do
-#   if Rails.env.test? || Danbooru.
-#     cache_config = [:memory_store, { size: 32.megabytes }]
-#   else
-#     cache_config = [
-#         :redis_cache_store,
-#         {
-#             url: Danbooru.config.redis_url,
-#             namespace: Danbooru.config.safe_app_name,
-#             connect_timeout:   30, # default: 20 seconds
-#             write_timeout:    0.2, # default: 1 second
-#             read_timeout:     0.2, # default: 1 second
-#             reconnect_attempts: 0, # default: 0
-#             error_handler: ->(method:, returning:, exception:) {
-#               DanbooruLogger.log(exception, method: method, returning: returning)
-#             }
-#         }
-#     ]
-#   end
-#
-#   cache_store = ActiveSupport::Cache.lookup_store(cache_config)
-#
-#   config.cache_store = cache_store
-#   config.action_controller.cache_store = cache_store
-#   Rails.cache = cache_store
-# end
-
+def get_cache_store
+  if Rails.env.test?
+    [:memory_store, { size: 32.megabytes }]
+  elsif Danbooru.config.disable_cache_store
+    :null_store
+  else
+    [:mem_cache_store, Danbooru.config.memcached_servers, { namespace: Danbooru.config.safe_app_name }]
+  end
+end
 
 Rails.application.configure do
   begin
-    if Rails.env.test?
-      config.cache_store = :memory_store, { size: 32.megabytes }
-      config.action_controller.cache_store = :memory_store, { size: 32.megabytes }
-      Rails.cache = ActiveSupport::Cache.lookup_store(Rails.application.config.cache_store)
-    else
-      config.cache_store = :mem_cache_store, Danbooru.config.memcached_servers, { namespace: Danbooru.config.safe_app_name }
-      config.action_controller.cache_store = :mem_cache_store, Danbooru.config.memcached_servers, { namespace: Danbooru.config.safe_app_name }
-      Rails.cache = ActiveSupport::Cache.lookup_store(Rails.application.config.cache_store)
-    end
+    config.cache_store = get_cache_store
+    config.action_controller.cache_store = get_cache_store
+    Rails.cache = ActiveSupport::Cache.lookup_store(Rails.application.config.cache_store)
   end
 end
