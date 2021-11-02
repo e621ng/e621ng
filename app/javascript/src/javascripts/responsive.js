d348a0f303a8a63f6a2eea9d2c148028ca0d42aa
Earlopain <earlopain@protonmail.com>
2021-11-02 14:19:46 +0100

    [Mobile] Prevent default on navbar click

--- app/javascript/src/javascripts/responsive.js
+++ app/javascript/src/javascripts/responsive.js
@@ -1,5 +1,6 @@
 $(function() {
-  $("#maintoggle").on("click.danbooru", function() {
+  $("#maintoggle").on("click.danbooru", function(e) {
+    e.preventDefault();
     $('#nav').toggle();
     $('#maintoggle-on').toggle();
     $('#maintoggle-off').toggle();
