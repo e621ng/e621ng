f1eaf58aa0d2fdc5c9136236d9b7501720866e75
Earlopain <earlopain@protonmail.com>
2021-11-22 20:20:28 +0100

    [Tests] Fix post flag tests

--- test/unit/post_flag_test.rb
+++ test/unit/post_flag_test.rb
@@ -4,6 +4,7 @@ class PostFlagTest < ActiveSupport::TestCase
   context "In all cases" do
     setup do
       travel_to(2.weeks.ago) do
+        @bob = create(:user)
         @alice = create(:privileged_user)
       end
       as(@alice) do
@@ -11,137 +12,71 @@ class PostFlagTest < ActiveSupport::TestCase
       end
     end
 
-    context "a basic user" do
-      setup do
-        travel_to(2.weeks.ago) do
-          @bob = create(:user)
-        end
-      end
+    should "respect the throttle limit" do
+      as(@bob) do
+        Danbooru.config.stubs(:disable_throttles?).returns(false)
+        Danbooru.config.stubs(:post_flag_limit).returns(0)
 
-      should "not be able to flag more than 1 post in 24 hours" do
-        @post_flag = PostFlag.new(post: @post, reason: "aaa", is_resolved: false)
-        @post_flag.expects(:flag_count_for_creator).returns(1)
-        assert_difference("PostFlag.count", 0) do
-          as(@bob) { @post_flag.save }
+        error = assert_raises(ActiveRecord::RecordInvalid) do
+          @post_flag = create(:post_flag, post: @post)
         end
-        assert_equal(["You can flag 1 post a day"], @post_flag.errors.full_messages)
+        assert_match(/You have reached the hourly limit for this action/, error.message)
       end
     end
 
-    context "a gold user" do
-      setup do
-        travel_to(2.weeks.ago) do
-          @bob = create(:privileged_user)
-        end
+    should "not be able to flag a deleted post" do
+      as(@alice) do
+        @post.update(is_deleted: true)
       end
 
-      should "not be able to flag a post more than twice" do
-        assert_difference(-> { PostFlag.count }, 1) do
-          as(@bob) do
-            @post_flag = PostFlag.create(post: @post, reason: "aaa", is_resolved: false)
-          end
-        end
-
-        assert_difference(-> { PostFlag.count }, 0) do
-          as(@bob) do
-            @post_flag = PostFlag.create(post: @post, reason: "aaa", is_resolved: false)
-          end
-        end
-
-        assert_equal(["have already flagged this post"], @post_flag.errors[:creator_id])
-      end
-
-      should "not be able to target a single uploader" do
-        travel_to(2.weeks.ago) do
-          as(@alice) do
-            @posts = FactoryBot.create_list(:post, 10, uploader: @alice)
-          end
-        end
-
+      error = assert_raises(ActiveRecord::RecordInvalid) do
         as(@bob) do
-          travel_to(1.week.ago) do
-            @flags = @posts.map {|x| PostFlag.create(reason: "bad #{x.id}", post: x)}
+          @post_flag = create(:post_flag, post: @post)
+        end
+      end
+      assert_match(/Post is deleted/, error.message)
+    end
+
+    should "not be able to flag a post in the cooldown period" do
+      @mod = create(:moderator_user)
+
+      travel_to(2.weeks.ago) do
+        @users = FactoryBot.create_list(:user, 2)
+      end
+
+      as(@users.first) do
+        @flag1 = create(:post_flag, post: @post)
+        puts @flag1.errors.full_messages
+      end
+
+      as(@mod) do
+        @post.approve!
+      end
+
+      travel_to(PostFlag::COOLDOWN_PERIOD.from_now - 1.minute) do
+        as(@users.second) do
+
+          error = assert_raises(ActiveRecord::RecordInvalid) do
+            @flag2 = create(:post_flag, post: @post)
           end
-
-          @bad_flag = PostFlag.create(post: @post, reason: "bad #{@post.id}")
-        end
-
-        assert_equal(["You cannot flag posts uploaded by this user"], @bad_flag.errors.full_messages)
-      end
-
-      should "not be able to flag more than 10 posts in 24 hours" do
-        as(@bob) do
-          @post_flag = PostFlag.new(post: @post, reason: "aaa", is_resolved: false)
-          @post_flag.expects(:flag_count_for_creator).returns(10)
-
-          assert_difference(-> { PostFlag.count }, 0) do
-            @post_flag.save
-          end
-        end
-        assert_equal(["You can flag 10 posts a day"], @post_flag.errors.full_messages)
-      end
-
-      should "not be able to flag a deleted post" do
-        as(@alice) do
-          @post.update(is_deleted: true)
-        end
-
-        assert_difference(-> { PostFlag.count }, 0) do
-          as(@bob) do
-            @post_flag = PostFlag.create(post: @post, reason: "aaa", is_resolved: false)
-          end
-        end
-        assert_equal(["Post is deleted"], @post_flag.errors.full_messages)
-      end
-
-      should "not be able to flag a pending post" do
-        as(@alice) do
-          @post.update(is_pending: true)
-        end
-        as(@bob) do
-          @flag = @post.flags.create(reason: "test")
-        end
-
-        assert_equal(["Post is pending and cannot be flagged"], @flag.errors.full_messages)
-      end
-
-      should "not be able to flag a post in the cooldown period" do
-        @mod = create(:moderator_user)
-
-        travel_to(2.weeks.ago) do
-          @users = FactoryBot.create_list(:user, 2)
-        end
-
-        as(@users.first) do
-          @flag1 = PostFlag.create(post: @post, reason: "something")
-        end
-
-        as(@mod) do
-          @post.approve!
-        end
-
-        travel_to(PostFlag::COOLDOWN_PERIOD.from_now - 1.minute) do
-          as(@users.second) do
-            @flag2 = PostFlag.create(post: @post, reason: "something")
-          end
-          assert_match(/cannot be flagged more than once/, @flag2.errors[:post].join)
-        end
-
-        travel_to(PostFlag::COOLDOWN_PERIOD.from_now + 1.minute) do
-          as(@users.second) do
-            @flag3 = PostFlag.create(post: @post, reason: "something")
-          end
-          assert(@flag3.errors.empty?)
+          assert_match(/cannot be flagged more than once/, error.message)
         end
       end
 
-      should "initialize its creator" do
-        @post_flag = as(@alice) do
-          PostFlag.create(:post => @post, :reason => "aaa", :is_resolved => false)
+      travel_to(PostFlag::COOLDOWN_PERIOD.from_now + 1.minute) do
+        as(@users.second) do
+          @flag3 = create(:post_flag, post: @post)
         end
-        assert_equal(@alice.id, @post_flag.creator_id)
-        assert_equal(IPAddr.new("127.0.0.1"), @post_flag.creator_ip_addr)
+        assert(@flag3.errors.empty?)
       end
+    end
+
+    should "initialize its creator" do
+      @post_flag = as(@alice) do
+        create(:post_flag, post: @post)
+      end
+      assert_equal(@alice.id, @post_flag.creator_id)
+      assert_equal(IPAddr.new("127.0.0.1"), @post_flag.creator_ip_addr)
     end
 
     context "a user with no_flag=true" do
@@ -151,13 +86,11 @@ class PostFlagTest < ActiveSupport::TestCase
         end
       end
 
-      should "not be able to flag more than 1 post in 24 hours" do
-        @post_flag = PostFlag.new(post: @post, reason: "aaa", is_resolved: false)
-        @post_flag.expects(:flag_count_for_creator).returns(1)
-        assert_difference("PostFlag.count", 0) do
-          as(@bob) { @post_flag.save }
+      should "not be able to flag" do
+        error = assert_raises(ActiveRecord::RecordInvalid) do
+          as(@bob) { create(:post_flag, post: @post) }
         end
-        assert_equal(["You cannot flag posts"], @post_flag.errors.full_messages.grep(/cannot flag posts/))
+        assert_match(/You cannot flag posts/, error.message)
       end
     end
   end
