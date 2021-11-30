09a04a4d18b0424c2989ffc6562069c2230134b3
Zwagoth <zwagoth@users.noreply.github.com>
2021-11-29 20:53:50 -0500

    Update tag_implication.rb

--- app/models/tag_implication.rb
+++ app/models/tag_implication.rb
@@ -8,7 +8,7 @@ class TagImplication < TagRelationship
   before_save :update_descendant_names
   after_save :update_descendant_names_for_parents
   after_destroy :update_descendant_names_for_parents
-  after_save :create_mod_action
+  after_save :create_mod_action, if: :status_changed?
   validates :antecedent_name, uniqueness: { scope: [:consequent_name], conditions: -> { duplicate_relevant } }
   validate :absence_of_circular_relation
   validate :absence_of_transitive_relation
