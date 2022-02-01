2f8b733f92b41f7ad5244d190354ccfcbb053b47
Earlopain <earlopain@protonmail.com>
2022-02-01 12:41:17 +0100

    [Users] Better wording for replacements regarding upload limit

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -52,7 +52,7 @@ class UserPresenter
 
     %{<abbr title="Base Upload Limit">#{user.base_upload_limit}</abbr>
     + (<abbr title="Approved Posts">#{upload_limit_pieces[:approved]}</abbr> / 10)
-    - (<abbr title="Deleted or Replaced Posts, Rejected Replacements\n#{upload_limit_pieces[:deleted_ignore]} of your Post Replacements do not affect your upload limit">#{upload_limit_pieces[:deleted]}</abbr> / 4)
+    - (<abbr title="Deleted or Replaced Posts, Rejected Replacements\n#{upload_limit_pieces[:deleted_ignore]} of your Replaced Posts do not affect your upload limit">#{upload_limit_pieces[:deleted]}</abbr> / 4)
     - <abbr title="Pending or Flagged Posts">#{upload_limit_pieces[:pending]}</abbr>
     = <abbr title="User Upload Limit Remaining">#{user.upload_limit}</abbr>}.html_safe
   end
