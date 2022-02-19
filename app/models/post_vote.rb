de0311a2683290a815875d18d22fbe3f220422b0
Earlopain <earlopain@protonmail.com>
2022-02-19 17:19:44 +0100

    [Cleanup] Remove a few unused files

--- app/models/post_vote.rb
+++ app/models/post_vote.rb
@@ -11,18 +11,6 @@ class PostVote < ApplicationRecord
 
   scope :for_user, ->(uid) {where("user_id = ?", uid)}
 
-  def self.positive_user_ids
-    select_values_sql("select user_id from post_votes where score > 0 group by user_id having count(*) > 100")
-  end
-
-  def self.negative_post_ids(user_id)
-    select_values_sql("select post_id from post_votes where score < 0 and user_id = ?", user_id)
-  end
-
-  def self.positive_post_ids(user_id)
-    select_values_sql("select post_id from post_votes where score > 0 and user_id = ?", user_id)
-  end
-
   def initialize_attributes
     self.user_id ||= CurrentUser.user.id
     self.user_ip_addr ||= CurrentUser.ip_addr
