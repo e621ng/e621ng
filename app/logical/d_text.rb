aa9739e96173dd389a021bd8697d5cac5e3bba68
Earlopain <earlopain@protonmail.com>
2022-03-16 15:17:31 +0100

    [Cleanup] Remove unused dtext code

--- app/logical/d_text.rb
+++ app/logical/d_text.rb
@@ -9,16 +9,6 @@ class DText
     "[quote]\n#{creator_name} said:\n\n#{stripped_body}\n[/quote]\n\n"
   end
 
-  def self.parse_mentions(text)
-    text = strip_blocks(text.to_s, "quote")
-
-    names = text.scan(MENTION_REGEXP).map do |mention|
-      mention.gsub(/(?:^\s*@)|(?:[:;,.!?\)\]<>]$)/, "")
-    end
-
-    names.uniq
-  end
-
   def self.strip_blocks(string, tag)
     n = 0
     stripped = ""
@@ -45,68 +35,5 @@ class DText
     end
 
     stripped.strip
-  end
-
-  def self.from_html(text, inline: false, &block)
-    html = Nokogiri::HTML.fragment(text)
-
-    dtext = html.children.map do |element|
-      block.call(element) if block.present?
-
-      case element.name
-      when "text"
-        element.content.gsub(/(?:\r|\n)+$/, "")
-      when "br"
-        "\n"
-      when "p", "ul", "ol"
-        from_html(element.inner_html, &block).strip + "\n\n"
-      when "blockquote"
-        "[quote]#{from_html(element.inner_html, &block).strip}[/quote]\n\n" if element.inner_html.present?
-      when "small", "sub"
-        "[tn]#{from_html(element.inner_html, &block)}[/tn]" if element.inner_html.present?
-      when "b", "strong"
-        "[b]#{from_html(element.inner_html, &block)}[/b]" if element.inner_html.present?
-      when "i", "em"
-        "[i]#{from_html(element.inner_html, &block)}[/i]" if element.inner_html.present?
-      when "u"
-        "[u]#{from_html(element.inner_html, &block)}[/u]" if element.inner_html.present?
-      when "s", "strike"
-        "[s]#{from_html(element.inner_html, &block)}[/s]" if element.inner_html.present?
-      when "li"
-        "* #{from_html(element.inner_html, &block)}\n" if element.inner_html.present?
-      when "h1", "h2", "h3", "h4", "h5", "h6"
-        hN = element.name
-        title = from_html(element.inner_html, &block)
-        "#{hN}. #{title}\n\n"
-      when "a"
-        title = from_html(element.inner_html, inline: true, &block).strip
-        url = element["href"]
-        %("#{title}":[#{url}]) if title.present? && url.present?
-      when "img"
-        alt_text = element.attributes["title"] || element.attributes["alt"] || ""
-        src = element["src"]
-
-        if inline
-          alt_text
-        elsif alt_text.present? && src.present?
-          %("#{alt_text}":[#{src}]\n\n)
-        else
-          ""
-        end
-      when "comment"
-        # ignored
-      else
-        from_html(element.inner_html, &block)
-      end
-    end.join
-
-    dtext
-  end
-
-  # extract the first paragraph `needle` occurs in.
-  def self.excerpt(dtext, needle)
-    dtext = dtext.gsub(/\r\n|\r|\n/, "\n")
-    excerpt = ActionController::Base.helpers.excerpt(dtext, needle, separator: "\n\n", radius: 1, omission: "")
-    excerpt
   end
 end
