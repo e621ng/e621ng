aceb632fd92422dfcbd1a873fe817b743acef570
Earlopain <earlopain@protonmail.com>
2022-03-30 19:23:27 +0200

    [Notes] Add fixer script for out of bounds notes
    These prevent replacements from being accepted

---
+++ script/fixes/106_note_ranges.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+
+require File.expand_path(File.join(File.dirname(__FILE__), '..', '..', 'config', 'environment'))
+
+Note.where("x < 0").in_batches.update_all(x: 0)
+Note.where("y < 0").in_batches.update_all(y: 0)
+Note.where("width < 0").in_batches.update_all(width: 0)
+Note.where("height < 0").in_batches.update_all(height: 0)
+Note.joins(:post).includes(:post).where("x + width > posts.image_width").find_each do |note|
+  note.update_column(:width, note.post.image_width - note.x)
+end
+
+Note.joins(:post).includes(:post).where("y + height > posts.image_height").find_each do |note|
+  note.update_column(:height, note.post.image_height - note.y)
+end
