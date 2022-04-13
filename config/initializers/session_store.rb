98335619428902c19334e2990e8c030dd87033b5
Earlopain <earlopain@protonmail.com>
2022-04-13 12:02:01 +0200

    [Rails] Remove old framework_defaults files
    The only change is `urlsafe_csrf_tokens`, which is now true.
    `cookies_same_site_protection` was specified at another place,
    :lax is now the default and can be removed

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,4 +1,3 @@
 # Be sure to restart your server when you modify this file.
 
 Rails.application.config.session_store :cookie_store, key: '_danbooru_session', same_site: :lax, secure: Rails.env.production?
-Rails.application.config.action_dispatch.cookies_same_site_protection = :lax
