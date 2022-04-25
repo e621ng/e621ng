c930c9ee52eb300d174fcc1fdaf15739e256d4b0
Earlopain <earlopain@protonmail.com>
2022-04-25 21:10:20 +0200

    [Cleanup] Remove unused binstubs + capistrano

--- config/deploy.rb
+++
@@ -1,9 +0,0 @@
-set :stages, %w(production development staging)
-set :default_stage, "staging"
-set :application, "danbooru"
-set :repo_url,  "git://github.com/zwagoth/e621ng.git"
-set :scm, :git
-set :deploy_to, "/var/www/danbooru"
-set :rbenv_ruby, "2.5.1"
-set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle')
-set :branch, ENV.fetch("branch", "master")
--- config/deploy/staging.rb
+++
@@ -1,7 +0,0 @@
-set :user, "danbooru"
-set :rails_env, "staging"
-server "testbooru.donmai.us", :roles => %w(web app db), :primary => true, :user => "danbooru"
-
-set :linked_files, fetch(:linked_files, []).push(".env.staging")
-set :rbenv_path, "/home/danbooru/.rbenv"
-set :deploy_to, "/var/www/danbooru2"
