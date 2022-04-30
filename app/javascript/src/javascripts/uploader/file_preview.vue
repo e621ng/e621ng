4229528189d3a509290927030277416cad5cb9ab
Earlopain <earlopain@protonmail.com>
2022-04-30 14:15:35 +0200

    [Uploader] Seperate image/video preview components

--- app/javascript/src/javascripts/uploader/file_preview.vue
+++ app/javascript/src/javascripts/uploader/file_preview.vue
@@ -2,15 +2,12 @@
     <div class="upload_preview_container" :class="classes">
         <div v-if="!preview.failed">
           <div class="upload_preview_dims">{{ previewDimensions }}</div>
-          <img class="upload_preview_img" :src="preview.url"
-              referrerpolicy="no-referrer"
-              v-if="!preview.isVideo"
-              v-on:load="$emit('load', $event)" v-on:error="$emit('error')"/>
-          <video class="upload_preview_img" controls :src="preview.url"
-              v-on:loadeddata="$emit('load', $event)" v-on:error="$emit('error')"
-              v-if="preview.isVideo"></video>
+          <preview-video v-if="preview.isVideo" :url="preview.url"
+            @load="$emit('load', $event)" @error="$emit('error')"></preview-video>
+          <preview-image v-else :url="preview.url"
+            @load="$emit('load', $event)" @error="$emit('error')"></preview-image>
         </div>
-        <div class="preview-fail box-section sect_yellow" v-if="preview.failed">
+        <div v-else class="preview-fail box-section sect_yellow">
           <p>The preview for this file failed to load. Please, double check that the URL you provided is correct.</p>
           Note that some sites intentionally prevent images they host from being displayed on other sites. The file can still be uploaded despite that.
         </div>
@@ -18,7 +15,14 @@
 </template>
 
 <script>
+import previewImage from "./preview_image.vue";
+import previewVideo from "./preview_video.vue";
 export default {
+
+  components: {
+    "preview-image": previewImage,
+    "preview-video": previewVideo,
+  },
   props: {
     classes: String,
     preview: Object
---
+++ app/javascript/src/javascripts/uploader/preview_image.vue
@@ -0,0 +1,13 @@
+<template>
+  <img class="upload_preview_img" :src="url"
+    referrerpolicy="no-referrer"
+    v-on:load="$emit('load', $event)" v-on:error="$emit('error')"/>
+</template>
+
+<script>
+  export default {
+    props: {
+      url: String
+    }
+  }
+</script>
