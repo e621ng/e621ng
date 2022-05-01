e59eee6154e708366ea3834a0703be93ab61d8bf
Earlopain <earlopain@protonmail.com>
2022-05-01 13:24:40 +0200

    [Uploader] Fix indentation on file preview

--- app/javascript/src/javascripts/uploader/file_preview.vue
+++ app/javascript/src/javascripts/uploader/file_preview.vue
@@ -1,17 +1,17 @@
 <template>
-    <div class="upload_preview_container" :class="classes">
-        <div v-if="!preview.failed">
-          <div class="upload_preview_dims">{{ previewDimensions }}</div>
-          <preview-video v-if="preview.isVideo" :url="preview.url"
-            @load="$emit('load', $event)" @error="$emit('error')"></preview-video>
-          <preview-image v-else :url="preview.url"
-            @load="$emit('load', $event)" @error="$emit('error')"></preview-image>
-        </div>
-        <div v-else class="preview-fail box-section sect_yellow">
-          <p>The preview for this file failed to load. Please, double check that the URL you provided is correct.</p>
-          Note that some sites intentionally prevent images they host from being displayed on other sites. The file can still be uploaded despite that.
-        </div>
+  <div class="upload_preview_container" :class="classes">
+    <div v-if="!preview.failed">
+      <div class="upload_preview_dims">{{ previewDimensions }}</div>
+      <preview-video v-if="preview.isVideo" :url="preview.url"
+        @load="$emit('load', $event)" @error="$emit('error')"></preview-video>
+      <preview-image v-else :url="preview.url"
+        @load="$emit('load', $event)" @error="$emit('error')"></preview-image>
     </div>
+    <div v-else class="preview-fail box-section sect_yellow">
+      <p>The preview for this file failed to load. Please, double check that the URL you provided is correct.</p>
+      Note that some sites intentionally prevent images they host from being displayed on other sites. The file can still be uploaded despite that.
+    </div>
+  </div>
 </template>
 
 <script>
