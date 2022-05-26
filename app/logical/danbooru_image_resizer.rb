7765bf5a75b50275043eb2c60bca54d3a4e456f5
Earlopain <earlopain@protonmail.com>
2022-05-26 18:40:20 +0200

    [Cleanup] Simplify image resize code somewhat

--- app/logical/danbooru_image_resizer.rb
+++ app/logical/danbooru_image_resizer.rb
@@ -5,22 +5,13 @@ module DanbooruImageResizer
   SRGB_PROFILE = "#{Rails.root}/config/sRGB.icm"
   # https://www.libvips.org/API/current/libvips-resample.html#vips-thumbnail
   THUMBNAIL_OPTIONS = { size: :down, linear: false, no_rotate: true, export_profile: SRGB_PROFILE, import_profile: SRGB_PROFILE }
-  THUMBNAIL_OPTIONS_NO_ICC = { size: :down, linear: false, no_rotate: true, export_profile: SRGB_PROFILE }
   # https://www.libvips.org/API/current/VipsForeignSave.html#vips-jpegsave
   JPEG_OPTIONS = { background: 0, strip: true, interlace: true, optimize_coding: true }
   CROP_OPTIONS = { linear: false, no_rotate: true, export_profile: SRGB_PROFILE, import_profile: SRGB_PROFILE, crop: :attention }
-  CROP_OPTIONS_NO_ICC = { linear: false, no_rotate: true, export_profile: SRGB_PROFILE, crop: :attention }
 
-  # https://github.com/libvips/libvips/wiki/HOWTO----Image-shrinking
-  # https://www.libvips.org/API/current/Using-vipsthumbnail.md.html
   def resize(file, width, height, resize_quality = 90)
     output_file = Tempfile.new
-    begin
-      resized_image = Vips::Image.thumbnail(file.path, width, height: height, **THUMBNAIL_OPTIONS)
-    rescue Vips::Error => e
-      raise e unless e.message =~ /icc_transform/i
-      resized_image = Vips::Image.thumbnail(file.path, width, height: height, **THUMBNAIL_OPTIONS_NO_ICC)
-    end
+    resized_image = thumbnail(file, width, height, THUMBNAIL_OPTIONS)
     resized_image.jpegsave(output_file.path, Q: resize_quality, **JPEG_OPTIONS)
 
     output_file
@@ -30,14 +21,18 @@ module DanbooruImageResizer
     return nil unless Danbooru.config.enable_image_cropping?
 
     output_file = Tempfile.new
-    begin
-      resized_image = Vips::Image.thumbnail(file.path, width, height: height, **CROP_OPTIONS)
-    rescue Vips::Error => e
-      raise e unless e.message =~ /icc_transform/i
-      resized_image = Vips::Image.thumbnail(file.path, width, height: height, **CROP_OPTIONS_NO_ICC)
-    end
+    resized_image = thumbnail(file, width, height, CROP_OPTIONS)
     resized_image.jpegsave(output_file.path, Q: resize_quality, **JPEG_OPTIONS)
 
     output_file
+  end
+
+  # https://github.com/libvips/libvips/wiki/HOWTO----Image-shrinking
+  # https://www.libvips.org/API/current/Using-vipsthumbnail.md.html
+  def thumbnail(file, width, height, options)
+    Vips::Image.thumbnail(file.path, width, height: height, **options)
+  rescue Vips::Error => e
+    raise e unless e.message =~ /icc_transform/i
+    Vips::Image.thumbnail(file.path, width, height: height, **options.except(:import_profile))
   end
 end
