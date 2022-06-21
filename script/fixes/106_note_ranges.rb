7cd266d7bb10ce6ad5b4faba682da359e46f2351
Earlopain <earlopain@protonmail.com>
2022-06-21 21:29:27 +0200

    [Notes] Change order of fixup script
    This doesn't leave notes with a negative width/height behind

--- script/fixes/106_note_ranges.rb
+++ script/fixes/106_note_ranges.rb
@@ -6,13 +6,6 @@ Note.where("x < 0").in_batches.update_all(x: 0)
 Note.where("y < 0").in_batches.update_all(y: 0)
 Note.where("width < 0").in_batches.update_all(width: 0)
 Note.where("height < 0").in_batches.update_all(height: 0)
-Note.joins(:post).includes(:post).where("x + width > posts.image_width").find_each do |note|
-  note.update_column(:width, note.post.image_width - note.x)
-end
-
-Note.joins(:post).includes(:post).where("y + height > posts.image_height").find_each do |note|
-  note.update_column(:height, note.post.image_height - note.y)
-end
 
 Note.joins(:post).includes(:post).where("x > posts.image_width").find_each do |note|
   note.update_column(:x, note.post.image_width)
@@ -20,4 +13,12 @@ end
 
 Note.joins(:post).includes(:post).where("y > posts.image_height").find_each do |note|
   note.update_column(:y, note.post.image_height)
+end
+
+Note.joins(:post).includes(:post).where("x + width > posts.image_width").find_each do |note|
+  note.update_column(:width, note.post.image_width - note.x)
+end
+
+Note.joins(:post).includes(:post).where("y + height > posts.image_height").find_each do |note|
+  note.update_column(:height, note.post.image_height - note.y)
 end
