ff5dc78cf4d463e78d5f0fff2832e6072ae40d35
Earlopain <earlopain@protonmail.com>
2022-07-10 16:09:24 +0200

    [Tags] Convert "Hide empty?" to checkbox
    Also added option for a default value and made checkbox inputs
    react to "truthy" for  backwards compatability

--- app/inputs/search_form_builder.rb
+++ app/inputs/search_form_builder.rb
@@ -3,20 +3,20 @@ class SearchFormBuilder < SimpleForm::FormBuilder
     options[:input_html] ||= {}
     options[:input_html][:data] = {}
     options[:input_html][:data][:autocomplete] = options[:autocomplete] if options[:autocomplete]
-    options = insert_value_from_search_params(attribute_name, options)
+    options = insert_value(attribute_name, options)
     super
   end
 
   private
 
-  def insert_value_from_search_params(attribute_name, options)
-    value = @options[:search_params][attribute_name]
+  def insert_value(attribute_name, options)
+    value = @options[:search_params][attribute_name] || options[:default]&.to_s
     return options if value.nil?
 
     if options[:collection]
       options[:selected] = value
     elsif options[:as]&.to_sym == :boolean
-      options[:input_html][:checked] = true if value == "1"
+      options[:input_html][:checked] = true if value.truthy?
     else
       options[:input_html][:value] = value
     end
