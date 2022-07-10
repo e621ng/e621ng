4702691288d1c54212e9d7e53470c2485ddaefb1
Earlopain <earlopain@protonmail.com>
2022-07-10 21:04:25 +0200

    [Tags] Fix MetaSearch finding results without input

--- app/logical/meta_searches/tag.rb
+++ app/logical/meta_searches/tag.rb
@@ -1,30 +1,28 @@
 class MetaSearches::Tag
   MAX_RESULTS = 25
-  attr_reader :search_params, :tags, :tag_aliases, :tag_implications
+  attr_reader :search, :tags, :tag_aliases, :tag_implications
 
   def initialize(search_params)
-    @search_params = search_params
+    @search = search_params[:name] || ""
   end
 
   def load_all
+    return if search.blank?
+
     load_tags
     load_tag_aliases
     load_tag_implications
   end
 
   def load_tags
-    @tags = ::Tag.name_matches(name_param).limit(MAX_RESULTS)
+    @tags = ::Tag.name_matches(search).limit(MAX_RESULTS)
   end
 
   def load_tag_aliases
-    @tag_aliases = TagAlias.name_matches(name_param).limit(MAX_RESULTS)
+    @tag_aliases = TagAlias.name_matches(search).limit(MAX_RESULTS)
   end
 
   def load_tag_implications
-    @tag_implications = TagImplication.name_matches(name_param).limit(MAX_RESULTS)
-  end
-
-  def name_param
-    search_params[:name] || ""
+    @tag_implications = TagImplication.name_matches(search).limit(MAX_RESULTS)
   end
 end
