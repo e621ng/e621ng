f7e6985c828acfc63e84f21ad28ce05d6e288f6e
Earlopain <earlopain@protonmail.com>
2022-07-13 21:12:37 +0200

    [Forms] Insert autocomplete through a shortcut property

---
+++ app/inputs/form_builder_common.rb
@@ -0,0 +1,8 @@
+module FormBuilderCommon
+  def insert_autocomplete(options)
+    options[:input_html] ||= {}
+    options[:input_html][:data] = {}
+    options[:input_html][:data][:autocomplete] = options[:autocomplete] if options[:autocomplete]
+    options
+  end
+end
