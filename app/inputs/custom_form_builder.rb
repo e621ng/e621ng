9d9b353e3f577de28517f0ab2490755135c4638b
Earlopain <earlopain@protonmail.com>
2022-08-05 19:48:18 +0200

    [Forms] Simplify button_select implementation
    It now behaves like all the other simple form input fields. It's also
    available in both the search builder and custom builder automatically.
    Turns out I was passing the label/value in reverse, whoops.

--- app/inputs/custom_form_builder.rb
+++ app/inputs/custom_form_builder.rb
@@ -4,14 +4,5 @@ class CustomFormBuilder < SimpleForm::FormBuilder
     super
   end
 
-  def button_select(attribute_name, values, **args)
-    html = collection_radio_buttons(attribute_name, values, :first, :last)
-    label = args[:label] || attribute_name.to_s.titleize
-    %(<div class="collection-radio-buttons input">
-      <label>#{label}</label>
-      #{html}
-    </div>).html_safe
-  end
-
   include FormBuilderCommon
 end
