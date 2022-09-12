9844f5f188613b61d363189dc28f09fcc028de99
Earlopain <earlopain@protonmail.com>
2022-09-12 18:00:25 +0200

    [TagRelations] Order by status, id instead of status, name
    This is how it works on basically every other page and is what I would expect

--- app/models/tag_relationship.rb
+++ app/models/tag_relationship.rb
@@ -110,7 +110,7 @@ class TagRelationship < ApplicationRecord
 
     def pending_first
       # unknown statuses return null and are sorted first
-      order(Arel.sql("array_position(array['queued', 'processing', 'pending', 'active', 'deleted', 'retired'], status::text) NULLS FIRST, antecedent_name, consequent_name"))
+      order(Arel.sql("array_position(array['queued', 'processing', 'pending', 'active', 'deleted', 'retired'], status::text) NULLS FIRST, id desc"))
     end
 
     def default_order
