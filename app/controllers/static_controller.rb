13f9f7365ee60826ddabaf1a95de8340ea0980d5
Kira <kira+git@e621.net>
2022-11-24 16:56:09 -0800

    [Rails 7] Fix open redirect protection on discord route.

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -54,7 +54,7 @@ class StaticController < ApplicationController
       hashed_values = Digest::SHA256.hexdigest("#{CurrentUser.name} #{CurrentUser.id} #{time} #{secret}")
       user_hash = "?user_id=#{CurrentUser.id}&username=#{CurrentUser.name}&time=#{time}&hash=#{hashed_values}"
 
-      redirect_to(Danbooru.config.discord_site + user_hash)
+      redirect_to(Danbooru.config.discord_site + user_hash, allow_other_host: true)
     end
   end
 
