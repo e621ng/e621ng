bf07012fc27003c75930ab566af68164820a2b66
Donovan Daniels <hewwo@yiff.rocks>
2023-03-16 04:06:41 -0500

    [HelpPages] Add default value for related & title

---
+++ db/migrate/20230316084945_help_pages_add_default_values.rb
@@ -0,0 +1,8 @@
+class HelpPagesAddDefaultValues < ActiveRecord::Migration[7.0]
+  def change
+    change_column_null :help_pages, :related, false
+    change_column_default :help_pages, :related, from: nil, to: ""
+    change_column_null :help_pages, :title, false
+    change_column_default :help_pages, :title, from: nil, to: ""
+  end
+end
