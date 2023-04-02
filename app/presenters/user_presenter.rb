8ba5ea2699d3d2c7cfd9e16813aeabf16e7f0745
Earlopain <earlopain@protonmail.com>
2023-04-02 23:25:09 +0200

    [Misc] Fix rubocop offense

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -131,7 +131,7 @@ class UserPresenter
   end
 
   def flag_count(template)
-    template.link_to(user.flag_count, template.post_flags_path( search: { creator_id: user.id }))
+    template.link_to(user.flag_count, template.post_flags_path(search: { creator_id: user.id }))
   end
 
   def approval_count(template)
