e621ng/e621ng

commit 2979c9623d92b9a537f15e2da729de32db2cb2c6

[Posts] Remove pixiv source special handling
Only worked with http links anyways

app/helpers/posts_helper.rb
@@ -18,27 +18,15 @@ module PostsHelper
   end
 
   def post_source_tag(source)
-
-    if source =~ %r!\Ahttp://img\d+\.pixiv\.net/img/([^\/]+)/!i
-      text = "pixiv/<wbr>#{wordbreakify($1)}".html_safe
-      source_search = "source:pixiv/#{$1}/"
-    elsif source =~ %r!\Ahttp://i\d\.pixiv\.net/img\d+/img/([^\/]+)/!i
-      text = "pixiv/<wbr>#{wordbreakify($1)}".html_safe
-      source_search = "source:pixiv/#{$1}/"
-    elsif source =~ %r{\Ahttps?://}i
-      text = source.sub(/\Ahttps?:\/\/(?:www\.)?/i, "")
-      source_search = "source:#{source.sub(/[^\/]*$/, "")}"
-    end
-
     # Only allow http:// and https:// links. Disallow javascript: links.
-    if source =~ %r!\Ahttps?://!i
-      source_link = link_to(text, source, target: "_blank", rel: "nofollow noreferrer noopener")
+    if source =~ %r{\Ahttps?://}i
+      source_link = link_to(source.sub(%r{\Ahttps?://(?:www\.)?}i, ""), source, target: "_blank", rel: "nofollow noreferrer noopener")
     else
       source_link = source
     end
 
-    if CurrentUser.is_janitor? && !source_search.blank?
-      source_link + "&nbsp;".html_safe + link_to("&raquo;".html_safe, posts_path(:tags => source_search), :rel => "nofollow")
+    if CurrentUser.is_janitor? && (source_search = "source:#{source.sub(%r{[^/]*$}, '')}").present?
+      source_link + "&nbsp;".html_safe + link_to("&raquo;".html_safe, posts_path(tags: source_search), rel: "nofollow")
     else
       source_link
     end

commit 1d835871d6f86c3609550c610f431a06d9065b11

[Posts] Fix non-url sources for janitors
Broken in https://github.com/e621ng/e621ng/commit/2979c9623d92b9a537f15e2da729de32db2cb2c6

app/helpers/posts_helper.rb
@@ -21,14 +21,14 @@ module PostsHelper
     # Only allow http:// and https:// links. Disallow javascript: links.
     if source =~ %r{\Ahttps?://}i
       source_link = link_to(source.sub(%r{\Ahttps?://(?:www\.)?}i, ""), source, target: "_blank", rel: "nofollow noreferrer noopener")
-    else
-      source_link = source
-    end
 
-    if CurrentUser.is_janitor? && (source_search = "source:#{source.sub(%r{[^/]*$}, '')}").present?
-      source_link + "&nbsp;".html_safe + link_to("&raquo;".html_safe, posts_path(tags: source_search), rel: "nofollow")
-    else
+      if CurrentUser.is_janitor?
+        source_link += " ".html_safe + link_to("»", posts_path(tags: "source:#{source.sub(%r{[^/]*$}, '')}"), rel: "nofollow")
+      end
+
       source_link
+    else
+      source
     end
   end
 