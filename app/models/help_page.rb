7221dfde3c3bdddd5f4c88ca3da71e46143f1c80
Earlopain <earlopain@protonmail.com>
2023-04-10 17:51:25 +0200

    [HelpPages] Invalidate cache on destroy
    Part of #492

--- app/models/help_page.rb
+++ app/models/help_page.rb
@@ -3,23 +3,24 @@ class HelpPage < ApplicationRecord
   validates :wiki_page, :name, presence: true
   before_validation :normalize
   validate :wiki_page_exists
+  after_destroy :invalidate_cache
   after_save :invalidate_cache
 
   def invalidate_cache
-    Cache.delete('help_index')
+    Cache.delete("help_index")
     Cache.delete("help_index:#{name}")
     true
   end
 
   def wiki_page_exists
-    errors.add(:wiki_page, "must exist") unless WikiPage.find_by_title(wiki_page).present?
+    errors.add(:wiki_page, "must exist") if WikiPage.find_by(title: wiki_page).blank?
   end
 
   def normalize
     self.name = HelpPage.normalize_name(name)
   end
 
-  def self.find_by_name(name)
+  def self.find_cached_by_name(name)
     Cache.fetch("help_index:#{name}", 12.hours.to_i) { where("name = ?", name).first }
   end
 
@@ -28,13 +29,13 @@ class HelpPage < ApplicationRecord
   end
 
   def self.title(name)
-    help = HelpPage.find_by_name(name)
+    help = HelpPage.find_cached_by_name(name)
 
     # Prevents exceptions when related links aren't in the help DB
     return name.titleize unless help
 
     # Generate pretty name if title doesn't exist
-    return help.name.titleize unless help.title.present?
+    return help.name.titleize if help.title.blank?
 
     help.title
   end
