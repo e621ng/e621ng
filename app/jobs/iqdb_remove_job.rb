2860412049568bce1f114ca610c5452f12f52617
Earlopain <earlopain@protonmail.com>
2023-04-28 10:26:53 +0200

    [IQDB] Remove old job classes

--- app/jobs/iqdb_remove_job.rb
+++
@@ -1,9 +0,0 @@
-class IqdbRemoveJob
-  include Sidekiq::Worker
-
-  sidekiq_options queue: 'iqdb'
-
-  def perform(post_id)
-    # STUB: The implementation of this is performed by the iqdb component.
-  end
-end
--- app/jobs/iqdb_update_job.rb
+++
@@ -1,9 +0,0 @@
-class IqdbUpdateJob
-  include Sidekiq::Worker
-
-  sidekiq_options queue: 'iqdb'
-
-  def perform(post_id, thumbnail_url)
-    # STUB: The implementation of this is performed by the iqdb component.
-  end
-end
