a33077b983b8c3204df09f5330b682a0e242051b
Earlopain <earlopain@protonmail.com>
2023-05-01 16:22:06 +0200

    [Sidekiq] Add plain ActiveJob copies of Sidekiq::Worker

---
+++ app/jobs/index_update_job_copy.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class IndexUpdateJobCopy < ApplicationJob
+  queue_as :high_prio
+  sidekiq_options lock: :until_executing
+
+  def perform(klass, id)
+    obj = klass.constantize.find(id)
+    obj.update_index(defer: false)
+  rescue ActiveRecord::RecordNotFound
+    # Do nothing
+  end
+end
---
+++ app/jobs/takedown_job_copy.rb
@@ -0,0 +1,33 @@
+# frozen_string_literal: true
+
+class TakedownJobCopy < ApplicationJob
+  queue_as :high_prio
+  sidekiq_options lock: :until_executing, lock_args_method: :lock_args
+
+  def self.lock_args(args)
+    [args[0]]
+  end
+
+  def perform(id, approver, del_reason)
+    @takedown = Takedown.find(id)
+    @approver = User.find(approver)
+    @takedown.approver_id = @approver.id
+    CurrentUser.scoped(@approver) do
+      ModAction.log(:takedown_process, { takedown_id: @takedown.id })
+    end
+
+    CurrentUser.as_system do
+      @takedown.status = @takedown.calculated_status
+      @takedown.save!
+      @takedown.actual_posts.find_each do |p|
+        if @takedown.should_delete(p.id)
+          next if p.is_deleted?
+          p.delete!("takedown ##{@takedown.id}: #{del_reason}", { force: true })
+        else
+          next unless p.is_deleted?
+          p.undelete!({ force: true })
+        end
+      end
+    end
+  end
+end
