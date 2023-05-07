2901c7c10cbfe254722c5a35fc4d68faa7b56e46
Earlopain <earlopain@protonmail.com>
2023-05-07 12:45:16 +0200

    [Search] Move the form generation to the helper method

--- app/helpers/form_search_helper.rb
+++ app/helpers/form_search_helper.rb
@@ -1,17 +1,25 @@
 module FormSearchHelper
-  def form_search(path:, always_display: false, &block)
+  def form_search(path:, always_display: false, &)
     # dedicated search routes like /comments/search should always show
     hideable = request.path.split("/")[2] != "search"
-    show_on_load = filled_form_fields(block).any? || always_display || !hideable
-    render "application/form_search", path: path, hideable: hideable, show_on_load: show_on_load, block: block
+    show_on_load = filled_form_fields(&).any? || always_display || !hideable
+    form = simple_form_for(:search, {
+      method: :get,
+      url: path,
+      builder: SearchFormBuilder,
+      search_params: params[:search],
+      defaults: { required: false },
+      html: { class: "inline-form" },
+    }, &)
+    render "application/form_search", hideable: hideable, show_on_load: show_on_load, form: form
   end
 
   # When the simple_form has f.input :name and search[name]=test [:name] will be returned
   # Some search params aren't exposed in the ui, but have links. In that case it
   # isn't expected to have the form be open, since no values are set.
-  def filled_form_fields(block)
+  def filled_form_fields(&)
     form_field_collector = FormFieldCollector.new
-    capture { block.call(form_field_collector) }
+    capture { yield(form_field_collector) }
     form_field_collector.fields & params[:search].keys.map(&:to_sym)
   end
 end
