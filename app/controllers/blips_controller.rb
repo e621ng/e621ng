41c8f26e079364b1c1d2cb75fe20efa5e3d18c05
Donovan Daniels <hewwo@yiff.rocks>
2023-05-16 12:33:44 -0500

    use shorter syntax
    Co-authored-by: Earlopain <earlopain+github@protonmail.com>

--- app/controllers/blips_controller.rb
+++ app/controllers/blips_controller.rb
@@ -116,10 +116,7 @@ class BlipsController < ApplicationController
         redirect_back(fallback_location: blips_path, flash: { notice: "You cannot edit blips more than 5 minutes old" })
       end
       format.json do
-        render json: {
-          success: false,
-          reason: "You cannot edit blips more than 5 minutes old",
-        }, status: 422
+       render_expected_error(422, "You cannot edit blips more than 5 minutes old")
       end
     end
   end
