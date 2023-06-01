8f59358bbaf14bfff6d23f97446f061627c9a23b
Earlopain <earlopain@protonmail.com>
2023-06-01 16:29:24 +0200

    [Cleanup] Remove `link_to_wiki`

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -108,14 +108,6 @@ module ApplicationHelper
     link_to ip, moderator_ip_addrs_path(:search => {:ip_addr => ip})
   end
 
-  def link_to_wiki(*wiki_titles, **options)
-    links = wiki_titles.map do |title|
-      link_to title.tr("_", " "), wiki_pages_path(title: title)
-    end
-
-    to_sentence(links, **options)
-  end
-
   def link_to_user(user, include_activation: false)
     return "anonymous" if user.blank?
 
