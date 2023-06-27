3b775e48bdbd95c058a3d373db3bea0f92fb9ff5
Earlopain <earlopain@protonmail.com>
2023-06-27 15:55:53 +0200

    [Discord] Improve janitor report
    * Make numbers bold
    * Add count of pending flags/replacements
    * Don't show negative numbers as diff

--- app/logical/janitor_report_generator.rb
+++ app/logical/janitor_report_generator.rb
@@ -1,24 +1,24 @@
 class JanitorReportGenerator
-  extend ActiveSupport::NumberHelper
-
   def self.run!
     return if Danbooru.config.janitor_reports_discord_webhook_url.blank?
 
     current_stats = stats
-    previous_queue_size = Cache.redis.get("janitor_reports:previous_queue_size") || current_stats[:queue_size]
-    Cache.redis.set("janitor_reports:previous_queue_size", current_stats[:queue_size])
+    previous_pending_posts = Cache.redis.get("janitor_reports:previous_pending_posts") || current_stats[:pending][:posts]
+    Cache.redis.set("janitor_reports:previous_pending_posts", current_stats[:pending][:posts])
 
-    diff = current_stats[:queue_size] - previous_queue_size.to_i
+    diff = current_stats[:pending][:posts] - previous_pending_posts.to_i
     content = <<~REPORT.chomp
       Janitor report for <t:#{Time.now.to_i}:D>
-      Currently, there are #{number_to_delimited(current_stats[:queue_size])} pending posts. There were #{number_to_delimited(previous_queue_size)} pending posts yesterday.
-      That is #{number_to_delimited(diff)} #{diff >= 0 ? 'more' : 'less'} than the day before.
+      Currently, there are:
+      #{formatted_number(current_stats[:pending][:posts])} pending posts. That is #{formatted_number(diff.abs)} #{diff >= 0 ? 'more' : 'less'} than the day before.
+      #{formatted_number(current_stats[:pending][:flags])} pending flags.
+      #{formatted_number(current_stats[:pending][:replacements])} pending replacements.
 
-      #{number_to_delimited(current_stats[:posts])} posts were added yesterday.
+      #{formatted_number(current_stats[:posts])} posts were uploaded yesterday.
 
-      #{number_to_delimited(current_stats[:approvals] + current_stats[:deletions][:total])} posts were processed.
-      Approvals: #{number_to_delimited(current_stats[:approvals])}
-      Deletions: #{number_to_delimited(current_stats[:deletions][:total])} (#{number_to_delimited(current_stats[:deletions][:automod] - current_stats[:deletions][:takedowns])} automated, #{number_to_delimited(current_stats[:deletions][:takedowns])} takedown, #{number_to_delimited(current_stats[:deletions][:total] - current_stats[:deletions][:automod])} manual)
+      #{formatted_number(current_stats[:approvals] + current_stats[:deletions][:total])} posts were processed.
+      Approvals: #{formatted_number(current_stats[:approvals])}
+      Deletions: #{formatted_number(current_stats[:deletions][:total])} (#{formatted_number(current_stats[:deletions][:automod] - current_stats[:deletions][:takedowns])} automated, #{formatted_number(current_stats[:deletions][:takedowns])} takedown, #{formatted_number(current_stats[:deletions][:total] - current_stats[:deletions][:automod])} manual)
     REPORT
 
     HTTParty.post(
@@ -36,7 +36,11 @@ class JanitorReportGenerator
   def self.stats
     deletions = PostFlag.where(is_deletion: true).where("created_at >= ?", 1.day.ago)
     {
-      queue_size: Post.pending.count,
+      pending: {
+        posts: Post.pending.count,
+        flags: Post.where(is_flagged: true).count,
+        replacements: PostReplacement.pending.count,
+      },
       deletions: {
         total: deletions.count,
         automod: deletions.where(creator_id: User.system.id).count,
@@ -45,5 +49,9 @@ class JanitorReportGenerator
       approvals: PostApproval.where("created_at >= ?", 1.day.ago).count,
       posts: Post.where("created_at >= ? ", 1.day.ago).count,
     }
+  end
+
+  def self.formatted_number(input)
+    "**#{ActiveSupport::NumberHelper.number_to_delimited(input)}**"
   end
 end
