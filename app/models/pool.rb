e621ng/e621ng

commit 4cd32c5f874b4bd8bb810c5864edb9a5eaee7bb5

[Cleanup] Remove `*_sql` ApplicationRecord methods

app/models/pool.rb
@@ -129,7 +129,7 @@ class Pool < ApplicationRecord
     if name =~ /\A\d+\z/
       name.to_i
     else
-      select_value_sql("SELECT id FROM pools WHERE lower(name) = ?", name.downcase.tr(" ", "_")).to_i
+      Pool.where("lower(name) = ?", name.downcase.tr(" ", "_")).pick(:id)
     end
   end
 

app/models/post_set.rb
@@ -42,7 +42,7 @@ class PostSet < ApplicationRecord
     if name =~ /\A\d+\z/
       name.to_i
     else
-      select_value_sql("SELECT id FROM post_sets WHERE lower(shortname) = ?", name.downcase.tr(" ", "_")).to_i
+      PostSet.where("lower(shortname) = ?", name.downcase.tr(" ", "_")).pick(:id)
     end
   end
 

app/models/tag.rb
@@ -117,17 +117,9 @@ class Tag < ApplicationRecord
         @category_mapping ||= CategoryMapping.new
       end
 
-      def select_category_for(tag_name)
-        select_value_sql("SELECT category FROM tags WHERE name = ?", tag_name).to_i
-      end
-
-      def category_for(tag_name, disable_cache: false)
-        if disable_cache
-          select_category_for(tag_name)
-        else
-          Cache.fetch("tc:#{tag_name}") do
-            select_category_for(tag_name)
-          end
+      def category_for(tag_name)
+        Cache.fetch("tc:#{tag_name}") do
+          Tag.where(name: tag_name).pick(:category)
         end
       end
 

app/models/application_record.rb
@@ -172,16 +172,6 @@ class ApplicationRecord < ActiveRecord::Base
         connection.execute("SET STATEMENT_TIMEOUT = #{CurrentUser.user.try(:statement_timeout) || 3_000}") unless Rails.env == "test"
       end
     end
-
-    %w(execute select_value select_values select_all).each do |method_name|
-      define_method("#{method_name}_sql") do |sql, *params|
-        self.class.connection.__send__(method_name, self.class.send(:sanitize_sql_array, [sql, *params]))
-      end
-
-      self.class.__send__(:define_method, "#{method_name}_sql") do |sql, *params|
-        connection.__send__(method_name, send(:sanitize_sql_array, [sql, *params]))
-      end
-    end
   end
 
   concerning :SimpleVersioningMethods do

app/models/note.rb
@@ -110,12 +110,9 @@ class Note < ApplicationRecord
   end
 
   def update_post
-    if self.saved_changes?
-      if Note.where(:is_active => true, :post_id => post_id).exists?
-        execute_sql("UPDATE posts SET last_noted_at = ? WHERE id = ?", updated_at, post_id)
-      else
-        execute_sql("UPDATE posts SET last_noted_at = NULL WHERE id = ?", post_id)
-      end
+    if saved_changes?
+      post_noted_at = Note.where(is_active: true, post_id: post_id).exists? ? updated_at : nil
+      Post.where(id: post_id).update_all(last_noted_at: post_noted_at)
       post.reload.update_index
     end
   end

app/models/tag_alias.rb
@@ -314,7 +314,7 @@ class TagAlias < TagRelationship
 
   def self.update_cached_post_counts_for_all
     TagAlias.without_timeout do
-      execute_sql("UPDATE tag_aliases SET post_count = tags.post_count FROM tags WHERE tags.name = tag_aliases.consequent_name")
+      connection.execute("UPDATE tag_aliases SET post_count = tags.post_count FROM tags WHERE tags.name = tag_aliases.consequent_name")
     end
   end
 

app/models/user.rb
@@ -147,7 +147,7 @@ class User < ApplicationRecord
       def name_to_id(name)
         normalized_name = normalize_name(name)
         Cache.fetch("uni:#{normalized_name}", expires_in: 4.hours) do
-          select_value_sql("SELECT id FROM users WHERE lower(name) = ?", normalized_name)&.to_i
+          User.where("lower(name) = ?", normalized_name).pick(:id)
         end
       end
 
@@ -171,7 +171,7 @@ class User < ApplicationRecord
           return RequestStore[:id_name_cache][user_id]
         end
         name = Cache.fetch("uin:#{user_id}", expires_in: 4.hours) do
-          select_value_sql("SELECT name FROM users WHERE id = ?", user_id) || Danbooru.config.default_guest_name
+          User.where(id: user_id).pick(:name) || Danbooru.config.default_guest_name
         end
         RequestStore[:id_name_cache][user_id] = name
         name

commit 12e996dc083f38768d476be7494423423e9dfc8d

[Tags] Fix category for non-existant tags
Regressed in 4cd32c5f874b4bd8bb810c5864edb9a5eaee7bb5
Caching nil as a category can cause problems in a bunch of places

app/models/pool.rb
@@ -129,7 +129,7 @@ class Pool < ApplicationRecord
     if name =~ /\A\d+\z/
       name.to_i
     else
-      Pool.where("lower(name) = ?", name.downcase.tr(" ", "_")).pick(:id)
+      Pool.where("lower(name) = ?", name.downcase.tr(" ", "_")).pick(:id).to_i
     end
   end
 

app/models/post_set.rb
@@ -42,7 +42,7 @@ class PostSet < ApplicationRecord
     if name =~ /\A\d+\z/
       name.to_i
     else
-      PostSet.where("lower(shortname) = ?", name.downcase.tr(" ", "_")).pick(:id)
+      PostSet.where("lower(shortname) = ?", name.downcase.tr(" ", "_")).pick(:id).to_i
     end
   end
 

app/models/tag.rb
@@ -119,7 +119,7 @@ class Tag < ApplicationRecord
 
       def category_for(tag_name)
         Cache.fetch("tc:#{tag_name}") do
-          Tag.where(name: tag_name).pick(:category)
+          Tag.where(name: tag_name).pick(:category).to_i
         end
       end
 

test/unit/tag_test.rb
@@ -17,6 +17,10 @@ class TagTest < ActiveSupport::TestCase
       assert_equal(Tag.categories.artist, Tag.category_for("!@ab"))
     end
 
+    should "return general for a tag that doesn't exist" do
+      assert_equal(Tag.categories.general, Tag.category_for("missing"))
+    end
+
     should "fetch for multiple tags" do
       create(:artist_tag, name: "aaa")
       create(:copyright_tag, name: "bbb")