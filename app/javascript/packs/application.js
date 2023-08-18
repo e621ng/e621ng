a188650867ddc6b6e0b40b6bfb7bef8096c31c6b
Earlopain <14981592+Earlopain@users.noreply.github.com>
2023-08-18 20:55:57 +0200

    [Cleanup] Remove dead code for related tags
    Vue takes care of that.

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -44,7 +44,6 @@ export { default as PostDeletion } from "../src/javascripts/post_delete.js";
 export { default as PostModeMenu } from '../src/javascripts/post_mode_menu.js';
 export { default as PostReplacement } from '../src/javascripts/post_replacement.js';
 export { default as PostVersions } from '../src/javascripts/post_versions.js';
-export { default as RelatedTag } from '../src/javascripts/related_tag.js';
 export { default as Replacer } from '../src/javascripts/replacer.js';
 export { default as Shortcuts } from '../src/javascripts/shortcuts.js';
 export { default as Utility } from '../src/javascripts/utility.js';
--- app/javascript/src/javascripts/related_tag.js
+++ app/javascript/src/javascripts/related_tag.js
@@ -1,25 +1,7 @@
-import Utility from './utility';
-import Post from './posts';
+const RelatedTag = {};
 
-let RelatedTag = {};
-
-RelatedTag.initialize_all = function() {
-  $(document).on("click.danbooru", ".related-tags-button", RelatedTag.on_click_related_tags_button);
-  $(document).on("click.danbooru", ".related-tags a.search-tag", RelatedTag.toggle_tag);
-  $(document).on("click.danbooru", "#show-related-tags-link", RelatedTag.show);
-  $(document).on("click.danbooru", "#hide-related-tags-link", RelatedTag.hide);
-  $(document).on("keyup.danbooru.relatedTags", "#post_tag_string", RelatedTag.update_selected);
-
-  $(document).on("danbooru:open-post-edit-dialog", RelatedTag.hide);
-  $(document).on("danbooru:close-post-edit-dialog", RelatedTag.show);
-
-  // Show the related tags automatically when the "Edit" tab is opened, or by default on the uploads page.
-  $(document).on("danbooru:open-post-edit-tab", RelatedTag.show);
-  $(document).on("danbooru:open-post-edit-tab", RelatedTag.init_post_show_editor);
-}
-
-import TagEditor from './tag_editor.vue';
-import { createApp } from 'vue';
+import TagEditor from "./tag_editor.vue";
+import { createApp } from "vue";
 
 RelatedTag.tag_editor_setup = false;
 RelatedTag.init_post_show_editor = function() {
@@ -28,118 +10,9 @@ RelatedTag.init_post_show_editor = function() {
   RelatedTag.tag_editor_setup = true;
 
   const app = createApp(TagEditor);
-  app.mount('#tag-string-editor');
-}
-
-RelatedTag.on_click_related_tags_button = function (event) {
-  RelatedTag.show();
-}
-
-RelatedTag.current_tag = function() {
-  // 1. abc def |  -> def
-  // 2. abc def|   -> def
-  // 3. abc de|f   -> def
-  // 4. abc |def   -> def
-  // 5. abc| def   -> abc
-  // 6. ab|c def   -> abc
-  // 7. |abc def   -> abc
-  // 8. | abc def  -> abc
-
-  var $field = $("#post_tag_string");
-  var string = $field.val();
-  var n = string.length;
-  var a = $field.prop('selectionStart');
-  var b = $field.prop('selectionStart');
-
-  if ((a > 0) && (a < (n - 1)) && (!/\s/.test(string[a])) && (/\s/.test(string[a - 1]))) {
-    // 4 is the only case where we need to scan forward. in all other cases we
-    // can drag a backwards, and then drag b forwards.
-
-    while ((b < n) && (!/\s/.test(string[b]))) {
-      b++;
-    }
-  } else if (string.search(/\S/) > b) { // case 8
-    b = string.search(/\S/);
-    while ((b < n) && (!/\s/.test(string[b]))) {
-      b++;
-    }
-  } else {
-    while ((a > 0) && ((/\s/.test(string[a])) || (string[a] === undefined))) {
-      a--;
-      b--;
-    }
-
-    while ((a > 0) && (!/\s/.test(string[a - 1]))) {
-      a--;
-      b--;
-    }
-
-    while ((b < (n - 1)) && (!/\s/.test(string[b]))) {
-      b++;
-    }
-  }
-
-  b++;
-  return string.slice(a, b);
-}
-
-RelatedTag.update_selected = function(e) {
-  var current_tags = $("#post_tag_string").val().toLowerCase().match(/\S+/g) || [];
-  var $all_tags = $(".related-tags a.search-tag");
-  $all_tags.removeClass("selected");
-
-  $all_tags.each(function(i, tag) {
-    if (current_tags.indexOf(tag.textContent.replace(/ /g, "_")) > -1) {
-      $(tag).addClass("selected");
-    }
-  });
-}
-
-RelatedTag.tags_include = function(name) {
-  var current = $("#post_tag_string").val().toLowerCase().match(/\S+/g) || [];
-  return $.inArray(name.toLowerCase(), current) > -1;
-}
-
-RelatedTag.toggle_tag = function(e) {
-  var $field = $("#post_tag_string");
-  var tag = $(e.target).html().replace(/ /g, "_").replace(/&gt;/g, ">").replace(/&lt;/g, "<").replace(/&amp;/g, "&");
-
-  if (RelatedTag.tags_include(tag)) {
-    var escaped_tag = Utility.regexp_escape(tag);
-    $field.val($field.val().replace(new RegExp("(^|\\s)" + escaped_tag + "($|\\s)", "gi"), "$1$2"));
-  } else {
-    $field.val($field.val() + " " + tag);
-  }
-  $field.val($field.val().trim().replace(/ +/g, " ") + " ");
-
-  RelatedTag.update_selected();
-
-  // The timeout is needed on Chrome since it will clobber the field attribute otherwise
-  setTimeout(function () { $field.prop('selectionStart', $field.val().length);}, 100);
-  e.preventDefault();
-
-  Post.update_tag_count({ target: $field });
-}
-
-RelatedTag.show = function(e) {
-  $(document).trigger("danbooru:show-related-tags");
-  $("#related-tags-container").removeClass("hidden").addClass("visible");
-
-  if (e) {
-    e.preventDefault();
-  }
-}
-
-RelatedTag.hide = function(e) {
-  $("#related-tags-container").removeClass("visible").addClass("hidden");
-
-  if (e) {
-    e.preventDefault();
-  }
+  app.mount("#tag-string-editor");
 }
 
 $(function() {
-  RelatedTag.initialize_all();
+  $(document).on("danbooru:open-post-edit-tab", RelatedTag.init_post_show_editor);
 });
-
-export default RelatedTag
