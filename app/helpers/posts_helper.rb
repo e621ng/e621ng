ac0f2e9a4031d36f3fbcd93ad1670c7f859bd043
Earlopain <14981592+Earlopain@users.noreply.github.com>
2023-09-19 21:10:18 +0200

    [Posts] Fix visual inconsistency on the parent message
    Same as https://github.com/e621ng/e621ng/commit/cd50549bbfd40132c35d633b04645135f30d260f

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -50,7 +50,7 @@ module PostsHelper
 
     html << " (#{link_to("learn more", wiki_pages_path(:title => "e621:post_relationships"))}) "
 
-    html << link_to("» show", "#", id: "has-parent-relationship-preview-link")
+    html << link_to("show »", "#", id: "has-parent-relationship-preview-link")
 
     html.html_safe
   end
