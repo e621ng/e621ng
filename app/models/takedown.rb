e621ng/e621ng

commit b4e16fa0fde17a78f89ec1df4d9d170ac75e6fc3

[Elasticsearch] Implement Response

app/models/takedown.rb
@@ -96,7 +96,7 @@ class Takedown < ApplicationRecord
     end
 
     def add_posts_by_tags!(tag_string)
-      new_ids = Post.tag_match_system("#{tag_string} -status:deleted").limit(1000).results.map(&:id)
+      new_ids = Post.tag_match_system("#{tag_string} -status:deleted").limit(1000).pluck(&:id)
       add_posts_by_ids!(new_ids.join(" "))
     end
 

app/controllers/admin/reowner_controller.rb
@@ -17,7 +17,7 @@ module Admin
       end
 
       moved_post_ids = []
-      Post.tag_match("user:!#{@old_user.id} #{query}").limit(300).records.each do |p|
+      Post.tag_match("user:!#{@old_user.id} #{query}").limit(300).each do |p|
         moved_post_ids << p.id
         p.do_not_version_changes = true
         p.update({ uploader_id: @new_user.id })

app/controllers/admin/stuck_dnp_controller.rb
@@ -16,7 +16,7 @@ module Admin
 
       dnp_tags = %w[avoid_posting conditional_dnp]
       post_ids = []
-      Post.tag_match_system("#{query} ~avoid_posting ~conditional_dnp").limit(1000).records.each do |p|
+      Post.tag_match_system("#{query} ~avoid_posting ~conditional_dnp").limit(1000).each do |p|
         previous_tags = p.fetch_tags(*dnp_tags)
 
         p.do_not_version_changes = true

app/controllers/posts_controller.rb
@@ -89,7 +89,7 @@ class PostsController < ApplicationController
 
   def random
     tags = params[:tags] || ''
-    @post = Post.tag_match(tags + " order:random").limit(1).records[0]
+    @post = Post.tag_match(tags + " order:random").limit(1).first
     raise ActiveRecord::RecordNotFound if @post.nil?
     respond_with(@post) do |format|
       format.html { redirect_to post_path(@post, :tags => params[:tags]) }

app/logical/danbooru/paginator/document_store_extensions.rb
@@ -57,7 +57,7 @@ module Danbooru
       end
     end
 
-    module ElasticsearchExtensions
+    module DocumentStoreExtensions
       include BaseExtension
 
       def paginate(page, options)
@@ -71,7 +71,7 @@ module Danbooru
           current_page: current_page,
         }
 
-        PaginatedArray.new(records(includes: options[:includes]).to_a, new_opts)
+        PaginatedArray.new(records(includes: options[:includes]), new_opts)
       end
 
       def paginate_numbered

app/logical/document_store/model.rb
@@ -36,7 +36,7 @@ module DocumentStore
 
       def document_store_search(body)
         search = SearchRequest.new({ index: __elasticsearch__.index_name, body: body }, document_store_client)
-        Elasticsearch::Model::Response::Response.new(self, search)
+        Response.new(self, search)
       end
 
       def document_store_create_index!(delete_existing: false)

app/logical/document_store/response.rb
@@ -1,0 +1,33 @@
+module DocumentStore
+  class Response
+    include Danbooru::Paginator::DocumentStoreExtensions
+
+    delegate_missing_to :records
+    attr_reader :klass, :search
+
+    def initialize(klass, search)
+      @klass = klass
+      @search = search
+    end
+
+    def response
+      @response ||= @search.execute!
+    end
+
+    def hits
+      response["hits"]["hits"]
+    end
+
+    def relation
+      klass.where(id: hits.pluck("_id"))
+    end
+
+    def records(includes: nil)
+      @records ||= begin
+        sql_records = relation
+        sql_records = sql_records.includes(includes) if includes
+        sql_records.records.sort_by { |sql_record| hits.index { |hit| hit["_id"] == sql_record.id.to_s } }
+      end
+    end
+  end
+end

app/models/artist.rb
@@ -240,7 +240,7 @@ class Artist < ApplicationRecord
       Cache.fetch("artist-domains-#{id}", expires_in: 1.day) do
         re = /\.(png|jpeg|jpg|webm|mp4)$/m
         counted = Hash.new(0)
-        sources = Post.tag_match(name, resolve_aliases: false).limit(100).records.pluck(:source).each do |source_string|
+        sources = Post.tag_match(name, resolve_aliases: false).limit(100).pluck(:source).each do |source_string|
           sources = source_string.split("\n")
           # try to filter out direct file urls
           domains = sources.filter {|s| !re.match?(s) }.map do |x|

commit 81ab93b7eec989ee7fb432960e9548522b482982

[Takedowns] Fix adding by tag search
Broken with https://github.com/e621ng/e621ng/commit/b4e16fa0fde17a78f89ec1df4d9d170ac75e6fc3

app/models/takedown.rb
@@ -96,7 +96,7 @@ class Takedown < ApplicationRecord
     end
 
     def add_posts_by_tags!(tag_string)
-      new_ids = Post.tag_match_system("#{tag_string} -status:deleted").limit(1000).pluck(&:id)
+      new_ids = Post.tag_match_system("#{tag_string} -status:deleted").limit(1000).pluck(:id)
       add_posts_by_ids!(new_ids.join(" "))
     end
 