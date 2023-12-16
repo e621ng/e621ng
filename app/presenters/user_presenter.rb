e8dfa69a813897feade2476417c14dd5643c8544
S87GMIL <S.Miller87@gmx.net>
2023-12-16 17:03:32 +0100

    Added the count of replaced posts to the user page

--- app/presenters/user_presenter.rb
+++ app/presenters/user_presenter.rb
@@ -88,6 +88,10 @@ class UserPresenter
     template.link_to(user.post_replacement_rejected_count, template.post_replacements_path(search: { creator_id: user.id, status: "rejected" }))
   end
 
+  def approved_replacements_count(template)
+    template.link_to(PostReplacement.approved.count, template.post_replacements_path(search: { creator_id: user.id, status: "approved" }))
+  end
+
   def favorite_count(template)
     template.link_to(user.favorite_count, template.favorites_path(user_id: user.id))
   end
