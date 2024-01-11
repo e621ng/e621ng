3482c01b824fb7912cd606ef78b71fa9d84460b0
Sindrake <cinder.wolf@pm.me>
2024-01-11 06:09:57 -0800

    [Tickets] Add warnings to the comment report form

--- app/helpers/ticket_helper.rb
+++ app/helpers/ticket_helper.rb
@@ -9,4 +9,15 @@ module TicketHelper
       status.titleize
     end
   end
+
+  def generate_comment_warnings(comment)
+    warnings = []
+    if comment.creator.is_banned? && comment.creator.recent_ban.expires_at.nil?
+      warnings << "The creator of this comment is already permanently banned"
+    end
+    warnings << "The creator of this comment already received a #{comment.warning_type} for its contents" if comment.warning_type
+    warnings << "The reported comment is older than 6 months" if comment.updated_at < 6.months.ago
+
+    warnings
+  end
 end
