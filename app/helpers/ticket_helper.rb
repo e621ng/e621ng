f5aa020ad34acb093efe29cb082d7cbd95dad6b2
Cinder <132787557+Sindrake@users.noreply.github.com>
2024-01-21 11:10:43 -0800

    [Tickets] Add warnings to the comment report form (#588)
    * [Tickets] Add warnings to the comment report form
    
    * [Tickets] Add warnings to blip and forum reports
    
    * Indent

--- app/helpers/ticket_helper.rb
+++ app/helpers/ticket_helper.rb
@@ -9,4 +9,15 @@ module TicketHelper
       status.titleize
     end
   end
+
+  def generate_content_warnings(message)
+    warnings = []
+    if message.creator.is_banned? && message.creator.recent_ban.expires_at.nil?
+      warnings << "The creator of this message is already permanently banned."
+    end
+    warnings << "The creator of this message already received a #{message.warning_type} for its contents." if message.warning_type
+    warnings << "The reported message is older than 6 months." if message.updated_at < 6.months.ago
+
+    warnings
+  end
 end
