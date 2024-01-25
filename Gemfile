a7228612a2e05278dad196080567c9fbd9f086da
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-01-25 22:05:29 +0100

    [Rails] Update to 7.1
    Run `app:update`, fix the one deprecation warning

--- Gemfile
+++ Gemfile
@@ -2,7 +2,7 @@ source 'https://rubygems.org/'
 
 gem 'dotenv-rails', require: 'dotenv/rails-now'
 
-gem "rails", "~> 7.0.0"
+gem "rails", "~> 7.1.0"
 gem "pg"
 gem "dalli", :platforms => :ruby
 gem "simple_form"
--- config/application.rb
+++ config/application.rb
@@ -25,6 +25,12 @@ module Danbooru
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+
+    # Please, add to the `ignore` list any other `lib` subdirectories that do
+    # not contain `.rb` files, or that should not be reloaded or eager loaded.
+    # Common ones are `templates`, `generators`, or `middleware`, for example.
+    # config.autoload_lib(ignore: %w(assets tasks))
+
     config.active_record.schema_format = :sql
     config.log_tags = [->(req) {"PID:#{Process.pid}"}]
     config.action_controller.action_on_unpermitted_parameters = :raise
