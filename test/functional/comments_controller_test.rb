e621ng/e621ng

commit 2c832dc633c4c0b0cf08dff93967a21c8671f580

[Comments] Show hidden sticky comments to Janitors

app/models/comment.rb
@@ -37,6 +37,8 @@ class Comment < ApplicationRecord
     def hidden(user)
       if user.is_moderator?
         where("not(score >= ? or is_sticky = true)", user.comment_threshold)
+      elsif user.is_janitor?
+        where("not((score >= ? or is_sticky = true) and (is_sticky = true or is_hidden = false or creator_id = ?))", user.comment_threshold, user.id)
       else
         where("not((score >= ? or is_sticky = true) and (is_hidden = false or creator_id = ?))", user.comment_threshold, user.id)
       end
@@ -45,6 +47,8 @@ class Comment < ApplicationRecord
     def visible(user)
       if user.is_moderator?
         where("score >= ? or is_sticky = true", user.comment_threshold)
+      elsif user.is_janitor?
+        where("(score >= ? or is_sticky = true) and (is_sticky = true or is_hidden = false or creator_id = ?)", user.comment_threshold, user.id)
       else
         where("(score >= ? or is_sticky = true) and (is_hidden = false or creator_id = ?)", user.comment_threshold, user.id)
       end
@@ -198,6 +202,7 @@ class Comment < ApplicationRecord
 
   def visible_to?(user)
     return true if user.is_moderator?
+    return true if user.is_janitor? && is_sticky?
     return true if is_hidden? == false
     creator_id == user.id # Can always see your own comments, even if hidden.
   end

app/controllers/comments_controller.rb
@@ -105,8 +105,7 @@ private
   end
 
   def index_by_comment
-    @comments = Comment
-    @comments = @comments.undeleted unless CurrentUser.is_moderator?
+    @comments = Comment.visible(CurrentUser.user)
     @comments = @comments.search(search_params).paginate(params[:page], :limit => params[:limit], :search_count => params[:search])
     @comment_votes = CommentVote.for_comments_and_user(@comments.map(&:id), CurrentUser.id)
     respond_with(@comments)

app/views/comments/_index_by_comment.html.erb
@@ -1,6 +1,6 @@
 <div id="p-index-by-comment" class="comments-for-post">
     <% @comments.each do |comment| %>
-      <% if comment.post.present? && (CurrentUser.is_moderator? || !comment.is_hidden?) %>
+      <% if comment.post.present? && comment.visible_to?(CurrentUser.user) %>
       <div class="comment-post">
         <div class="post-container">
           <%= PostPresenter.preview(comment.post, inline: true, show_deleted: true) %>

app/views/comments/_index_by_post.html.erb
@@ -10,7 +10,7 @@
   <% end %>
 
   <% @posts.select {|x| x.visible?}.each do |post| %>
-    <% if CurrentUser.is_moderator? || post.comments.undeleted.exists? %>
+    <% if post.comments.visible(CurrentUser.user).exists? %>
       <div class="comment-post">
         <div class="post-container">
           <%= PostPresenter.preview(post, inline: true, show_deleted: true) %>

commit 0fad5d56598431ac2cd88def8470f188203f93c3

[Comments] Fix exception when searching by poster_id
Caused by https://github.com/e621ng/e621ng/commit/2c832dc633c4c0b0cf08dff93967a21c8671f580
poster_id requires a join and these columns weren't fully qualified.

app/models/comment.rb
@@ -36,21 +36,21 @@ class Comment < ApplicationRecord
 
     def hidden(user)
       if user.is_moderator?
-        where("not(score >= ? or is_sticky = true)", user.comment_threshold)
+        where("not(comments.score >= ? or comments.is_sticky = true)", user.comment_threshold)
       elsif user.is_janitor?
-        where("not((score >= ? or is_sticky = true) and (is_sticky = true or is_hidden = false or creator_id = ?))", user.comment_threshold, user.id)
+        where("not((comments.score >= ? or comments.is_sticky = true) and (comments.is_sticky = true or comments.is_hidden = false or comments.creator_id = ?))", user.comment_threshold, user.id)
       else
-        where("not((score >= ? or is_sticky = true) and (is_hidden = false or creator_id = ?))", user.comment_threshold, user.id)
+        where("not((comments.score >= ? or comments.is_sticky = true) and (comments.is_hidden = false or comments.creator_id = ?))", user.comment_threshold, user.id)
       end
     end
 
     def visible(user)
       if user.is_moderator?
-        where("score >= ? or is_sticky = true", user.comment_threshold)
+        where("comments.score >= ? or comments.is_sticky = true", user.comment_threshold)
       elsif user.is_janitor?
-        where("(score >= ? or is_sticky = true) and (is_sticky = true or is_hidden = false or creator_id = ?)", user.comment_threshold, user.id)
+        where("(comments.score >= ? or comments.is_sticky = true) and (comments.is_sticky = true or comments.is_hidden = false or comments.creator_id = ?)", user.comment_threshold, user.id)
       else
-        where("(score >= ? or is_sticky = true) and (is_hidden = false or creator_id = ?)", user.comment_threshold, user.id)
+        where("(comments.score >= ? or comments.is_sticky = true) and (comments.is_hidden = false or comments.creator_id = ?)", user.comment_threshold, user.id)
       end
     end
 

test/functional/comments_controller_test.rb
@@ -29,6 +29,11 @@ class CommentsControllerTest < ActionDispatch::IntegrationTest
         get comments_path(group_by: "comment")
         assert_response :success
       end
+
+      should "render for the poster_id search parameter" do
+        get comments_path(group_by: "comment", search: { poster_id: 123 })
+        assert_response :success
+      end
     end
 
     context "search action" do