16de61c044fdf8a675835439fab2dc6a7459e66f
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-02-15 20:11:44 +0100

    [Help] Sort entries by their displayed title

--- app/models/help_page.rb
+++ app/models/help_page.rb
@@ -32,6 +32,6 @@ class HelpPage < ApplicationRecord
   end
 
   def self.help_index
-    Cache.fetch("help_index", expires_in: 12.hours) { HelpPage.order(:name).to_a }
+    Cache.fetch("help_index", expires_in: 12.hours) { HelpPage.all.sort_by(&:pretty_title) }
   end
 end
