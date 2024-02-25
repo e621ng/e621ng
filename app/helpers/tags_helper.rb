9b59c860ff7abf09c0eaa50703518482aed44168
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-02-25 15:22:33 +0100

    [Cleanup] Add helper method for show_or_new_wiki_pages_path

--- app/helpers/tags_helper.rb
+++ app/helpers/tags_helper.rb
@@ -6,25 +6,25 @@ module TagsHelper
 
     if tag.antecedent_alias
       html << "<p class='hint'>This tag has been aliased to "
-      html << link_to(tag.antecedent_alias.consequent_name, show_or_new_wiki_pages_path(:title => tag.antecedent_alias.consequent_name))
+      html << link_to_wiki_or_new(tag.antecedent_alias.consequent_name)
       html << " (#{link_to "learn more", wiki_pages_path(title: "e621:tag_aliases")}).</p>"
     end
 
     if tag.consequent_aliases.present?
       html << "<p class='hint'>The following tags are aliased to this tag: "
-      html << raw(tag.consequent_aliases.map {|x| link_to(x.antecedent_name, show_or_new_wiki_pages_path(:title => x.antecedent_name))}.join(", "))
+      html << raw(tag.consequent_aliases.map {|x| link_to_wiki_or_new(x.antecedent_name) }.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "e621:tag_aliases")}).</p>"
     end
 
     if tag.antecedent_implications.present?
       html << "<p class='hint'>This tag implicates "
-      html << raw(tag.antecedent_implications.map {|x| link_to(x.consequent_name, show_or_new_wiki_pages_path(:title => x.consequent_name))}.join(", "))
+      html << raw(tag.antecedent_implications.map {|x| link_to_wiki_or_new(x.consequent_name) }.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "e621:tag_implications")}).</p>"
     end
 
     if tag.consequent_implications.present?
       html << "<p class='hint'>The following tags implicate this tag: "
-      html << raw(tag.consequent_implications.map {|x| link_to(x.antecedent_name, show_or_new_wiki_pages_path(:title => x.antecedent_name))}.join(", "))
+      html << raw(tag.consequent_implications.map {|x| link_to_wiki_or_new(x.antecedent_name) }.join(", "))
       html << " (#{link_to "learn more", wiki_pages_path(title: "e621:tag_implications")}).</p>"
     end
 
--- app/helpers/wiki_pages_helper.rb
+++ app/helpers/wiki_pages_helper.rb
@@ -1,4 +1,8 @@
 module WikiPagesHelper
+  def link_to_wiki_or_new(text, tag = text)
+    link_to(text, show_or_new_wiki_pages_path(title: tag))
+  end
+
   def wiki_page_alias_and_implication_list(wiki_page)
     alias_and_implication_list(wiki_page.tag || Tag.new({name: wiki_page.title}))
   end
