e2d289baca026ff6ec9fb8d99538daf6ace45b44
Earlopain <14981592+Earlopain@users.noreply.github.com>
2024-03-31 13:41:13 +0200

    [Misc] Add this years april fools joke
    I'm so sorry

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -44,12 +44,55 @@ module ApplicationHelper
   end
 
   def format_text(text, **options)
+    something_funny = options[:fool] && fool?
+    options = options.except(:fool)
     # preserve the currrent inline behaviour
     if options[:inline]
       dtext_ragel(text, **options)
     else
-      raw %(<div class="styled-dtext">#{dtext_ragel(text, **options)}</div>)
+      result = %(<div class="styled-dtext">#{dtext_ragel(text, **options)}</div>)
+      if something_funny
+        raw uwuify(result)
+      else
+        raw result
+      end
     end
+  end
+
+  def fool?
+    return true if params[:fool]
+
+    Time.use_zone(Time.find_zone("UTC")) do
+      target = Date.new(2024, 4, 1)
+      start = target.beginning_of_day - 12.hours
+      stop = target.end_of_day + 12.hours
+      Time.now.between?(start, stop)
+    end
+  end
+
+  FACE_CHANCE = 0.25
+  FACE = [" (・`ω´・) ", " ;;w;; ", " owo ", " UwU ", " >w< ", " ^w^ "].freeze
+
+  def uwuify(html_input)
+    fragment = Nokogiri::HTML5.fragment(html_input)
+
+    fragment.traverse do |x|
+      if x.text?
+        text = x.content.tr("rlRL", "wwWW")
+        text = text.gsub(/(n)([aeiou])/i, '\1y\2')
+        text = text.gsub("ove", "uv")
+        text = text.gsub(/(\.+)/) do
+          if Random.rand < FACE_CHANCE
+            FACE.sample
+          else
+            $1
+          end
+        end
+        x.content = text
+      end
+    end
+
+    fragment.to_html
   end
 
   def custom_form_for(object, *args, &)
