e621ng/e621ng

commit af0036db6bdb2287766590a1cf76e7afb9dbc51b

[Forums] Clean up permission checks (#674)

app/controllers/forum_topics_controller.rb
@@ -25,9 +25,8 @@ class ForumTopicsController < ApplicationController
     params[:search] ||= {}
     params[:search][:order] ||= "sticky" if request.format == Mime::Type.lookup("text/html")
 
-    @query = ForumTopic.permitted.active.search(search_params)
-    @query = ForumTopic.permitted.search(search_params) if CurrentUser.is_moderator?
-    @forum_topics = @query.paginate(params[:page], :limit => per_page, :search_count => params[:search])
+    @query = ForumTopic.visible(CurrentUser.user).search(search_params)
+    @forum_topics = @query.paginate(params[:page], limit: per_page, search_count: params[:search])
 
     respond_with(@forum_topics) do |format|
       format.html do

app/models/forum_post.rb
@@ -46,19 +46,18 @@ class ForumPost < ApplicationRecord
       where("forum_posts.creator_id = ?", user_id)
     end
 
-    def active
-      where("(forum_posts.is_hidden = false or forum_posts.creator_id = ?)", CurrentUser.id)
-    end
-
-    def permitted
-      q = joins(:topic)
-      q = q.where("(forum_topics.is_hidden = false or forum_posts.creator_id = ?)", CurrentUser.id) unless CurrentUser.is_moderator?
+    def visible(user)
+      q = joins(topic: :category).where("forum_categories.can_view <= ?", user.level)
+      unless user.is_moderator?
+        q = q.where("forum_topics.is_hidden = FALSE OR forum_topics.creator_id = ?", user.id)
+        q = q.where("forum_posts.is_hidden = FALSE OR forum_posts.creator_id = ?", user.id)
+      end
       q
     end
 
     def search(params)
       q = super
-      q = q.permitted
+      q = q.visible(CurrentUser.user)
 
       q = q.where_user(:creator_id, :creator, params)
 

app/controllers/forum_posts_controller.rb
@@ -20,9 +20,8 @@ class ForumPostsController < ApplicationController
   end
 
   def index
-    @query = ForumPost.permitted.active.search(search_params)
-    @query = ForumPost.permitted.search(search_params) if CurrentUser.is_moderator?
-    @forum_posts = @query.includes(:topic).paginate(params[:page], :limit => params[:limit], :search_count => params[:search])
+    @query = ForumPost.visible(CurrentUser.user).search(search_params)
+    @forum_posts = @query.includes(:topic).paginate(params[:page], limit: params[:limit], search_count: params[:search])
     respond_with(@forum_posts)
   end
 

app/models/forum_topic.rb
@@ -53,12 +53,10 @@ class ForumTopic < ApplicationRecord
   end
 
   module SearchMethods
-    def active
-      where("(forum_topics.is_hidden = false or forum_topics.creator_id = ?)", CurrentUser.id)
-    end
-
-    def permitted
-      joins(:category).where('forum_categories.can_view <= ?', CurrentUser.level)
+    def visible(user)
+      q = joins(:category).where("forum_categories.can_view <= ?", user.level)
+      q = q.where("forum_topics.is_hidden = FALSE OR forum_topics.creator_id = ?", user.id) unless user.is_moderator?
+      q
     end
 
     def sticky_first
@@ -71,7 +69,7 @@ class ForumTopic < ApplicationRecord
 
     def search(params)
       q = super
-      q = q.permitted
+      q = q.visible(CurrentUser.user)
 
       q = q.attribute_matches(:title, params[:title_matches])
 
@@ -119,7 +117,7 @@ class ForumTopic < ApplicationRecord
         ForumTopicVisit.create(:user_id => user.id, :forum_topic_id => id, :last_read_at => updated_at)
       end
 
-      has_unread_topics = ForumTopic.permitted.active.where("forum_topics.updated_at >= ?", user.last_forum_read_at)
+      has_unread_topics = ForumTopic.visible(user).where("forum_topics.updated_at >= ?", user.last_forum_read_at)
       .joins("left join forum_topic_visits on (forum_topic_visits.forum_topic_id = forum_topics.id and forum_topic_visits.user_id = #{user.id})")
       .where("(forum_topic_visits.id is null or forum_topic_visits.last_read_at < forum_topics.updated_at)")
       .exists?

app/models/user.rb
@@ -385,7 +385,7 @@ class User < ApplicationRecord
   module ForumMethods
     def has_forum_been_updated?
       return false unless is_member?
-      max_updated_at = ForumTopic.permitted.active.order(updated_at: :desc).first&.updated_at
+      max_updated_at = ForumTopic.visible(self).order(updated_at: :desc).first&.updated_at
       return false if max_updated_at.nil?
       return true if last_forum_read_at.nil?
       return max_updated_at > last_forum_read_at

app/views/forum_posts/index.html.erb
@@ -12,18 +12,16 @@
       </thead>
       <tbody>
         <% @forum_posts.each do |forum_post| %>
-          <% if forum_post.visible?(CurrentUser.user) %>
-            <tr id="forum-post-<%= forum_post.id %>" data-topic-is-hidden="<%= forum_post.topic.is_hidden? %>" data-is-hidden="<%= forum_post.is_hidden? %>">
-              <td class="forum-post-topic-title">
-                <%= link_to forum_post.topic.title, forum_topic_path(forum_post.topic) %>
-              </td>
-              <td class="forum-post-excerpt">
-                <%= link_to truncate(forum_post.body, :length => 50), forum_post_path(forum_post) %>
-              </td>
-              <td><%= link_to_user forum_post.creator %></td>
-              <td><%= time_ago_in_words_tagged forum_post.created_at %></td>
-            </tr>
-          <% end %>
+          <tr id="forum-post-<%= forum_post.id %>" data-topic-is-hidden="<%= forum_post.topic.is_hidden? %>" data-is-hidden="<%= forum_post.is_hidden? %>">
+            <td class="forum-post-topic-title">
+              <%= link_to forum_post.topic.title, forum_topic_path(forum_post.topic) %>
+            </td>
+            <td class="forum-post-excerpt">
+              <%= link_to truncate(forum_post.body, :length => 50), forum_post_path(forum_post) %>
+            </td>
+            <td><%= link_to_user forum_post.creator %></td>
+            <td><%= time_ago_in_words_tagged forum_post.created_at %></td>
+          </tr>
         <% end %>
       </tbody>
     </table>

test/functional/forum_posts_controller_test.rb
@@ -62,6 +62,27 @@ class ForumPostsControllerTest < ActionDispatch::IntegrationTest
         assert_response :success
       end
 
+      context "with posts in a hidden category" do
+        setup do
+          as(@mod) do
+            @category2 = ForumCategory.create!(name: "test", can_view: @mod.level)
+            @forum_topic = create(:forum_topic, category: @category2, title: "test", original_post_attributes: { body: "test" })
+            @forum_post2 = @forum_topic.original_post
+          end
+        end
+
+        should "only list visible posts" do
+          get forum_posts_path
+          assert_response :success
+          assert_select "#forum-post-#{@forum_post.id}", true
+          assert_select "#forum-post-#{@forum_post2.id}", false
+
+          get forum_posts_path(format: :json)
+          assert_response :success
+          assert_equal([@forum_post.id], @response.parsed_body.pluck("id"))
+        end
+      end
+
       context "with search conditions" do
         should "list all matching forum posts" do
           get forum_posts_path, params: {:search => {:body_matches => "xxx"}}

commit b28bb5c99fe2694d2a8cf2e0c16a8fb448da2843

[ForumTopics] Fix page number mismatch (#675)
Fixes an issue introduced in #674.

app/controllers/forum_topics_controller.rb
@@ -42,7 +42,7 @@ class ForumTopicsController < ApplicationController
     if request.format == Mime::Type.lookup("text/html")
       @forum_topic.mark_as_read!(CurrentUser.user)
     end
-    @forum_posts = ForumPost.includes(topic: [:category]).search(:topic_id => @forum_topic.id).reorder("forum_posts.id").paginate(params[:page])
+    @forum_posts = ForumPost.permitted(CurrentUser.user).includes(topic: [:category]).search(topic_id: @forum_topic.id).reorder("forum_posts.id").paginate(params[:page])
     @original_forum_post_id = @forum_topic.original_post.id
     respond_with(@forum_topic)
   end

app/models/forum_post.rb
@@ -47,18 +47,22 @@ class ForumPost < ApplicationRecord
     end
 
     def visible(user)
+      active(user).permitted(user)
+    end
+
+    def permitted(user)
       q = joins(topic: :category).where("forum_categories.can_view <= ?", user.level)
-      unless user.is_moderator?
-        q = q.where("forum_topics.is_hidden = FALSE OR forum_topics.creator_id = ?", user.id)
-        q = q.where("forum_posts.is_hidden = FALSE OR forum_posts.creator_id = ?", user.id)
-      end
+      q = q.joins(:topic).where("forum_topics.is_hidden = FALSE OR forum_topics.creator_id = ?", user.id) if user.is_moderator?
       q
+    end
+
+    def active(user)
+      return all if user.is_moderator?
+      where("forum_posts.is_hidden = FALSE OR forum_posts.creator_id = ?", user.id)
     end
 
     def search(params)
       q = super
-      q = q.visible(CurrentUser.user)
-
       q = q.where_user(:creator_id, :creator, params)
 
       if params[:topic_id].present?

test/functional/forum_topics_controller_test.rb
@@ -31,6 +31,26 @@ class ForumTopicsControllerTest < ActionDispatch::IntegrationTest
         @user.reload
         assert_nil(@user.last_forum_read_at)
       end
+
+      should "have the correct page number" do
+        Danbooru.config.stubs(:records_per_page).returns(2)
+        assert_equal(1, @forum_topic.last_page)
+        as(@user) { @forum_posts = create_list(:forum_post, 3, topic: @forum_topic) }
+        assert_equal(2, @forum_topic.last_page)
+
+        get_auth forum_topic_path(@forum_topic), @user, params: { page: 2 }
+        assert_select "#forum_post_#{@forum_posts.second.id}"
+        assert_select "#forum_post_#{@forum_posts.third.id}"
+        assert_equal([1, 2, 2], @forum_posts.map(&:forum_topic_page))
+        assert_equal(2, @forum_topic.last_page)
+
+        as(@mod) { @forum_posts.first.hide! }
+        get_auth forum_topic_path(@forum_topic), @user, params: { page: 2 }
+        assert_select "#forum_post_#{@forum_posts.second.id}"
+        assert_select "#forum_post_#{@forum_posts.third.id}"
+        assert_equal([1, 2, 2], @forum_posts.map(&:forum_topic_page))
+        assert_equal(2, @forum_topic.last_page)
+      end
     end
 
     context "index action" do