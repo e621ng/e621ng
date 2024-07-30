cba6355d89f1802491d2b35a2dc1ca9041ecc282
Donovan Daniels <hewwo@yiff.rocks>
2024-07-30 14:26:27 -0500

    Fix static page

--- app/helpers/avoid_posting_helper.rb
+++ app/helpers/avoid_posting_helper.rb
@@ -2,7 +2,7 @@
 
 module AvoidPostingHelper
   def format_avoid_posting_list
-    avoid_postings = AvoidPosting.active.order("artist_name ASC").group_by(&:header)
+    avoid_postings = AvoidPosting.active.joins(:artist).order("artists.name ASC").group_by(&:header)
     text = ""
     avoid_postings.each do |header, entries|
       text += "h2. #{header} [##{anchor(header)}]\n"
