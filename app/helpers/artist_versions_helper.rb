8cf6052f91ad86bc15141ffe7dd927aad0bf15cc
Cinder <cinder@e621.net>
2024-08-02 16:16:23 -0700

    [Misc] Fix the bug on the history page of a destroyed artist

--- app/helpers/artist_versions_helper.rb
+++ app/helpers/artist_versions_helper.rb
@@ -8,7 +8,11 @@ module ArtistVersionsHelper
   def artist_version_other_names_diff(artist_version)
     new_names = artist_version.other_names
     old_names = artist_version.previous.try(:other_names)
-    latest_names = artist_version.artist.other_names
+    if artist_version.artist.present?
+      latest_names = artist_version.artist.other_names
+    else
+      latest_names = new_names
+    end
 
     diff_list_html(new_names, old_names, latest_names)
   end
@@ -16,7 +20,11 @@ module ArtistVersionsHelper
   def artist_version_urls_diff(artist_version)
     new_urls = artist_version.urls
     old_urls = artist_version.previous.try(:urls)
-    latest_urls = artist_version.artist.urls.map(&:to_s)
+    if artist_version.artist.present?
+      latest_urls = artist_version.artist.urls.map(&:to_s)
+    else
+      latest_urls = new_urls
+    end
 
     diff_list_html(new_urls, old_urls, latest_urls)
   end
