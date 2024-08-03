fea7a0c91d2fb6b3a3ba3180a40f854ae507dd52
Donovan Daniels <hewwo@yiff.rocks>
2024-08-02 19:32:04 -0500

    Use implication creator's ip address in fixer

--- db/fixes/118_create_avoid_postings_from_implications.rb
+++ db/fixes/118_create_avoid_postings_from_implications.rb
@@ -6,7 +6,7 @@ require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config",
 CurrentUser.as_system do
   TagImplication.order(created_at: :asc).approved.where(consequent_name: %w[avoid_posting conditional_dnp]).find_each do |implication|
     artist = Artist.find_or_create_by!(name: implication.antecedent_name)
-    dnp = CurrentUser.scoped(implication.creator, "127.0.0.1") do
+    dnp = CurrentUser.scoped(implication.creator, implication.creator_ip_addr) do
       AvoidPosting.create(artist: artist, created_at: implication.created_at, updated_at: implication.created_at)
     end
     if dnp.valid?
