1f63f4b9d50491b2e3e393e83c92fa9a2ef8d319
Cinder <cinder@e621.net>
2024-08-04 15:14:46 -0700

    [Blacklist] Redesign and rework the blacklist (#693)

---
+++ app/javascript/src/javascripts/models/PostCache.js
@@ -0,0 +1,57 @@
+/**
+ * Simple post cache, used to speed up blacklist processing.
+ * Fetches data from the data-attributes of a thumbnail.
+ */
+export default class PostCache {
+  static _cache = {};
+
+  static fromThumbnail ($element) {
+    const id = $element.data("id");
+    if (this._cache[id]) return this._cache[id];
+
+    // As of right now, the code below will take up three
+    // times as long to execute compared to simply fetching
+    // the data from cache. While understandable, it should
+    // still be optimized wherever possible.
+
+    const data = $element[0].dataset; // Faster than $element.data()
+
+    // For some reason, this takes 10x as long on the first post.
+    // But it's still only ~1ms (rather than 0.1ms), so it's fine
+    const tag_string = data.tags || "",
+      tags = tag_string.split(" ");
+
+    const pools = [];
+    for (let value of (data.pools + "").split(" ")) {
+      value = parseInt(value);
+      if (value) pools.push(value);
+    }
+
+    const value = {
+      tag_string: tag_string,
+      tags: tags,
+      tagcount: tags.length,
+
+      id: parseInt(data.id),
+      flags: (data.flags || "").split(" "),
+      rating: data.rating || "",
+      file_ext: data.fileExt || "",
+
+      width: parseInt(data.width) || -1,
+      height: parseInt(data.height) || -1,
+      size: parseInt(data.size) || -1,
+
+      score: parseInt(data.score) || 0,
+      fav_count: parseInt(data.favCount) || 0,
+      is_favorited: !!data.isFavorited,
+
+      uploader: (data.uploader || "").toLowerCase(),
+      uploader_id: parseInt(data.uploaderId) || -1,
+
+      pools: pools,
+    };
+
+    this._cache[id] = value;
+    return value;
+  }
+}
