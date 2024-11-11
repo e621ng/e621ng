cf94b1b827847a2fa6301297b3c0e0d87e1471a4
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2024-11-10 23:22:14 -0500

    [DangerZone] Allow admins to hide pending posts for a specified amount of hours (#760)

--- test/functional/admin/danger_zone_controller_test.rb
+++ test/functional/admin/danger_zone_controller_test.rb
@@ -10,6 +10,7 @@ class Admin::DangerZoneControllerTest < ActionDispatch::IntegrationTest
 
     teardown do
       DangerZone.min_upload_level = User::Levels::MEMBER
+      DangerZone.hide_pending_posts_for = 0
     end
 
     context "index action" do
@@ -23,6 +24,13 @@ class Admin::DangerZoneControllerTest < ActionDispatch::IntegrationTest
       should "work" do
         put_auth uploading_limits_admin_danger_zone_index_path, @admin, params: { uploading_limits: { min_level: User::Levels::PRIVILEGED } }
         assert_equal DangerZone.min_upload_level, User::Levels::PRIVILEGED
+      end
+    end
+
+    context "hide pending posts action" do
+      should "work" do
+        put_auth hide_pending_posts_admin_danger_zone_index_path, @admin, params: { hide_pending_posts: { duration: 24 } }
+        assert_equal DangerZone.hide_pending_posts_for, 24
       end
     end
   end
