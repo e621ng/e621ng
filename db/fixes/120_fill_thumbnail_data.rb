acc16d98bd76df1235defa44a9e4ddce6c61d7bf
Cinder <cinder@e621.net>
2024-12-01 07:20:36 -0800

    Tweaks to the fixer script

--- db/fixes/120_fill_thumbnail_data.rb
+++ db/fixes/120_fill_thumbnail_data.rb
@@ -4,12 +4,21 @@
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 
 CurrentUser.as_system do
-  Post.where(thumbnail: nil).find_each do |post|
-    puts post.id
+  counter = 0
+  Post.find_each do |post|
+    counter += 1
+    puts "scheduled: #{counter}" if counter % 1000
+
     side = [post.image_width, post.image_height].min
-    post.thumbnail = "0/0/#{side}"
+    if post.image_width > post.image_height
+      origin = ((post.image_width - side) / 2).floor
+      post.thumbnail = "#{origin}/0/#{side}"
+    else
+      post.thumbnail = "0/0/#{side}"
+    end
     post.save!
 
     ThumbnailJob.perform_later(post.id)
   end
+  puts "finished: #{counter}"
 end
