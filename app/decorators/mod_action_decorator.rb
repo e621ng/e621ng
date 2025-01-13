29b98e5002d55ae615b970237e38994246153428
clragon <clragon@clynamic.net>
2025-01-13 04:28:32 +0100

    [StaffNotes] Add updating and deleting (#823)

--- app/decorators/mod_action_decorator.rb
+++ app/decorators/mod_action_decorator.rb
@@ -55,7 +55,7 @@ class ModActionDecorator < ApplicationDecorator
     when "artist_delete"
       "Deleted artist ##{vals['artist_id']} (#{vals['artist_name']})"
     when "artist_page_rename"
-      "Renamed artist page (\"#{vals['old_name']}\":/artists/show_or_new?name=#{vals['old_name']} -> \"#{vals['new_name']}\":/artists/show_or_new?name=#{vals['new_name']})"
+      "Renamed artist page (\"#{vals['old_name']}\":/artists/show_or_new?name=#{vals['old_name']} → \"#{vals['new_name']}\":/artists/show_or_new?name=#{vals['new_name']})"
     when "artist_page_lock"
       "Locked artist page artist ##{vals['artist_page']}"
     when "artist_page_unlock"
@@ -76,6 +76,16 @@ class ModActionDecorator < ApplicationDecorator
       "Deleted \"avoid posting ##{vals['id']}\":/avoid_postings/#{vals['id']} for [[#{vals['artist_name']}]]"
     when "avoid_posting_undelete"
       "Undeleted \"avoid posting ##{vals['id']}\":/avoid_postings/#{vals['id']} for [[#{vals['artist_name']}]]"
+
+      ### Staff Note ###
+    when "staff_note_create"
+      "Created \"staff note ##{vals['id']}\":/staff_notes/#{vals['id']} for #{user}\n#{vals['body']}"
+    when "staff_note_update"
+      "Updated \"staff note ##{vals['id']}\":/staff_notes/#{vals['id']} for #{user}\n#{vals['body']}"
+    when "staff_note_delete"
+      "Deleted \"staff note ##{vals['id']}\":/staff_notes/#{vals['id']} for #{user}"
+    when "staff_note_undelete"
+      "Undeleted \"staff note ##{vals['id']}\":/staff_notes/#{vals['id']} for #{user}"
 
       ### User ###
 
@@ -276,7 +286,7 @@ class ModActionDecorator < ApplicationDecorator
       ### BURs ###
 
     when "mass_update"
-      "Mass updated [[#{vals['antecedent']}]] -> [[#{vals['consequent']}]]"
+      "Mass updated [[#{vals['antecedent']}]] → [[#{vals['consequent']}]]"
     when "nuke_tag"
       "Nuked tag [[#{vals['tag_name']}]]"
 
@@ -319,7 +329,7 @@ class ModActionDecorator < ApplicationDecorator
         "Edited whitelist entry"
       else
         if vals['old_pattern'] && vals['old_pattern'] != vals['pattern'] && CurrentUser.is_admin?
-          "Edited whitelist entry '#{vals['old_pattern']}' -> '#{vals['pattern']}'"
+          "Edited whitelist entry '#{vals['old_pattern']}' → '#{vals['pattern']}'"
         else
           "Edited whitelist entry '#{CurrentUser.is_admin? ? vals['pattern'] : vals['note']}'"
         end
