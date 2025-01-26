0dfcb4710dbb7f70d2fb40fcb0df850ea9c5afb7
Cinder <cinder@e621.net>
2025-01-26 06:26:11 -0800

    [Posts] Rework the post search page to be mobile-first (#855)

--- app/logical/post_sets/favorites.rb
+++ app/logical/post_sets/favorites.rb
@@ -28,11 +28,15 @@ module PostSets
       end
     end
 
+    def hidden_posts
+      @hidden_posts ||= posts.reject(&:visible?)
+    end
+
     def api_posts
-      _posts = posts
-      fill_children(_posts)
-      fill_tag_types(_posts)
-      _posts
+      result = posts
+      fill_children(result)
+      fill_tag_types(result)
+      result
     end
 
     def tag_array
@@ -41,6 +45,10 @@ module PostSets
 
     def presenter
       ::PostSetPresenters::Post.new(self)
+    end
+
+    def is_random?
+      false
     end
   end
 end
