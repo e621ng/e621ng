6ad4257d7e098836939df2ca66d6af18469d103f
clragon <clragon@clynamic.net>
2025-02-02 17:05:59 +0000

    [UserFeedbacks] Update fixer to restore original feedback ID

--- db/fixes/123_convert_staffnote_to_deleted_feedback.rb
+++ db/fixes/123_convert_staffnote_to_deleted_feedback.rb
@@ -3,33 +3,30 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 
-def extract_feedback_data(note)
-  match = note.body.match(%r{(?<deletion_user>"(?<user_name>.+?)":/users/(?<user_id>\d+)) deleted (?<category>\w+) feedback, created (?<created_date>\d{4}-\d{2}-\d{2}) by (?<creator_user>"(?<creator_name>.+?)":/users/(?<creator_id>\d+)):(?<body>[\s\S]+)})
-  return unless match
+CUTOFF_DATE = Date.new(2024, 8, 1)
 
-  {
-    user_id: note.user_id,
-    creator_id: match[:creator_id].to_i,
-    category: match[:category],
-    body: match[:body].strip,
-    created_at: Date.parse(match[:created_date]),
-    updated_at: note.updated_at,
-    updater_id: note.updater_id,
-    is_deleted: true,
-  }
-end
-
-StaffNote.where(creator: User.system).find_in_batches(batch_size: 10_000) do |notes|
-  feedback_data = []
-  staff_note_ids = []
-
-  notes.each do |note|
-    next unless (data = extract_feedback_data(note))
-
-    feedback_data << data
-    staff_note_ids << note.id
+ModAction.where(action: "user_feedback_destroy")
+         # .where("created_at < ?", CUTOFF_DATE)
+         .find_in_batches(batch_size: 10_000) do |batch|
+  feedback_data = batch.map do |mod_action|
+    {
+      id: mod_action.values["record_id"].to_i,
+      user_id: mod_action.values["user_id"].to_i,
+      creator_id: mod_action.creator_id,
+      category: mod_action.values["type"],
+      body: mod_action.values["reason"]&.strip || "",
+      created_at: mod_action.created_at,
+      updated_at: mod_action.created_at,
+      updater_id: mod_action.creator_id,
+      is_deleted: true,
+    }
   end
 
   UserFeedback.insert_all(feedback_data) if feedback_data.any?
-  StaffNote.where(id: staff_note_ids).delete_all if staff_note_ids.any?
+end
+
+StaffNote.where(creator: User.system)
+         .where("body LIKE ?", "%deleted%feedback%")
+         .find_in_batches(batch_size: 10_000) do |batch|
+  StaffNote.where(id: batch.map(&:id)).delete_all
 end
