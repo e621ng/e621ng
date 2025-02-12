14865a8323411d58bb0a180e53925ce84550b155
Cinder <cinder@e621.net>
2025-02-11 21:59:37 -0800

    [Posts] Add an option for a sticky searchbar

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -29,6 +29,7 @@ module IconHelper
 
     # Posts
     fullscreen: %(<path d="M3 7V5a2 2 0 0 1 2-2h2"/><path d="M17 3h2a2 2 0 0 1 2 2v2"/><path d="M21 17v2a2 2 0 0 1-2 2h-2"/><path d="M7 21H5a2 2 0 0 1-2-2v-2"/><rect width="10" height="8" x="7" y="8" rx="1"/>),
+    anchor: %(<path d="M12 22V8"/><path d="M5 12H2a10 10 0 0 0 20 0h-3"/><circle cx="12" cy="5" r="3"/>),
   }.freeze
 
   def svg_icon(name, *args)
