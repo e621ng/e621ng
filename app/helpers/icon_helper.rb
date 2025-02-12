9135ccd4b9f6d359c1ba91e8184900a9fce5650a
Cinder <cinder@e621.net>
2025-02-11 22:27:16 -0800

    [Posts] Replace the search submit icon with a lucide one (#910)

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -28,6 +28,7 @@ module IconHelper
     ellipsis: %(<circle cx="12" cy="12" r="1"/><circle cx="19" cy="12" r="1"/><circle cx="5" cy="12" r="1"/>),
 
     # Posts
+    search: %(<circle cx="11" cy="11" r="8"/><path d="m21 21-4.3-4.3"/>),
     fullscreen: %(<path d="M3 7V5a2 2 0 0 1 2-2h2"/><path d="M17 3h2a2 2 0 0 1 2 2v2"/><path d="M21 17v2a2 2 0 0 1-2 2h-2"/><path d="M7 21H5a2 2 0 0 1-2-2v-2"/><rect width="10" height="8" x="7" y="8" rx="1"/>),
   }.freeze
 
