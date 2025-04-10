a9cc8dde95fafaacd9cc718a99e440bc1f473fdd
Catt0s <5874051+mm12@users.noreply.github.com>
2025-04-10 17:07:01 -0500

    [post] fix delreason navigation bug on paging

--- app/logical/post_search_context.rb
+++ app/logical/post_search_context.rb
@@ -6,7 +6,7 @@ class PostSearchContext
   def initialize(params)
     tags = params[:q].presence || params[:tags].presence || ""
     tags += " rating:s" if CurrentUser.safe_mode?
-    tags += " -status:deleted" unless TagQuery.has_metatag?(tags, "status", "-status")
+    tags += " -status:deleted" unless TagQuery.has_metatag?(tags, "status", "-status", "delreason", "-delreason")
     pagination_mode = params[:seq] == "prev" ? "a" : "b"
     @post = Post.tag_match(tags).paginate("#{pagination_mode}#{params[:id]}", limit: 1).first || Post.find(params[:id])
   end
