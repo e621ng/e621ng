9ba36115f285221947a69cdaa7ff968493d561d9
Cinder <cinder@e621.net>
2025-04-30 20:45:30 -0700

    fix typo

--- db/migrate/20250430193448_add_post_votes_compond_index.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddPostVotesCompondIndex < ActiveRecord::Migration[7.1]
-  def change
-    add_index :post_votes, %i[user_id created_at], order: { user_id: :asc, created_at: :asc }
-  end
-end
