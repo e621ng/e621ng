30169738cf08a0e6f6e7e44a49d11d08b72a8260
Cinder <cinder@e621.net>
2025-05-08 08:23:59 -0700

    [PostVotes] Fix a timeout that occurs while creating an index (#1099)

--- db/migrate/20250430193448_add_post_votes_compound_index.rb
+++ db/migrate/20250430193448_add_post_votes_compound_index.rb
@@ -2,6 +2,8 @@
 
 class AddPostVotesCompoundIndex < ActiveRecord::Migration[7.1]
   def change
-    add_index :post_votes, %i[user_id created_at]
+    PostVote.without_timeout do
+      add_index :post_votes, %i[user_id created_at]
+    end
   end
 end
