fc4b8dd2087e2e6b3332d725719bbb8b91ddc9e7
Cinder <cinder@e621.net>
2025-05-14 10:50:43 -0700

    [Posts] Change the fixer script to regenerate all videos (#1104)

--- db/fixes/124_regenerate_video_samples.rb
+++ db/fixes/124_regenerate_video_samples.rb
@@ -3,7 +3,7 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 
-Post.where("(file_ext = ? OR file_ext = ?) AND generated_samples IS NOT NULL", "webm", "mp4").find_each do |post|
+Post.where("(file_ext = ? OR file_ext = ?)", "webm", "mp4").find_each do |post|
   # next if post.video_samples.empty?
   PostVideoConversionJob.perform_later(post.id)
 end
