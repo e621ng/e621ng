7ae99f7e44ab127f595356eaa25a5f609640f876
Cinder <cinder@e621.net>
2025-05-13 21:07:07 -0700

    [Votes] Fix performance issues with vote searches (#1102)

--- db/structure.sql
+++ db/structure.sql
@@ -3624,6 +3624,13 @@ CREATE INDEX index_comment_votes_on_user_id ON public.comment_votes USING btree 
 
 
 --
+-- Name: index_comment_votes_on_user_id_and_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_comment_votes_on_user_id_and_id ON public.comment_votes USING btree (user_id, id);
+
+
+--
 -- Name: index_comments_on_creator_id; Type: INDEX; Schema: public; Owner: -
 --
 
@@ -4160,6 +4167,13 @@ CREATE INDEX index_post_votes_on_user_id ON public.post_votes USING btree (user_
 --
 
 CREATE INDEX index_post_votes_on_user_id_and_created_at ON public.post_votes USING btree (user_id, created_at);
+
+
+--
+-- Name: index_post_votes_on_user_id_and_id; Type: INDEX; Schema: public; Owner: -
+--
+
+CREATE INDEX index_post_votes_on_user_id_and_id ON public.post_votes USING btree (user_id, id);
 
 
 --
@@ -4710,6 +4724,7 @@ ALTER TABLE ONLY public.staff_notes
 SET search_path TO "$user", public;
 
 INSERT INTO "schema_migrations" (version) VALUES
+('20250512221037'),
 ('20250430193448'),
 ('20250429022022'),
 ('20250423141854'),
