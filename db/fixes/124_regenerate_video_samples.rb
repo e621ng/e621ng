a034f6f7ff7b30a29891a20a24933012f7b4cefc
Cinder <cinder@e621.net>
2025-05-14 19:23:02 -0700

    Update 124_regenerate_video_samples.rb

--- db/fixes/124_regenerate_video_samples.rb
+++ db/fixes/124_regenerate_video_samples.rb
@@ -3,9 +3,11 @@
 
 require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
 
-Post.where("(file_ext = ? OR file_ext = ?)", "webm", "mp4").find_in_batches.with_index do |group, index|
-  puts "Processing batch #{index} with #{group.size} posts"
-  group.each do |post|
-    PostVideoConversionJob.perform_later(post.id)
+Post.without_timeout do
+  Post.where("(file_ext = ? OR file_ext = ?)", "webm", "mp4").find_in_batches.with_index do |group, index|
+    puts "Processing batch #{index} with #{group.size} posts"
+    group.each do |post|
+      PostVideoConversionJob.perform_later(post.id)
+    end
   end
 end
