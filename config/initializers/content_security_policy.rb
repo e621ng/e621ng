b681f104a37a44993f9fe1c55459bae9a68d9060
Cinder <cinder@e621.net>
2025-05-16 08:02:38 -0700

    [Misc] Replace Plausible with Umami (#1108)

--- config/initializers/content_security_policy.rb
+++ config/initializers/content_security_policy.rb
@@ -11,7 +11,7 @@ Rails.application.configure do
     policy.default_src :self
     policy.script_src  :self, "ads.dragonfru.it", "https://www.google.com/recaptcha/", "https://www.gstatic.com/recaptcha/", "https://www.recaptcha.net/", "https://assets.freespeechcoalition.com"
     policy.style_src   :self, :unsafe_inline
-    policy.connect_src :self, "ads.dragonfru.it", "plausible.dragonfru.it", "static1.e621.net", "static1.e926.net", "api.freespeechcoalition.com"
+    policy.connect_src :self, "ads.dragonfru.it", "umami.dragonfru.it", "static1.e621.net", "static1.e926.net", "api.freespeechcoalition.com"
     policy.object_src  :self, "static1.e621.net", "static1.e926.net"
     policy.media_src   :self, "static1.e621.net", "static1.e926.net"
     policy.frame_ancestors :none
