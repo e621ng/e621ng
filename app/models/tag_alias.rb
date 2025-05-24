9e4bb7a00a2eda7f8187a9ee653556fb72be12a8
Cinder <cinder@e621.net>
2025-05-24 12:35:56 -0700

    [AIBUR] Transfer linked artist ID upon alias approval

--- app/models/tag_alias.rb
+++ app/models/tag_alias.rb
@@ -321,10 +321,12 @@ class TagAlias < TagRelationship
   end
 
   def rename_artist
-    if antecedent_tag.category == Tag.categories.artist
-      if antecedent_tag.artist.present? && consequent_tag.artist.blank?
-        antecedent_tag.artist.update!(name: consequent_name)
-      end
+    return unless antecedent_tag.category == Tag.categories.artist && antecedent_tag.artist.present?
+    if consequent_tag.artist.blank?
+      antecedent_tag.artist.update!(name: consequent_name)
+    elsif antecedent_tag.artist.linked_user_id.present? && consequent_tag.artist.linked_user_id.blank?
+      consequent_tag.artist.update(linked_user_id: antecedent_tag.artist.linked_user_id)
+      antecedent_tag.artist.update(linked_user_id: nil)
     end
   end
 
