9f25a1dd7c0461da865509d0def04c6cc47bb332
Cinder <cinder@e621.net>
2025-05-28 07:44:40 -0700

    [UploadWhitelists] Add input validation to is_allowed (#1125)

--- test/unit/upload_whitelist_test.rb
+++ test/unit/upload_whitelist_test.rb
@@ -11,15 +11,21 @@ class UploadWhitelistTest < ActiveSupport::TestCase
       @whitelist = create(:upload_whitelist, pattern: "*.e621.net/data/*", note: "e621")
     end
 
-    should "match" do
-      assert_equal([true, nil], UploadWhitelist.is_whitelisted?(Addressable::URI.parse("https://static1.e621.net/data/123.png")))
-      assert_equal([false, "123.com not in whitelist"], UploadWhitelist.is_whitelisted?(Addressable::URI.parse("https://123.com/what.png")))
+    should "succeed for valid URLs" do
+      assert_equal([true, nil], UploadWhitelist.is_whitelisted?("https://static1.e621.net/data/123.png"))
+    end
+
+    should "fail for invalid URLs" do
+      assert_equal([false, "invalid url"], UploadWhitelist.is_whitelisted?(""))
+      assert_equal([false, "invalid url"], UploadWhitelist.is_whitelisted?(nil))
+      assert_equal([false, "123.com not in whitelist"], UploadWhitelist.is_whitelisted?("https://123.com/what.png"))
+      assert_equal([false, "aaa not in whitelist"], UploadWhitelist.is_whitelisted?("aaa"))
     end
 
     should "bypass for admins" do
       CurrentUser.user.level = 50
       Danbooru.config.stubs(:bypass_upload_whitelist?).returns(true)
-      assert_equal([true, "bypassed"], UploadWhitelist.is_whitelisted?(Addressable::URI.parse("https://123.com/what.png")))
+      assert_equal([true, "bypassed"], UploadWhitelist.is_whitelisted?("https://123.com/what.png"))
     end
   end
 end
