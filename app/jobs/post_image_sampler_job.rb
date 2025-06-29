18d87b208400c0f4b2e35ba7a89629f5cf771126
Cinder <cinder@e621.net>
2025-06-28 19:47:07 -0700

    [Posts] Rework the mass sample regeneration script

--- app/jobs/post_image_sampler_job.rb
+++ app/jobs/post_image_sampler_job.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class PostImageSamplerJob < ApplicationJob
-  queue_as :video
+  queue_as :thumb
   sidekiq_options lock: :until_executed, lock_args_method: :lock_args, retry: 1
 
   def self.lock_args(args)
