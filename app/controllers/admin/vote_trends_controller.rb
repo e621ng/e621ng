e022e34b155580542d4551e08167ab43173e5d56
Catting <5874051+mm12@users.noreply.github.com>
2025-07-09 17:07:38 -0500

    autocomplete usernames

--- app/controllers/admin/vote_trends_controller.rb
+++ app/controllers/admin/vote_trends_controller.rb
@@ -5,21 +5,23 @@ module Admin
     before_action :admin_only
     respond_to :html, :json
     def index
-      params.permit(:user, :limit, :threshold, :duration, :vote_normality, :id, :page, search: {})
-      if params[:user].blank? || !User.exists?(params[:user].to_i)
+      permitted_params = params.permit(:user, :limit, :threshold, :duration, :vote_normality, :id, :page, :commit, search: {})
+      vote_abuse_args = {}
+      vote_abuse_args[:user] = User.find_by_name_or_id(permitted_params[:user]) if permitted_params[:user].present? # rubocop:disable Rails/DynamicFindBy
+      vote_abuse_args[:vote_normality] = permitted_params[:vote_normality].to_i != 1
+      vote_abuse_args[:limit] = permitted_params[:limit].to_i if permitted_params[:limit].present?
+      vote_abuse_args[:threshold] = permitted_params[:threshold].to_f if permitted_params[:threshold].present?
+      vote_abuse_args[:duration] = permitted_params[:duration] if permitted_params[:duration].present?
+
+      if vote_abuse_args[:user].nil?
+        Rails.logger.warn("Vote trends: No user found for '#{permitted_params[:user]}'")
+        flash[:notice] = "No user found for '#{permitted_params[:user]}'"
         @vote_trends = []
         respond_with(@vote_trends) # show the page without any data
         return
       end
 
-      vote_abuse_args = {
-        user: User.find(params[:user].to_i),
-        vote_normality: params[:vote_normality].to_i != 1,
-      }
-      vote_abuse_args[:limit] = params[:limit].to_i if params[:limit].present?
-      vote_abuse_args[:threshold] = params[:threshold].to_f if params[:threshold].present?
-      vote_abuse_args[:duration] = params[:duration] if params[:duration].present?
-
+      Rails.logger.info("Vote trends: #{vote_abuse_args.inspect}")
       @vote_trends = VoteManager::VoteAbuseMethods.vote_abuse_patterns(**vote_abuse_args)
       respond_with(@vote_trends)
     end
