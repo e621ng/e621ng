542a6eaf3e7fc4e05e19d8429d8930c63db98592
Justin Morris <jmortiger@gmail.com>
2025-07-20 16:18:49 -0400

    [Posts] Fix test case for `order:rank`

--- test/unit/elastic_post_query_builder_test.rb
+++ test/unit/elastic_post_query_builder_test.rb
@@ -70,9 +70,9 @@ class ElasticPostQueryBuilderTest < ActiveSupport::TestCase
       "tagcount" => [[{ tag_count: :desc }], [{ tag_count: :asc }]],
       "tagcount_desc" => [[{ tag_count: :desc }], [{ tag_count: :asc }]],
       "tagcount_asc" => [[{ tag_count: :asc }], [{ tag_count: :desc }]],
+      "hot" => [[{ _score: :desc }]],
       "rank" => [[{ _score: :desc }]],
       "random" => [[{ _score: :desc }]],
-      "hot" => [[{ _score: :desc }]],
       "portrait" => [[{ aspect_ratio: :asc }], [{ aspect_ratio: :desc }]],
       "landscape" => [[{ aspect_ratio: :desc }], [{ aspect_ratio: :asc }]],
     },
@@ -422,8 +422,9 @@ class ElasticPostQueryBuilderTest < ActiveSupport::TestCase
           when "rank", "hot" # TODO: Test with `hot_from`
             assert_includes(r.must, { range: { score: { gt: 0 } } }, msg)
             datetime_ago = nil
-            assert(r.must.any? { |x| datetime_ago ||= x[:range]&.fetch(:created_at, nil)&.fetch(:gte, nil) }, msg)
-            assert_in_delta(comparison, datetime_ago, TIME_DELTA, msg)
+            # TODO: Remove conditional if rank is bounded again
+            assert(r.must.any? { |x| datetime_ago ||= x[:range]&.fetch(:created_at, nil)&.fetch(:gte, nil) }, msg) if k == "hot"
+            assert_in_delta(comparison, datetime_ago, TIME_DELTA, msg) if k == "hot"
           # FIXME: Find a way to test the function score and assert these commented out lines
           # TODO: Check with `hot`
           #   assert_equals({
