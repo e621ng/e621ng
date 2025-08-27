30c2b7a83256d07a7cb73cfb22b7f2fdea0d6b78
clragon <clragon@clynamic.net>
2025-08-27 01:01:06 +0000

    [Alternates] Update fixer script

--- db/fixes/108_source_cleaning.rb
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env ruby
-# frozen_string_literal: true
-
-require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
-
-Post.find_each do |post|
-  post.strip_source
-  post.save(validate: false)
-end
---
+++ db/fixes/128_source_cleaning.rb
@@ -0,0 +1,13 @@
+#!/usr/bin/env ruby
+# frozen_string_literal: true
+
+require File.expand_path(File.join(File.dirname(__FILE__), "..", "..", "config", "environment"))
+
+Post.without_timeout do
+  Post.in_batches(load: true, order: :desc).each do |group|
+    group.each do |post|
+      post.strip_source
+      post.save(validate: false)
+    end
+  end
+end
