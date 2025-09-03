b23541a841fc33cff6da50ef8dae1dfdeb4b9b20
Cinder <cinder@e621.net>
2025-09-02 18:40:47 -0700

    [PostFlags] Add flag ID attribute to the table row (#1272)
    Also some rubocop cleanup.

--- app/controllers/post_flags_controller.rb
+++ app/controllers/post_flags_controller.rb
@@ -1,9 +1,22 @@
 # frozen_string_literal: true
 
 class PostFlagsController < ApplicationController
-  before_action :member_only, :except => [:index, :show]
+  before_action :member_only, except: %i[index show]
   before_action :janitor_only, only: [:destroy]
   respond_to :html, :json
+
+  def index
+    @search_params = search_params
+    @post_flags = PostFlag.includes(:creator, post: %i[flags uploader approver]).search(@search_params).paginate(params[:page], limit: params[:limit])
+    respond_with(@post_flags)
+  end
+
+  def show
+    @post_flag = PostFlag.find(params[:id])
+    respond_with(@post_flag) do |fmt|
+      fmt.html { redirect_to post_flags_path(search: { id: @post_flag.id }) }
+    end
+  end
 
   def new
     @post_flag = PostFlag.new(post_flag_params)
@@ -11,22 +24,15 @@ class PostFlagsController < ApplicationController
     respond_with(@post_flag)
   end
 
-  def index
-    @search_params = search_params
-    @post_flags = PostFlag.search(@search_params).includes(:creator, post: [:flags, :uploader, :approver])
-    @post_flags = @post_flags.paginate(params[:page], limit: params[:limit])
-    respond_with(@post_flags)
-  end
-
   def create
     @post_flag = PostFlag.create(post_flag_params)
     respond_with(@post_flag) do |fmt|
       fmt.html do
-        if @post_flag.errors.size > 0
+        if @post_flag.errors.empty?
+          redirect_to post_path(id: @post_flag.post_id)
+        else
           @post = Post.find(params[:post_flag][:post_id])
           respond_with(@post_flag)
-        else
-          redirect_to post_path(id: @post_flag.post_id)
         end
       end
     end
@@ -39,13 +45,6 @@ class PostFlagsController < ApplicationController
       @post.approve!
     end
     respond_with(nil)
-  end
-
-  def show
-    @post_flag = PostFlag.find(params[:id])
-    respond_with(@post_flag) do |fmt|
-      fmt.html {redirect_to post_flags_path(search: {id: @post_flag.id})}
-    end
   end
 
   private
