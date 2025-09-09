1fd196df4895d49d756537f3118f403b584bd4ee
Cinder <cinder@e621.net>
2025-09-09 13:42:31 -0700

    update to rails 7.2

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -22,7 +22,7 @@ Rails.application.configure do
   # Configure public file server for tests with Cache-Control for performance.
   config.public_file_server.enabled = true
   config.public_file_server.headers = {
-    "Cache-Control" => "public, max-age=#{1.hour.to_i}"
+    "Cache-Control" => "public, max-age=#{1.hour.to_i}",
   }
 
   # Show full error reports and disable caching.
@@ -59,4 +59,6 @@ Rails.application.configure do
 
   # Raise error when a before_action's only/except options reference missing actions
   config.action_controller.raise_on_missing_callback_actions = true
+
+  config.active_job.queue_adapter = :test
 end
