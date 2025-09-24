fc44182fbd4355e733f9b36274b48ef3dc112d05
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2025-09-24 09:09:29 -0400

    Update tag alias tester

--- test/unit/tag_alias_test.rb
+++ test/unit/tag_alias_test.rb
@@ -60,10 +60,18 @@ class TagAliasTest < ActiveSupport::TestCase
           end
         end
 
-        %w|: ~ + _ ` ( ) { } [ ] /|.each do |x|
-          should "allow tag names starting with #{x}" do
-            ta1 = create(:tag_alias, antecedent_name: "#{x}foo", consequent_name: "bar")
-            assert(ta1.valid?)
+        %w|~ + _ ` ( ) { } [ ] /|.each do |x|
+          should "not allow tag names starting with #{x}" do
+            assert_raises(ActiveRecord::RecordInvalid) do
+              create(:tag_alias, antecedent_name: "#{x}foo", consequent_name: "bar")
+            end
+          end
+        end
+
+        should "not allow zero-width spaces within tag names" do
+          # There is a zero width space between the two o's
+          assert_raises(ActiveRecord::RecordInvalid) do
+            create(:tag_alias, antecedent_name: "fo​o", consequent_name: "bar")
           end
         end
 
@@ -72,9 +80,18 @@ class TagAliasTest < ActiveSupport::TestCase
           assert(ta1.valid?)
         end
 
-        should "allow zero-width spaces within tag names" do
-          # There is a zero width space between the two o's
-          ta1 = create(:tag_alias, antecedent_name: "fo​o", consequent_name: "bar")
+        should "allow tags to start with :" do
+          ta1 = create(:tag_alias, antecedent_name: ":foo", consequent_name: "bar")
+          assert(ta1.valid?)
+        end
+
+        should "allow tags to contain $" do
+          ta1 = create(:tag_alias, antecedent_name: "fo$o", consequent_name: "bar")
+          assert(ta1.valid?)
+        end
+
+        should "allow tags to contain \\" do
+          ta1 = create(:tag_alias, antecedent_name: "fo\\o", consequent_name: "bar")
           assert(ta1.valid?)
         end
       end
