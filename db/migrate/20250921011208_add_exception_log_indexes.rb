3ad3f789a9e419c4c8538a2a634725fe9dd7ed0c
Cinder <cinder@e621.net>
2025-09-24 09:12:39 -0700

    [Misc] Run the exception log migration without timeout

--- db/migrate/20250921011208_add_exception_log_indexes.rb
+++ db/migrate/20250921011208_add_exception_log_indexes.rb
@@ -2,8 +2,10 @@
 
 class AddExceptionLogIndexes < ActiveRecord::Migration[7.2]
   def change
-    add_foreign_key :exception_logs, :users, column: :user_id
-    add_index :exception_logs, :user_id
-    add_index :exception_logs, :code
+    ExceptionLog.without_timeout do
+      add_foreign_key :exception_logs, :users, column: :user_id
+      add_index :exception_logs, :user_id
+      add_index :exception_logs, :code
+    end
   end
 end
