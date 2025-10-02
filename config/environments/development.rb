c2011a5e8b67a37f4ce04f4a0e33bdd3dd22f37e
Catting <5874051+mm12@users.noreply.github.com>
2025-10-02 01:03:36 +0000

    dumb fix of a even dumber problem

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -67,11 +67,15 @@ Rails.application.configure do # rubocop:disable Metrics/BlockLength
 
   config.hosts << "e621ng.local"
 
-  # Allow access from GitHub Codespaces, if applicable
-  if ENV["CODESPACE_NAME"].present? && ENV["GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN"].present?
-    codespace_host = "#{ENV.fetch('CODESPACE_NAME', nil)}-3000.#{ENV.fetch('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', nil)}"
-    config.hosts << codespace_host
-  elsif ENV["GITHUB_CODESPACE_HOST"].present?
-    config.hosts << ENV["GITHUB_CODESPACE_HOST"]
+  # config.after_initialize do
+    # Allow access from GitHub Codespaces, if applicable
+  if ENV["CODESPACES"].present? && ENV.fetch("CODESPACES", "false") == "true"
+    # codespace_host = "#{ENV.fetch('CODESPACE_NAME', '')}-#{ENV.fetch('EXPOSED_SERVER_PORT', '3000')}.#{ENV.fetch('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', 'app.github.dev')}"
+    codespace_host = /#{ENV.key?('CODESPACE_NAME') ? Regexp.escape(ENV['CODESPACE_NAME']) : '.*'}-#{ENV.fetch('EXPOSED_SERVER_PORT', '3000')}.#{Regexp.escape(ENV.fetch('GITHUB_CODESPACES_PORT_FORWARDING_DOMAIN', 'app.github.dev'))}/
+    config.hosts << codespace_host # for some reason, rails doesn't like the full domain
+    # puts "Allowed Codespaces host: #{codespace_host}"
+  # else
+    # puts "Not running in Codespaces"
   end
+  # end
 end
