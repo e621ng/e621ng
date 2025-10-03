614710513b62834da570301718751157a9658241
Cinder <cinder@e621.net>
2025-10-03 08:25:02 -0700

    structure.sql junk

--- db/structure.sql
+++ db/structure.sql
@@ -1,8 +1,3 @@
-\restrict SpSkVTe8ygdfbTMIrGDceafuUq9O2DledcAppM3R6NHq6L2VdZYxi7aFHzTgwc1
-
--- Dumped from database version 15.12
--- Dumped by pg_dump version 15.14
-
 SET statement_timeout = 0;
 SET lock_timeout = 0;
 SET idle_in_transaction_session_timeout = 0;
@@ -4905,8 +4900,6 @@ ALTER TABLE ONLY public.staff_notes
 --
 -- PostgreSQL database dump complete
 --
-
-\unrestrict SpSkVTe8ygdfbTMIrGDceafuUq9O2DledcAppM3R6NHq6L2VdZYxi7aFHzTgwc1
 
 SET search_path TO "$user", public;
 
