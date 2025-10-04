a98570c7df6993aabcbcadceff5a18240dd2e7c1
Cinder <cinder@e621.net>
2025-10-04 08:43:10 -0700

    Migrate to a new route

--- app/javascript/src/js/pages/post_flags/post_flags.js
+++ app/javascript/src/js/pages/post_flags/post_flags.js
@@ -16,11 +16,11 @@ PostFlags.initExpandableNotes = function () {
 
 PostFlags.initFlagForm = function () {
   // Form should always be present
-  const form = $("#new_post_flag");
+  const form = $("#post-report-form");
   if (form.length === 0) return;
 
   // Label the note field as required if the selected reason requires an explanation
-  const noteLabel = form.find("label[for='flag_note_field']");
+  const noteLabel = form.find("label[for='report_note_field']");
   $("<span>")
     .addClass("required-indicator")
     .text(" (required)")
@@ -29,7 +29,7 @@ PostFlags.initFlagForm = function () {
 
   let isNoteRequired = false;
   function updateNoteRequired () {
-    const selected = form.find("input[name='post_flag[reason_name]']:checked");
+    const selected = form.find("input[name='reason_name']:checked");
     isNoteRequired = selected.data("requireExplanation") === true;
     noteLabel.toggleClass("required", isNoteRequired);
     toggleSubmitButton();
@@ -38,7 +38,7 @@ PostFlags.initFlagForm = function () {
 
   // Determine if the note field is empty dynamically
   let isNoteEmpty = false;
-  const noteField = form.find("#flag_note_field").on("input", () => {
+  const noteField = form.find("#report_note_field").on("input", () => {
     isNoteEmpty = (noteField.val() || "").trim() === "";
     toggleSubmitButton();
   });
@@ -52,7 +52,7 @@ PostFlags.initFlagForm = function () {
 
 
   // Extra validation on submit
-  const noteHint = form.find(".flag-note-hint");
+  const noteHint = form.find(".report-note-hint");
   form.on("submit", (event) => {
     noteHint.hide();
     if (!isNoteRequired || !isNoteEmpty) return true;
@@ -64,7 +64,7 @@ PostFlags.initFlagForm = function () {
 
 
   // Initial state
-  isNoteRequired = form.find("input[name='post_flag[reason_name]']:checked").data("requireExplanation") === true;
+  isNoteRequired = form.find("input[name='reason_name']:checked").data("requireExplanation") === true;
   isNoteEmpty = (noteField.val() || "").trim() === "";
   noteLabel.toggleClass("required", isNoteRequired);
   toggleSubmitButton();
@@ -72,7 +72,7 @@ PostFlags.initFlagForm = function () {
 
 $(() => {
   PostFlags.initExpandableNotes();
-  if (Page.matches("post-flags", "new"))
+  if (Page.matches("posts", "report"))
     PostFlags.initFlagForm();
 });
 
