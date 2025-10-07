7acc7895be94a4da43954451332f26a586516d86
clragon <clragon@clynamic.net>
2025-10-07 11:54:47 +0000

    [ApiKeys] Improve migration

--- db/migrate/20250829232928_upgrade_api_keys.rb
+++ db/migrate/20250829232928_upgrade_api_keys.rb
@@ -2,12 +2,14 @@
 
 class UpgradeApiKeys < ActiveRecord::Migration[7.1]
   def change
-    add_column :api_keys, :name, :string, default: "", null: false
-    add_column :api_keys, :last_used_at, :datetime
-    add_column :api_keys, :last_ip_address, :inet
-    add_column :api_keys, :last_user_agent, :text
-    add_column :api_keys, :expires_at, :datetime
-    remove_index :api_keys, :user_id
-    add_index :api_keys, %i[name user_id], unique: true
+    ApiKey.without_timeout do
+      add_column :api_keys, :name, :string, default: "", null: false
+      add_column :api_keys, :last_used_at, :datetime
+      add_column :api_keys, :last_ip_address, :inet
+      add_column :api_keys, :last_user_agent, :text
+      add_column :api_keys, :expires_at, :datetime
+      remove_index :api_keys, :user_id
+      add_index :api_keys, %i[name user_id], unique: true
+    end
   end
 end
