e26896a50f03fc108177ae83817fc52dfd08cb26
Catting <5874051+mm12@users.noreply.github.com>
2025-10-09 15:15:21 -0500

    Update markdown

--- .devcontainer/post-attach.sh
+++ .devcontainer/post-attach.sh
@@ -12,4 +12,4 @@ fi
 # Ensure the local configuration file is kept in sync.
 cp /app/docker/danbooru_local_config.rb /app/config/danbooru_local_config.rb
 
-echo -e "\e[1;33mImportant:\e[0m if using devcontainers through GitHub Codespaces or similar services, it is \e[1mrecommended\e[0m to modify 'db/populate.rb' to use e926 or specify 'rating:s' in the post population"
+echo -e "\e[1;33mImportant:\e[0m if using devcontainers through GitHub Codespaces or similar services, it is \e[1mrecommended\e[0m to modify 'db/populate.rb' to use e926 or specify 'rating:s' in the post population\n\nFor help with the devcontainer, see .devcontainer/devcontainer.md\n"
