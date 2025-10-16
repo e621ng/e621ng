32922ea4509b84ae467f36a67df2fd9a57503eca
Cinder <cinder@e621.net>
2025-10-15 19:13:48 -0700

    Cleanup

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 set -e
 
-<<<<<<< HEAD
 if [ -d "vendor/dtext" ] && [ "$LOCAL_DTEXT" = "true" ]; then
   echo "dtext: Recompiling..."
   cd vendor/dtext
-=======
-if [ -d "vendor/dtext_rb" ] && [ "$USE_LOCAL_DTEXT" = "true" ]; then
-  echo "dtext_rb: Recompiling..."
-  cd vendor/dtext_rb
->>>>>>> e54e14ab1 ([DText] Allow for the usage of a local dtext_rb gem)
   
   rm -f lib/dtext/dtext.so
   rm -rf tmp/
@@ -17,11 +11,7 @@ if [ -d "vendor/dtext_rb" ] && [ "$USE_LOCAL_DTEXT" = "true" ]; then
   bundle install --quiet
   bundle exec rake compile
   cd /app
-<<<<<<< HEAD
   echo "dtext: Recompiled successfully"
-=======
-  echo "dtext_rb: Recompiled successfully"
->>>>>>> e54e14ab1 ([DText] Allow for the usage of a local dtext_rb gem)
 fi
 
 exec "$@"
