a4b333a71ec6a8984b98cb3fae7000c9b23593bb
Catting <5874051+mm12@users.noreply.github.com>
2025-10-16 16:20:56 -0500

    mock setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -1018,7 +1018,8 @@ class User < ApplicationRecord
 
   def user_color
     # Returns a hex color code based on the user's ID.
-    hash = Digest::MD5.hexdigest(id.to_s)
-    "##{hash[0..5]}"
+    return "##{Digest::MD5.hexdigest(id.to_s)[0..5]}" unless avatar_id.present?
+    # use avatar id as a placeholder for the setting
+    "##{Digest::MD5.hexdigest(avatar_id.to_s)[0..5]}"
   end
 end
