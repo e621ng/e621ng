fb430a00faee2e65cae2126f67f7bffcc0dca672
Cinder <cinder@e621.net>
2025-11-05 08:24:18 -0800

    [Build] Lazy-load Vue components (#1477)

--- app/javascript/src/javascripts/related_tag.js
+++ app/javascript/src/javascripts/related_tag.js
@@ -1,18 +1,26 @@
+import $ from "jquery";
+
 const RelatedTag = {};
 
-import TagEditor from "./tag_editor.vue";
-import { createApp } from "vue";
-
 RelatedTag.tag_editor_setup = false;
-RelatedTag.init_post_show_editor = function () {
-  if (RelatedTag.tag_editor_setup)
-    return;
+RelatedTag.init_post_show_editor = async function () {
+  if (RelatedTag.tag_editor_setup) return;
   RelatedTag.tag_editor_setup = true;
+
+  // Import Vue as needed
+  const [{ createApp }, { default: TagEditor }] = await Promise.all([
+    import("vue"),
+    import("./tag_editor.vue"),
+  ]);
 
   const app = createApp(TagEditor);
   app.mount("#tag-string-editor");
 };
 
 $(function () {
-  $(document).on("danbooru:open-post-edit-tab", RelatedTag.init_post_show_editor);
+  $(document).one("danbooru:open-post-edit-tab", () => {
+    RelatedTag.init_post_show_editor().catch(console.error);
+  });
 });
+
+export default RelatedTag;
--- app/javascript/src/javascripts/uploader.js
+++ app/javascript/src/javascripts/uploader.js
@@ -1,9 +1,40 @@
-import Uploader from "./uploader/uploader.vue";
-import { createApp } from "vue";
+import Page from "./utility/page.js";
+import $ from "jquery";
 
-export default {
-  init () {
+const UploaderModule = {
+  async init () {
+    const uploaderElement = document.getElementById("uploader");
+    if (!uploaderElement) return;
+    window.Danbooru.Uploader = UploaderModule;
+
+    // Import Vue as needed
+    const [{ createApp }, { default: Uploader }] = await Promise.all([
+      import("vue"),
+      import("./uploader/uploader.vue"),
+    ]);
+
+    const dataset = uploaderElement.dataset;
+    window.uploaderSettings = {
+      compactMode: dataset.compactMode === "true",
+      safeSite: dataset.safeSite === "true",
+      uploadTags: JSON.parse(dataset.uploadTags || "[]"),
+      recentTags: JSON.parse(dataset.recentTags || "[]"),
+      allowLockedTags: dataset.allowLockedTags === "true",
+      allowRatingLock: dataset.allowRatingLock === "true",
+      allowUploadAsPending: dataset.allowUploadAsPending === "true",
+      maxFileSize: parseInt(dataset.maxFileSize || "0"),
+      maxFileSizeMap: JSON.parse(dataset.maxFileSizeMap || "{}"),
+      verifiedArtistTags: JSON.parse(dataset.verifiedArtistTags || "[]"),
+    };
+
     const app = createApp(Uploader);
     app.mount("#uploader");
   },
 };
+
+$(() => {
+  if (!Page.matches("uploads", "new")) return;
+  UploaderModule.init().catch(console.error);
+});
+
+export default UploaderModule;
