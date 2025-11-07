1b289bf678d69e8dfe7d17d0ca7058338cc01e7a
Cinder <cinder@e621.net>
2025-11-06 21:33:59 -0800

    rubocop

--- test/mailers/previews/maintenance/user/email_confirmation_mailer_preview.rb
+++ test/mailers/previews/maintenance/user/email_confirmation_mailer_preview.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Maintenance::User::EmailConfirmationMailerPreview < ActionMailer::Preview
+class Maintenance::User::EmailConfirmationMailerPreview < ActionMailer::Preview # rubocop:disable Style/ClassAndModuleChildren
   def confirmation
     Maintenance::User::EmailConfirmationMailer.confirmation(User.first)
   end
