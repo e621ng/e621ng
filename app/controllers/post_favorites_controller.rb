0912fff6acb4dcfe8322af4f5522c9d6404445e2
Cinder <cinder@e621.net>
2025-12-09 21:21:00 -0800

    [Favorites] Optimize the page that lists users who favorited the post (#1567)

--- app/controllers/post_favorites_controller.rb
+++ app/controllers/post_favorites_controller.rb
@@ -5,12 +5,23 @@ class PostFavoritesController < ApplicationController
 
   def index
     @post = Post.find(params[:post_id])
-    query = User.includes(:user_status).joins(:favorites)
+
+    # Base query: users who favorited this post
+    query = User.includes(:user_status)
+                .joins(favorites: {}).where(favorites: { post_id: @post.id })
+
+    # Privacy filter for non-moderators
     unless CurrentUser.is_moderator?
-      query = query.where("bit_prefs & :value != :value", { value: User.flag_value_for("enable_privacy_mode") }).or(query.where(favorites: { user_id: CurrentUser.id }))
+      privacy_flag = User.flag_value_for("enable_privacy_mode")
+      query = query.where("(users.bit_prefs & ?) = 0", privacy_flag)
     end
-    query = query.where(favorites: { post_id: @post.id })
+
     query = query.order("users.name asc")
-    @users = query.paginate(params[:page], limit: params[:limit])
+
+    paginate_options = {}
+    paginate_options[:limit] = params[:limit].to_i.clamp(1, 100) if params[:limit].present?
+    paginate_options[:total_count] = @post.fav_count if @post.fav_count > 1000
+
+    @users = query.paginate(params[:page], paginate_options)
   end
 end
