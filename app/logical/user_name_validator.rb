9437b6cf870bae2a69312b8d065bc16c8cd5fe34
Cinder <cinder@e621.net>
2026-02-10 16:19:36 -0800

    Typo

--- app/logical/user_name_validator.rb
+++ app/logical/user_name_validator.rb
@@ -17,7 +17,7 @@ class UserNameValidator < ActiveModel::EachValidator
       end
     end
     rec.errors.add(attr, "must be 2 to 20 characters long") unless name.length.between?(2, 20)
-    rec.errors.add(attr, "must contain only alphanumeric characters, hypens, apostrophes, tildes and underscores") unless name =~ /\A[a-zA-Z0-9\-_~']+\z/
+    rec.errors.add(attr, "must contain only alphanumeric characters, hyphens, apostrophes, tildes and underscores") unless name =~ /\A[a-zA-Z0-9\-_~']+\z/
     rec.errors.add(attr, "must not begin with a special character") if name =~ /\A[_\-~']/
     rec.errors.add(attr, "must not contain consecutive special characters") if name =~ /_{2}|-{2}|~{2}|'{2}/
     rec.errors.add(attr, "cannot begin or end with an underscore") if name =~ /\A_|_\z/
