5cea09992a3368cd2dcf73c391fb1f99cebc593a
Cinder <cinder@e621.net>
2026-03-24 12:19:42 -0700

    [Tags] Add debug output to solve stuck aliases

--- app/controllers/tag_corrections_controller.rb
+++ app/controllers/tag_corrections_controller.rb
@@ -2,7 +2,12 @@
 
 class TagCorrectionsController < ApplicationController
   respond_to :html, :json
-  before_action :janitor_only, only: [:new, :create]
+  before_action :janitor_only, only: %i[new create]
+
+  def show
+    @correction = TagCorrection.new(params[:tag_id])
+    respond_with(@correction)
+  end
 
   def new
     @from_wiki = request.referer.try(:include?, "wiki_pages") || false
@@ -15,14 +20,17 @@ class TagCorrectionsController < ApplicationController
       @aliases = TagAlias.where("(antecedent_name = ? OR consequent_name = ?) AND NOT status = ?", @tag.name, @tag.name, "deleted").count
       @implications = TagImplication.where("(antecedent_name = ? OR consequent_name = ?) AND NOT status = ?", @tag.name, @tag.name, "deleted").count
 
+      # If a tag is aliased away but still has posts, it means one of two things:
+      # 1. Alias is still being processed, tag counts have not been updated yet
+      # 2. Alias is stuck, tag counts will never be updated
+      @is_aliased_away = TagAlias.where(antecedent_name: @correction.tag.name, status: %w[active processing queued]).exists?
+      if @is_aliased_away && @true_count != 0
+        @true_post_ids = @true_count > 20 ? "> 20" : Post.tag_match("#{@tag.name} status:any", resolve_aliases: false).pluck(:id).join(", ")
+      end
+
       @destroyable = @true_count == 0 && @aliases == 0 && @implications == 0
     end
 
-    respond_with(@correction)
-  end
-
-  def show
-    @correction = TagCorrection.new(params[:tag_id])
     respond_with(@correction)
   end
 
