fa7b84ac42bc3425ad2b8ff1a80af8ecbbb657e9
Cinder <cinder@e621.net>
2026-03-26 11:10:56 -0700

    [UploadWhitelists] Minor fix to URL parsing

--- app/logical/downloads/file.rb
+++ app/logical/downloads/file.rb
@@ -3,7 +3,7 @@
 module Downloads
   class File
     include ActiveModel::Validations
-    class Error < Exception ; end
+    class Error < StandardError; end
 
     attr_reader :url
 
@@ -11,9 +11,7 @@ module Downloads
 
     def initialize(url)
       begin
-        unencoded = Addressable::URI.unencode(url)
-        escaped = Addressable::URI.escape(unencoded)
-        @url = Addressable::URI.parse(escaped)
+        @url = Addressable::URI.heuristic_parse(url).normalize
       rescue Addressable::URI::InvalidURIError
         @url = nil
       end
