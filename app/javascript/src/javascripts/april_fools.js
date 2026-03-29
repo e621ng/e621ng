7667750449f970bf3694e93ee7b00d1446394eff
J Mor <jmortiger@gmail.com>
2026-03-29 15:09:10 +0000

    Added paused overlay

--- app/javascript/src/javascripts/april_fools.js
+++ app/javascript/src/javascripts/april_fools.js
@@ -39,17 +39,48 @@ function rootInit () {
       grid-row: 2 / 3;
       grid-column: 3 / 4;
     }
+
+    #snake-overlay {
+      display: flex;
+      background-color: rgba(0, 0, 0, 0.5);
+      align-items: center;
+      justify-content: space-around;
+      position: absolute;
+      top: 0;
+      left: 0;
+    }
+    .overlay-button {
+      background-color: rgba(0, 0, 0, 0.5);
+      color: rgba(255, 255, 255, 1);
+      width: 3rem;
+      height: 3rem;
+      font-size: x-large;
+    }
   </style>`);
   const canvas = document.createElement("canvas");
   canvas.id = "snake-game";
-  // container.prepend(canvas); // document.body.prepend(canvas);
   canvas.width = 300;
   canvas.height = 300;
-  var ctx = canvas.getContext("2d");
-  if (!ctx) {
-    throw Error("Failed to retrieve canvas context.");
-  }
-  var state = EngineConfig.toUI(EngineConfig.defaults, initialize);
+  const ctx = canvas.getContext("2d");
+  if (!ctx) throw Error("Failed to retrieve canvas context.");
+  const state = EngineConfig.toUI(EngineConfig.defaults, initialize);
+  const playButton = html`<button id=snake-play class=overlay-button>▶</button>`;
+  const overlay = html`
+  <span id=snake-overlay>
+    ${playButton}
+  </span>
+  `;
+  // Update overlay on canvas change
+  (new ResizeObserver((entries, _observer) => {
+    const width = canvas.clientWidth || (entries && entries[0] && ((entries[0].borderBoxSize && entries[0].borderBoxSize[0]?.inlineSize) || entries[0].contentRect?.width)) || undefined;
+    const height = canvas.clientHeight || (entries && entries[0] && ((entries[0].borderBoxSize && entries[0].borderBoxSize[0]?.blockSize) || entries[0].contentRect?.height)) || undefined;
+    const left = canvas.offsetLeft || (entries && entries[0] && entries[0].contentRect?.left) || undefined;
+    const top = canvas.offsetTop || (entries && entries[0] && entries[0].contentRect?.top) || undefined;
+    if (width) overlay.style.width = `${width}px`;
+    if (height) overlay.style.height = `${height}px`;
+    if (left) overlay.style.left = `${left}px`;
+    if (top) overlay.style.top = `${top}px`;
+  })).observe(canvas);
   const container = html`
   <div id=snake-pane>
     ${(() => {
@@ -59,13 +90,16 @@ function rootInit () {
   })()}
     <div id=snake-container>
       ${canvas}
+      ${overlay}
       ${touchControls}
       ${state.form}
     </div>
   </div>
   `;
   document.body.appendChild(container);
-  var lastEngineStats;
+  overlay.style.top = `${canvas.offsetTop}px`;
+  overlay.style.left = `${canvas.offsetLeft}px`;
+  /** @type {HTMLElement} **/ let lastEngineStats;
   function initialize (cfg) {
     if (lastEngineStats)
       lastEngineStats.remove();
@@ -86,16 +120,37 @@ function rootInit () {
         makeOverlay: false,
       },
     );
+    const toggleOverlay = () => {
+        overlay.style.display = r.engine.isGamePaused ? "" : "none";
+      },
+      playClicked = () => {
+        r.engine.isGamePaused ? r.engine.resumeGame() : r.engine.pauseGame();
+      },
+      changeToReplayButton = () => {
+        playButton.innerText = "⟳";
+        playButton.onclick = () => {
+          state.form.requestSubmit();
+          playButton.innerText = "▶";
+        };
+      };
+    r.engine.onGameOver.add(toggleOverlay, changeToReplayButton);
+    r.engine.onGamePaused.add(toggleOverlay);
+    r.engine.onGameResumed.add(toggleOverlay);
+
     lastEngineStats = r.engine.renderStats();
     canvas.insertAdjacentElement("afterend", lastEngineStats);
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     r.initGame().then(() => {
-      document.onkeyup = (e) => {
-        if (e.key === " ") {
-          r.startGame();
-          document.onkeyup = null;
-        }
+      playButton.onclick = () => {
+        r.startGame();
+        playButton.onclick = playClicked;
       };
+      // document.onkeyup = (e) => {
+      //   if (e.key === " ") {
+      //     r.startGame();
+      //     document.onkeyup = null;
+      //   }
+      // };
       r.draw({ engine: r.engine });
     }).catch((error) => {
       console.error("Failed to load game assets:", error);
