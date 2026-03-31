04f88ecd06e974d228f58a8885073c2e9b1a5d3f
Cinder <cinder@e621.net>
2026-03-31 07:57:25 -0700

    [Posts] Fix an error caused by a very long source metatag value

--- app/logical/tag_query.rb
+++ app/logical/tag_query.rb
@@ -240,6 +240,10 @@ class TagQuery
   # rather than letting the search reach OpenSearch and return a cryptic 400 response.
   OPENSEARCH_MAX_CLAUSE_COUNT = 1024
 
+  # OpenSearch limits wildcard query automaton states to ~1000. A prefix wildcard `term*` generates
+  # roughly `len(term) + 1` states, so cap the prefix at 999 characters to stay within the limit.
+  MAX_SOURCE_WILDCARD_LENGTH = 999
+
   # Used for quickly profiling optimizations, tweaking desired behavior, etc. Should be removed
   # after reviews are completed.
   # * `COUNT_TAGS_WITH_SCAN_RECURSIVE` [`false`]: Use `TagQuery.scan_recursive` to increment
@@ -1402,7 +1406,7 @@ class TagQuery
       when "change", "-change", "~change" then add_to_query(type, :change_seq, ParseValue.range(g2))
 
       when "source", "-source", "~source"
-        add_to_query(type, :sources, g2, any_none_key: :source, wildcard: true) { "#{g2}*" }
+        add_to_query(type, :sources, g2, any_none_key: :source, wildcard: true) { "#{g2.first(MAX_SOURCE_WILDCARD_LENGTH)}*" }
 
       when "date", "-date", "~date" then add_to_query(type, :date, ParseValue.date_range(g2))
 
