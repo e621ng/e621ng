e44824e6bb2c0dbc9d2e2fbe293d63b0c2e0f784
Tarrgon <61888458+Tarrgon@users.noreply.github.com>
2026-03-31 11:27:54 -0400

    Type check post controller `params[:post]`

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -169,7 +169,7 @@ class PostsController < ApplicationController
   private
 
   def tag_query
-    params[:tags] || (params[:post] && params[:post][:tags])
+    params[:tags] || (params[:post] && params[:post].is_a?(Hash) && params[:post][:tags]) || ""
   end
 
   def respond_with_post_after_update(post)
