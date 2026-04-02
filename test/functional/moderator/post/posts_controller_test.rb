c63edabc554c7addc640c7a27464254e46996ce8
Cinder <cinder@e621.net>
2026-04-02 09:51:41 -0700

    Test

--- test/functional/moderator/post/posts_controller_test.rb
+++ test/functional/moderator/post/posts_controller_test.rb
@@ -49,6 +49,20 @@ module Moderator
             assert_equal("Post ##{@post.id} is already deleted", flash[:notice])
             assert(@post.reload.is_deleted?)
           end
+
+          should "return a 409 with a reason if the post is already deleted and the request is JSON" do
+            as(@user) do
+              @post.delete!("first delete")
+            end
+
+            assert_no_difference(-> { PostEvent.count }) do
+              post_auth delete_moderator_post_post_path(@post), @admin, params: { reason: "xxx", commit: "Delete", format: :json }
+            end
+
+            assert_response :conflict
+            assert_equal("Post ##{@post.id} is already deleted", response.parsed_body["reason"])
+            assert(@post.reload.is_deleted?)
+          end
         end
 
         context "undelete action" do
