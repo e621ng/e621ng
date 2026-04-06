67264ad0cbc753e67d2f18b18bbf60120bbca5fb
Cinder <cinder@e621.net>
2026-04-06 14:16:01 -0700

    Minor note

--- app/logical/user_name_validator.rb
+++ app/logical/user_name_validator.rb
@@ -7,6 +7,9 @@ class UserNameValidator < ActiveModel::EachValidator
     # Blank names should never be saved. This check ensures that on user creation and name change requests,
     # an error is shown immediately instead of only requiring a name change on the next screen.
     # Check if the presence validator already added an error for blank name to avoid duplicate error messages.
+    #
+    # Note that this only works if the presence validator is defined before this custom validator.
+    # If the order ever changes, this will break.
     return if rec.errors.added?(attr, :blank)
     if name.blank? # If the presence validator is not used, we still want to add an error for blank name.
       rec.errors.add(attr, "can't be blank")
