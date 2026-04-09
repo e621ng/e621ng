37f414c648a7bdeb96db0c2d5363d7668d7db1e5
Cinder <cinder@e621.net>
2026-04-09 14:02:49 -0700

    [Recommender] Boost similar posts higher in the recommendations
    Based on character, copyright, and species tags

--- app/logical/post_sets/recommended.rb
+++ app/logical/post_sets/recommended.rb
@@ -2,32 +2,13 @@
 
 module PostSets
   class Recommended < PostSets::Base
-    attr_reader :tag_array, :limit
+    attr_reader :limit
 
     def initialize(post, limit: 6)
       super()
       @original_post = post
       @limit = limit.to_i.clamp(1, 20)
-
-      tags = post.known_artist_tags.sort_by(&:name).first(10).map { |t| "~#{t.name}" }
-      if tags.empty?
-        @no_results = true
-        @tag_array = []
-        return
-      end
-
-      tags << "-id:#{post.id}"
-      tags << "-parent:#{post.id}"
-      tags << "-child:#{post.id}"
-      tags << "order:random"
-      tags << "rating:safe" if CurrentUser.safe_mode?
-      tags << "randseed:#{post.id}"
-
-      @tag_array = TagQuery.scan_search(tags.join(" "), error_on_depth_exceeded: true)
-    end
-
-    def tag_string
-      @tag_string ||= @tag_array.join(" ")
+      @no_results = post.known_artist_tags.empty?
     end
 
     def post_ids
@@ -35,7 +16,7 @@ module PostSets
     end
 
     def posts
-      @posts ||= @no_results ? [] : ::Post.tag_match(tag_string).limit(@limit).to_a
+      @posts ||= @no_results ? [] : RecommendedQueryBuilder.new(@original_post).search.limit(@limit).to_a
     end
   end
 end
