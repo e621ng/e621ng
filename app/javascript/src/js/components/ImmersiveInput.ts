df9ee8af9c000c1c84fd724daba9ff2ad7cf0f52
Cinder <cinder@e621.net>
2026-04-14 08:56:40 -0700

    Fix contentful state

--- app/javascript/src/js/components/ImmersiveInput.ts
+++ app/javascript/src/js/components/ImmersiveInput.ts
@@ -8,10 +8,18 @@ export default class ImmersiveInput {
     this.wrapper = input.parent();
     this.input
       .off("focus.immersive blur.immersive input.immersive")
-      .on("focus.immersive", () => this.wrapper.addClass("focused"))
+      .on("focus.immersive", () => {
+        this.wrapper.addClass("focused");
+        this.updateContentfulState();
+      })
       .on("blur.immersive", () => this.wrapper.removeClass("focused"))
-      .on("input.immersive", (event) => {
-        this.wrapper.toggleClass("contentful", event.target.value.length > 0);
-      });
+      .on("input.immersive", () => this.updateContentfulState());
+
+    // Account for auto-filled inputs on page load
+    this.updateContentfulState();
+  }
+
+  private updateContentfulState () {
+    this.wrapper.toggleClass("contentful", this.input.val()?.length > 0);
   }
 }
