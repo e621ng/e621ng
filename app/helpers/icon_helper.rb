4603206b0b6f634c203d870c0b9c31857a11121a
Cinder <cinder@e621.net>
2026-04-14 10:38:54 -0700

    We already have that icon, turns out

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -98,9 +98,6 @@ module IconHelper
     face_meh: %(<circle cx="12" cy="12" r="10"/><line x1="8" x2="16" y1="15" y2="15"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
     face_angry: %(<circle cx="12" cy="12" r="10"/><path d="M16 16s-1.5-2-4-2-4 2-4 2"/><path d="M7.5 8 10 9"/><path d="m14 9 2.5-1"/><path d="M9 10h.01"/><path d="M15 10h.01"/>),
     face_happy: %(<circle cx="12" cy="12" r="10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
-
-    # Wiki Pages
-    arrow_corner_down_right: %(<path d="m15 10 5 5-5 5"/><path d="M4 4v7a4 4 0 0 0 4 4h12"/>),
   }.freeze
 
   def svg_icon(name, *args)
