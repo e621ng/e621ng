3d525d972e4f6571a73ac4ef57cb263140f4ae56
Cinder <cinder@e621.net>
2026-04-14 10:33:03 -0700

    [Posts] Replace icon counter icons with Lucide ones
    Addresses a part of the project in #1809.

--- app/helpers/icon_helper.rb
+++ app/helpers/icon_helper.rb
@@ -96,8 +96,8 @@ module IconHelper
     thumbs_up: %(<path d="M15 5.88 14 10h5.83a2 2 0 0 1 1.92 2.56l-2.33 8A2 2 0 0 1 17.5 22H4a2 2 0 0 1-2-2v-8a2 2 0 0 1 2-2h2.76a2 2 0 0 0 1.79-1.11L12 2a3.13 3.13 0 0 1 3 3.88Z"/><path d="M7 10v12"/>),
     thumbs_down: %(<path d="M9 18.12 10 14H4.17a2 2 0 0 1-1.92-2.56l2.33-8A2 2 0 0 1 6.5 2H20a2 2 0 0 1 2 2v8a2 2 0 0 1-2 2h-2.76a2 2 0 0 0-1.79 1.11L12 22a3.13 3.13 0 0 1-3-3.88Z"/><path d="M17 14V2"/>),
     face_meh: %(<circle cx="12" cy="12" r="10"/><line x1="8" x2="16" y1="15" y2="15"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
-    face_angry: %(<circle cx="12" cy="12" r="10"/><path d="M16 16s-1.5-2-4-2-4 2-4 2"/><path d="M7.5 8 10 9"/><path d="m14 9 2.5-1"/><path d="M9 10h.01"/><path d="M15 10h.01"/>),
-    face_happy: %(<circle cx="12" cy="12" r="10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
+    face_smile: %(<circle cx="12" cy="12" r="10"/><path d="M8 14s1.5 2 4 2 4-2 4-2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
+    face_frown: %(<circle cx="12" cy="12" r="10"/><path d="M16 16s-1.5-2-4-2-4 2-4 2"/><line x1="9" x2="9.01" y1="9" y2="9"/><line x1="15" x2="15.01" y1="9" y2="9"/>),
   }.freeze
 
   def svg_icon(name, *args)
@@ -105,6 +105,7 @@ module IconHelper
     width = options[:width] || 24
     height = options[:height] || 24
     klass = options[:class]
+    id = options[:id]
     title = options[:title]
     stroke_width = options[:stroke_width] || 2
 
@@ -119,6 +120,7 @@ module IconHelper
       "stroke-linecap": "round",
       "stroke-linejoin": "round",
       "class": klass,
+      "id": id,
       "name": name.to_s,
     ) do
       concat tag.title(title) if title.present?
