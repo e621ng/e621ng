7cfa5b8393a92d65b1596ac1d4d89eefe31235b7
Catting <5874051+mm12@users.noreply.github.com>
2026-04-15 10:51:35 -0500

    [UI] Add bypasses for default fields in search forms
    fixes e621ng/e621ng#1830

--- app/helpers/form_search_helper.rb
+++ app/helpers/form_search_helper.rb
@@ -1,10 +1,14 @@
 # frozen_string_literal: true
 
 module FormSearchHelper
-  def form_search(path:, always_display: false, hideable: request.path.split("/")[2] != "search", method: :get, &)
-    # dedicated search routes like /comments/search should always show
+  DEFAULT_SEARCH_FIELDS = %i[id created_at updated_at].freeze # needed for filled_form_fields to know which fields to look for in the params
+
+  def form_search(path:, always_display: false, hideable: request.path.split("/")[2] != "search", method: :get, exclude_default_fields: %i[], &)
+    # NOTE: dedicated search routes like /comments/search should always show
+    exclude_default_fields = Array(exclude_default_fields).map(&:to_sym)
     search_params = params[:search] || {}
-    show_on_load = filled_form_fields(search_params, &).any? || always_display || !hideable
+
+    show_on_load = filled_form_fields(search_params, exclude_default_fields: exclude_default_fields, &).any? || always_display || !hideable
     form = simple_form_for(:search, {
       method: method,
       url: path,
@@ -13,10 +17,13 @@ module FormSearchHelper
       defaults: { required: false },
       html: { class: "inline-form" },
     }) do |f|
-      id_input = f.input(:id, label: "ID", hide_unless_value: true)
-      created_at_input = f.input(:created_at, hide_unless_value: true)
-      updated_at_input = f.input(:updated_at, hide_unless_value: true)
-      id_input + created_at_input + updated_at_input + capture { yield(f) } + f.submit("Search")
+      inputs = []
+      inputs << f.input(:id, label: "ID", hide_unless_value: true) unless exclude_default_fields.include?(:id)
+      inputs << f.input(:created_at, hide_unless_value: true) unless exclude_default_fields.include?(:created_at)
+      inputs << f.input(:updated_at, hide_unless_value: true) unless exclude_default_fields.include?(:updated_at)
+      inputs << capture { yield(f) }
+      inputs << f.submit("Search")
+      safe_join(inputs)
     end
     render "application/form_search", hideable: hideable, show_on_load: show_on_load, form: form
   end
@@ -24,10 +31,10 @@ module FormSearchHelper
   # When the simple_form has f.input :name and search[name]=test [:name] will be returned
   # Some search params aren't exposed in the ui, but have links. In that case it
   # isn't expected to have the form be open, since no values are set.
-  def filled_form_fields(search_params, &)
+  def filled_form_fields(search_params, exclude_default_fields: %i[], &)
     form_field_collector = FormFieldCollector.new
     capture { yield(form_field_collector) }
-    available_fields = %i[id created_at updated_at] + form_field_collector.fields
+    available_fields = DEFAULT_SEARCH_FIELDS - Array(exclude_default_fields).map(&:to_sym) + form_field_collector.fields
     available_fields & search_params.keys.map(&:to_sym)
   end
 end
