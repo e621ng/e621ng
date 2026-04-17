269294283ed39a547ad0ad0affc07f13238b2c61
Cinder <cinder@e621.net>
2026-04-16 19:20:55 -0700

    [RelatedTags] Fix an unhandled CountExceededError

--- app/logical/related_tag_query.rb
+++ app/logical/related_tag_query.rb
@@ -4,7 +4,9 @@ class RelatedTagQuery
   attr_reader :query, :category_id
 
   def initialize(query: nil, category_id: nil)
-    @query = TagAlias.to_aliased(query.to_s.downcase.strip).join(" ")
+    tags = TagQuery.normalize(query).split
+    raise TagQuery::CountExceededError if tags.length > Danbooru.config.tag_query_limit
+    @query = tags.join(" ")
     @category_id = category_id
   end
 
--- test/functional/related_tags_controller_test.rb
+++ test/functional/related_tags_controller_test.rb
@@ -9,6 +9,12 @@ class RelatedTagsControllerTest < ActionDispatch::IntegrationTest
         get_auth related_tag_path, create(:user), params: { query: "touhou" }
         assert_response :success
       end
+
+      should "return 422 instead of 500 when too many tags are searched" do
+        query = (1..41).map { |i| "tag_#{i}" }.join(" ")
+        get_auth related_tag_path, create(:user), params: { search: { query: query, category_id: TagCategory::GENERAL } }
+        assert_response 422
+      end
     end
   end
 end
