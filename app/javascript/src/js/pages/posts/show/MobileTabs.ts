5c05fddb81c4f750e948d50e335fa2d5c3062f44
Cinder <cinder@e621.net>
2026-04-23 07:29:02 -0700

    Preserve tab state

--- app/javascript/src/js/pages/posts/show/MobileTabs.ts
+++ app/javascript/src/js/pages/posts/show/MobileTabs.ts
@@ -1,9 +1,15 @@
+import LStorage from "@/utility/storage";
+
 function bootstrapTabs () {
   const container = $(".post-display");
   const validActions = ["comments", "tags"];
+
+  container.attr("data-tab-state", LStorage.Posts.MobileTab as any);
   container.find(".post-mobile-tab").on("click", (event) => {
     const action = $(event.currentTarget).data("action");
     if (!validActions.includes(action)) return;
+    LStorage.Posts.MobileTab = action;
+
     container.attr("data-tab-state", action);
   });
 }
