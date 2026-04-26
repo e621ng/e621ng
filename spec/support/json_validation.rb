8134604ef6fef2c5fac3b00f51a07bc50455c3b8
J Mor <jmortiger@gmail.com>
2026-04-26 12:49:32 +0000

    Improved `match_json_format` w/ `DateTime` & root type check support

--- spec/support/json_validation.rb
+++ spec/support/json_validation.rb
@@ -2,21 +2,40 @@
 
 require "rspec/expectations"
 
-# Allows easily testing JSON output for conformity. Requires the following to either be passed in or
-# defined in the execution context via `let`(/`let!`?):
+# Allows easily testing JSON output for conformity.
+#
+# ### Parameters
 # * `json_format` {`Hash`}: All valid JSON keys mapped to their value's type
 # * `nullable_keys` {`Array`} [`[]`]: JSON keys that can be nil
+# * `:root_type` {`Symbol` | `Class`} [`nil`]: The expected type of the root object (`actual`).
+#    * `:array`/`Array`: Will fail if the given object isn't an `Array` of `Hash`es following the
+#    format defined by `json_format`.
+#    * `:object`/`:hash`/`Hash`: Will fail if the given object isn't a `Hash` following the format
+#    defined by `json_format`.
+#    * All other values will not enforce a strict type for the root object; it can be either a
+#    `Hash` following the format defined by `json_format`, or an `Array` of such.
 #
-# Manually passing in a value for the above will use that while ignoring any `let`-defined
-# alternatives. E.g. In cases like validating the `UserController` endpoints, where the results will
-# have added/missing keys depending on the endpoint & if the requested user is the logged-in user,
-# overriding the `let`-defined `json_format` with a `merge`d version manually passed in will work.
+# Requires `json_format` to either be passed in or defined in the execution context via
+# `let`/`let!`. `nullable_keys` can be defined the same way.
+#
+# Manually passing in a value for `json_format` & `nullable_keys` will use that while ignoring any
+# `let`-defined alternatives. E.g. In cases like validating the `UserController` endpoints, where
+# the results will have added/missing keys depending on the endpoint & if the requested user is the
+# logged-in user, overriding the `let`-defined `json_format` with a `merge`d version manually passed
+# in will work.
+#
+# Validated types:
+# * `String`
+# * `Numeric`
+# * `Integer`
+# * `IPAddr` (natively recognized by `parsed_body` somehow)
+# * `Date` (manually parsed from `String` given by `parsed_body`)
+# * `DateTime` (manually parsed from `String` given by `parsed_body`)
 #
 # TODO: * `optional_keys` {`Array`} [`[]`]: JSON keys that can be entirely omited from the response.
 # TODO: Add support for typed arrays in `json_format`
-# IDEA: Allow explictly defining whether the root must be an array or a hash?
 RSpec.shared_context "validating JSON" do
-  RSpec::Matchers.define :match_json_format do |json_format, nullable_keys|
+  RSpec::Matchers.define :match_json_format do |json_format = nil, nullable_keys = nil, root_type: nil|
     define_method :validate_instance do |obj|
       json_format ||= self.json_format
       nullable_keys ||= self.nullable_keys || []
@@ -24,21 +43,31 @@ RSpec.shared_context "validating JSON" do
       expect(obj.keys).to match_array(json_format.keys.map(&:to_s))
       obj.each_pair do |k, val|
         key_sym = k.to_sym
-        if json_format[key_sym] == Date
-          expect { val.is_a?(String) ? val.to_date : val }.not_to raise_error
-          expect(val.is_a?(String) ? val.to_date : val).to be_a(Date) | (be_a(NilClass) & satisfy { |_v| expect(nullable_keys).to include(key_sym) })
+        expected_type = json_format[key_sym]
+        if expected_type.in?([Date, DateTime])
+          expect { val = val.send(:"to_#{expected_type.name.downcase}") if val.is_a?(String) }.not_to raise_error
+          expect(val).to be_a(expected_type) | (be_a(NilClass) & satisfy { |_v| expect(nullable_keys).to include(key_sym) })
         elsif nullable_keys.include?(key_sym)
-          expect(val).to be_a(json_format[key_sym]) | be_a(NilClass)
+          expect(val).to be_a(expected_type) | be_a(NilClass)
         else
-          expect(val).to be_a(json_format[key_sym])
+          expect(val).to be_a(expected_type)
         end
       end
     end
     match(notify_expectation_failures: true) do |actual|
-      if actual.is_a?(Array)
+      case root_type
+      when Array, :array
+        expect(actual).to be_a(Array)
         actual.each { |e| validate_instance(e) }
-      else
+      when Hash, :hash, :object
+        expect(actual).to be_a(Hash)
         validate_instance(actual)
+      else
+        if actual.is_a?(Array)
+          actual.each { |e| validate_instance(e) }
+        else
+          validate_instance(actual)
+        end
       end
     end
   end
