2bd1612eea34b60c3f4fe6f9bdbda761077c6465
Catting <5874051+mm12@users.noreply.github.com>
2026-04-29 12:27:37 -0500

    prep merge

--- old_test/factories/ban.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory(:ban) do |f|
-    banner factory: :admin_user
-    sequence(:reason) { |n| "ban_reason_#{n}" }
-    duration { 60 }
-  end
-end
