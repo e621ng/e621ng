81cb1ec52084848eab5570067453fe241d326cb6
J Mor <jmortiger@gmail.com>
2026-04-29 17:17:41 +0000

    Added flash

--- app/javascript/src/js/pages/dmails/copy_links_to_clipboard.ts
+++ app/javascript/src/js/pages/dmails/copy_links_to_clipboard.ts
@@ -1,21 +1,23 @@
+import Flash from "@/utility/Flash";
+
 class DMails {
+  static url?: string;
+  static catchCopyLinkToClipboardError (error: any, url = this.url) {
+    console.warn("Failed! %o", error);
+    alert(`Failed to automatically copy sharable link.\n\nLink: ${url}`);
+  }
+
   static init () {
     const shareLink = document.querySelector<HTMLAnchorElement>(`a[href^=${document.location.pathname.replace(/\//g, "\\/")}\\?key]`);
     if (!shareLink) return;
-    const url = shareLink.href;
+    this.url = shareLink.href;
     shareLink.onclick = (e) => {
       e.preventDefault();
       e.stopImmediatePropagation();
       try {
-        navigator.clipboard.writeText(url).then(() => {
-          // TODO: Copied notification
-          console.log("Copied Successfully!");
-          alert("Copied Successfully!");
-        });
+        navigator.clipboard.writeText(this.url).then(() => Flash.notice("Copied to clipboard!")).catch(() => this.catchCopyLinkToClipboardError);
       } catch (error) {
-        console.warn("Failed! %o", error);
-        alert(`Failed to automatically copy sharable link.\n\nLink: ${url}`);
-        // window.location.href = url;
+        this.catchCopyLinkToClipboardError(error);
       }
     };
   }
