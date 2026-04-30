886e49e85ee401cf7cdbdd19ab10791eab1243bf
Catting <5874051+mm12@users.noreply.github.com>
2026-04-29 22:35:30 -0500

    prep merge

--- old_test/mailers/previews/maintenance/user/password_reset_mailer_preview.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class Maintenance::User::PasswordResetMailerPreview < ActionMailer::Preview # rubocop:disable Style/ClassAndModuleChildren
-  def confirmation
-    user = User.first
-    nonce = UserPasswordResetNonce.create(user_id: user.id)
-    Maintenance::User::PasswordResetMailer.reset_request(user, nonce)
-  end
-end
