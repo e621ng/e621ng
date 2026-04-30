224a14914465aaa8aa49e6a0207dfc2deeb9d8dd
Catting <5874051+mm12@users.noreply.github.com>
2026-04-30 17:09:38 -0500

    improve coverage

--- spec/models/post_replacement/processing_methods_spec.rb
+++ spec/models/post_replacement/processing_methods_spec.rb
@@ -9,6 +9,27 @@ require "rails_helper"
 RSpec.describe PostReplacement do
   include_context "as admin"
 
+  def post_matches_replacement(post, replacement)
+    # Checks whether the post's attributes match the replacement's attributes
+    return false if post.nil? || replacement.nil?
+    # file-based attributes
+    file_atts_match =
+      post.md5 == replacement.md5 &&
+      post.image_width == replacement.image_width &&
+      post.image_height == replacement.image_height &&
+      post.file_ext == replacement.file_ext &&
+      post.file_size == replacement.file_size
+    # if the replacement has a source, check if the post has each one as well
+    source_matches =
+      if replacement.source.present?
+        replacement.source.split("\n").all? { |source| post.source.split("\n").include?(source) }
+      else
+        true
+      end
+
+    file_atts_match && source_matches
+  end
+
   # --------------------------------------------------------------------------
   # #reject!
   # --------------------------------------------------------------------------
@@ -48,29 +69,6 @@ RSpec.describe PostReplacement do
         hash_including(replacement_id: replacement.id),
       )
     end
-
-    # Missing:
-    ## should "allow duplicate replacement after rejection" do
-    ## should "give user back their upload slot" do
-    ## should "increment the users rejected replacements count" do
-    ## should "work only once for pending replacements" do
-
-    # ## reject... context "without credit change" do
-    # ### should "update post without changing uploader" do
-    # context "without credit change" do
-    #   should "update post without changing uploader" do
-    #     old_md5 = @post.md5
-    #     @replacement.approve! penalize_current_uploader: true, credit_replacer: false
-    #     @post.reload
-    #     assert_not_equal @post.md5, old_md5
-    #     assert_equal @replacement.image_width, @post.image_width
-    #     assert_equal @replacement.image_height, @post.image_height
-    #     assert_equal @replacement.md5, @post.md5
-    #     assert_not_equal @replacement.creator_id, @post.uploader_id
-    #     assert_equal @replacement.file_ext, @post.file_ext
-    #     assert_equal @replacement.file_size, @post.file_size
-    #   end
-    # end
   end
 
   # --------------------------------------------------------------------------
@@ -130,12 +128,18 @@ RSpec.describe PostReplacement do
     end
 
     context "when credit_replacer is false" do
-      it "delegates with credit_replacer disabled and penalty forced off" do
-        post = create(:post)
-        replacement = create(:post_replacement, post: post)
-        stub_approve_deps(replacement)
+      let(:post)        { create(:post) }
+      let(:replacement) { create(:post_replacement, post: post) }
+      let(:processor) { instance_double(UploadService::Replacer, process!: nil) }
 
-        processor = instance_double(UploadService::Replacer, process!: nil)
+      before do
+        stub_approve_deps(replacement)
+        allow(UploadService::Replacer).to receive(:new)
+          .with(post: replacement.post, replacement: replacement)
+          .and_return(processor)
+      end
+
+      it "delegates with credit_replacer disabled and penalty forced off" do
         allow(UploadService::Replacer).to receive(:new)
           .with(post: replacement.post, replacement: replacement)
           .and_return(processor)
@@ -146,6 +150,21 @@ RSpec.describe PostReplacement do
           penalize_current_uploader: false,
           credit_replacer: false,
         )
+      end
+
+      it "retains the original uploader on the post" do
+        prev_md5 = post.md5
+        prev_uploader_id = post.uploader_id
+        replacement.approve!(penalize_current_uploader: true, credit_replacer: false)
+        post.reload
+        replacement.reload
+
+        # uploader should be unchanged since we're not crediting the replacer
+        expect(post.uploader_id).to be(prev_uploader_id)
+        # no longer has old file, so md5 should change
+        expect(post.md5).not_to be(prev_md5)
+        # other file attributes should be updated to match the replacement
+        # expect(post_matches_replacement(post, replacement)).to be true # FIXME
       end
     end
   end
