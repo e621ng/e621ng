7f95d844976e2e384d54fc9433b9ec6da7702c76
Catting <5874051+mm12@users.noreply.github.com>
2026-04-30 18:51:29 -0500

    remove stale code

--- app/controllers/post_replacements_controller.rb
+++ app/controllers/post_replacements_controller.rb
@@ -94,11 +94,6 @@ class PostReplacementsController < ApplicationController
       end
     end
 
-    if @post_replacement.errors.any?
-      render plain: "Replacement approval failed: #{@post_replacement.errors.full_messages.join('; ')}", status: 400
-      return
-    end
-
     respond_with(@post_replacement) do |format|
       format.html { render_partial_safely("post_replacements/partials/show/post_replacement", post_replacement: @post_replacement) }
       format.json
