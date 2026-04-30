c8938603995657a2b1f54242d2fa2013eaef3e49
Catting <5874051+mm12@users.noreply.github.com>
2026-04-29 21:22:45 -0500

    prep merge

--- old_test/factories/pool.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-FactoryBot.define do
-  factory(:pool) do
-    sequence(:name) { |n| "pool_#{n}" }
-    association :creator, factory: :user
-    sequence(:description) { |n| "pool_description_#{n}" }
-  end
-end
--- old_test/unit/zeitwerk_test.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-require "test_helper"
-
-class ZeitwerkTest < ActiveSupport::TestCase
-  should "eager load all files without errors" do
-    assert_nothing_raised { Rails.application.eager_load! }
-  end
-end
