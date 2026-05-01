ad6deb262af05745269a92cf14f939cc2cc3a3e9
Cinder <cinder@e621.net>
2026-05-01 12:33:54 -0700

    cleanup avatars when a post is destroyed

--- app/jobs/avatar_cleanup_job.rb
+++ app/jobs/avatar_cleanup_job.rb
@@ -3,12 +3,12 @@
 class AvatarCleanupJob < ApplicationJob
   queue_as :default
 
-  def perform(user_id)
+  def perform(user_id, force: false)
     user = User.find(user_id)
 
     # Don't perform cleanup if the user has a cropped avatar set.
     # Otherwise, we risk deleting the newly changed avatar if this job runs late.
-    return if user.avatar_id.present? && user.has_cropped_avatar?
+    return if !force && user.avatar_id.present? && user.has_cropped_avatar?
 
     sm = Danbooru.config.storage_manager
     sm.delete_avatar(user_id, "jpg")
