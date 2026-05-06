6d5cb505b845c7e76d50e655bf4bca61a9f8d2f7
Cinder <cinder@e621.net>
2026-05-06 16:04:11 -0700

    [PostSets] Rework PostSetSyncJob

---
+++ spec/jobs/bulk_index_update_job_spec.rb
@@ -0,0 +1,23 @@
+# frozen_string_literal: true
+
+require "rails_helper"
+
+RSpec.describe BulkIndexUpdateJob do
+  subject(:job) { described_class }
+
+  include_context "as member"
+
+  describe "#perform" do
+    it "re-indexes the specified records via import" do
+      posts = create_list(:post, 3)
+      ids   = posts.map(&:id)
+      expect(Post.document_store).to receive(:import).with(query: { id: ids })
+      job.perform_now("Post", ids)
+    end
+
+    it "does not raise when the id list is empty" do
+      allow(Post.document_store).to receive(:import)
+      expect { job.perform_now("Post", []) }.not_to raise_error
+    end
+  end
+end
