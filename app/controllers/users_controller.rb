22259cda1bd8b86a3239d077250505c2ef59b309
Cinder <cinder@e621.net>
2026-05-14 09:30:39 -0700

    [Users] Fix an error thrown when anon users access the settings page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@
 class UsersController < ApplicationController
   respond_to :html, :json
   skip_before_action :api_check
-  before_action :logged_in_only, only: %i[edit upload_limit update]
+  before_action :logged_in_only, only: %i[edit settings upload_limit update]
   before_action :member_only, only: %i[custom_style avatar_menu]
   before_action :janitor_only, only: %i[toggle_uploads fix_counts]
   before_action :admin_only, only: %i[flush_favorites]
