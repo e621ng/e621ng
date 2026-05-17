77f67cbd05154e2430964f91a2a421fbfaf979e0
Cinder <cinder@e621.net>
2026-05-17 07:47:29 -0700

    [Discord] Rework the discord reports (#2040)

---
+++ app/controllers/admin/discord_reports_controller.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+module Admin
+  class DiscordReportsController < ApplicationController
+    before_action :admin_only
+
+    def index
+      @janitor_report = DiscordReport::JanitorStats.new.report(update_cache: false)
+      @moderator_report = DiscordReport::ModeratorStats.new.report(update_cache: false)
+      @aibur_report = DiscordReport::AiburStats.new.report(update_cache: false)
+    end
+  end
+end
