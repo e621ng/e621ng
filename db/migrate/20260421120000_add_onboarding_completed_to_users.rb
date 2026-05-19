3ce086654d9c5a86827a7d231299e8d63de7a621
PlutonusDev <plutonus.stream@gmail.com>
2026-05-20 03:33:42 +1000

    [DB] Remove onboarding-related migration

--- db/migrate/20260421120000_add_onboarding_completed_to_users.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddOnboardingCompletedToUsers < ActiveRecord::Migration[7.1]
-  def change
-    add_column :users, :onboarding_completed, :boolean, default: false, null: false
-  end
-end
