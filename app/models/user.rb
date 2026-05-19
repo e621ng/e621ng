b6168cb5da3458561bf867b19e6bed0aa61b75ad
Cinder <cinder@e621.net>
2026-05-19 12:41:27 -0700

    [Users] Include `has_cropped_avatar` in the API output (#2062)

--- app/models/user.rb
+++ app/models/user.rb
@@ -785,6 +785,7 @@ class User < ApplicationRecord
         post_upload_count post_update_count note_update_count
         is_banned can_approve_posts can_upload_free
         level_string avatar_id is_verified?
+        has_cropped_avatar?
       ]
 
       if id == CurrentUser.user.id
