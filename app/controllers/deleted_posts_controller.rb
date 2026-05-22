754edf4b88e86b93d81eae27355918c03e63da82
DRLa <273903890+DRLaDRLa@users.noreply.github.com>
2026-05-21 20:49:18 -0400

    CR changes

--- app/controllers/deleted_posts_controller.rb
+++ app/controllers/deleted_posts_controller.rb
@@ -8,7 +8,7 @@ class DeletedPostsController < ApplicationController
       @user = User.find(params[:user_id])
       @posts = Post.where(is_deleted: true)
       @posts = @posts.where(uploader_id: @user.id)
-      @posts = @posts.includes(:uploader).includes(:flags).where.not(post_flags: { id: nil }).order(Arel.sql("post_flags.created_at DESC")).paginate(params[:page])
+      @posts = @posts.includes(:uploader).includes(:flags).where.not(post_flags: { id: nil }).order(post_flags: { created_at: :desc }).paginate(params[:page])
     else
       post_flags = PostFlag.where(is_deletion: true, is_resolved: false).includes(post: %i[uploader flags]).order(id: :desc).paginate(params[:page])
       new_opts = { pagination_mode: :numbered, records_per_page: post_flags.records_per_page, total_count: post_flags.total_count, current_page: post_flags.current_page }
