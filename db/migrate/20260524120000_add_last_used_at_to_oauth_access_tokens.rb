e9c96dbf6a6fb83243325f8f9cefaa93d75f760b
clragon <clragon@clynamic.net>
2026-05-24 22:56:33 +0200

    [OAuth] Consolidate doorkeeper migrations into single file

--- db/migrate/20260524120000_add_last_used_at_to_oauth_access_tokens.rb
+++
@@ -1,7 +0,0 @@
-# frozen_string_literal: true
-
-class AddLastUsedAtToOauthAccessTokens < ActiveRecord::Migration[8.1]
-  def change
-    add_column :oauth_access_tokens, :last_used_at, :datetime
-  end
-end
--- db/migrate/20260524130000_add_app_metadata_to_oauth_applications.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class AddAppMetadataToOauthApplications < ActiveRecord::Migration[8.1]
-  def change
-    add_column :oauth_applications, :description, :text
-    add_column :oauth_applications, :homepage_url, :string
-    add_column :oauth_applications, :minimum_user_level, :integer, null: false, default: 0
-  end
-end
