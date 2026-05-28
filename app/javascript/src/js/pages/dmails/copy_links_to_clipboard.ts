d5c07f17965d22e16429de48888ac12eb55f8781
Cinder <cinder@e621.net>
2026-05-28 13:26:08 -0700

    Tweaks

--- app/javascript/src/js/pages/dmails/copy_links_to_clipboard.ts
+++ app/javascript/src/js/pages/dmails/copy_links_to_clipboard.ts
@@ -1,25 +1,30 @@
 import Toast from "@/utility/Toast";
 
 class DMails {
-  private static url?: string;
-  private static catchCopyLinkToClipboardError (error: any, url = this.url) {
-    console.warn("Failed! %o", error);
-    alert(`Failed to automatically copy sharable link.\n\nLink: ${url}`);
+  static init () {
+    if (typeof navigator.clipboard !== "object")
+      return; // Clipboard API not supported
+
+    const shareLink = $("#share-link") as JQuery<HTMLAnchorElement>;
+    if (!shareLink.length) return;
+    const url = shareLink.attr("href");
+
+    shareLink.on("click.copyLink", (event) => {
+      event.preventDefault();
+      event.stopImmediatePropagation();
+      try {
+        navigator.clipboard.writeText(url)
+          .then(() => Toast.notice("Copied to clipboard!"))
+          .catch(() => this.releaseCopyEvent());
+      } catch { this.releaseCopyEvent(); }
+    });
   }
 
-  static init () {
-    const shareLink = document.querySelector<HTMLAnchorElement>(`a[href^=${document.location.pathname.replace(/\//g, "\\/")}\\?key]`);
-    if (!shareLink) return;
-    this.url = shareLink.href;
-    shareLink.onclick = (e) => {
-      e.preventDefault();
-      e.stopImmediatePropagation();
-      try {
-        navigator.clipboard.writeText(this.url).then(() => Toast.notice("Copied to clipboard!")).catch(() => this.catchCopyLinkToClipboardError);
-      } catch (error) {
-        this.catchCopyLinkToClipboardError(error);
-      }
-    };
+  // Ungraceful fallback for browsers that do not support the Clipboard API
+  private static releaseCopyEvent () {
+    $("#share-link")
+      .off("click.copyLink")
+      .trigger("click");
   }
 };
 
