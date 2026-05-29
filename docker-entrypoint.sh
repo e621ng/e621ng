9e0489b6dbc1d1a2ad8f41ab03f00729a2ca0319
Cinder <cinder@e621.net>
2026-05-29 12:29:59 -0700

    [Build] Switch to npm (#1895)

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set -e
 
-yarn install --frozen-lockfile
+npm install --no-audit --no-fund --no-update-notifier
 rm -f .overmind.sock
 
 if [ -d "vendor/dtext" ] && [ "$LOCAL_DTEXT" = "true" ]; then
