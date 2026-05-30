a35a412d2203d67e8b2a5ad1ddd70fbe6d9d9c77
clragon <clragon@clynamic.net>
2026-05-30 23:12:09 +0200

    [DbExport] Remove personal favorite and vote exports

--- app/controllers/db_exports_controller.rb
+++ app/controllers/db_exports_controller.rb
@@ -2,7 +2,6 @@
 
 class DbExportsController < ApplicationController
   before_action :validate_exports_enabled
-  before_action :member_only, only: %i[favorites votes]
 
   def index
     @exports = DbExport.order(:name)
@@ -13,37 +12,7 @@ class DbExportsController < ApplicationController
     end
   end
 
-  def favorites
-    user = CurrentUser.user
-    stream_personal_csv(
-      "SELECT id, post_id, created_at FROM favorites WHERE user_id = #{user.id} ORDER BY id DESC",
-      "favorites-#{user.name}-#{Date.current}.csv",
-    )
-  end
-
-  def votes
-    user = CurrentUser.user
-    stream_personal_csv(
-      "SELECT id, post_id, score, created_at FROM post_votes WHERE user_id = #{user.id} ORDER BY id DESC",
-      "votes-#{user.name}-#{Date.current}.csv",
-    )
-  end
-
   private
-
-  def stream_personal_csv(query, filename)
-    headers["Content-Disposition"] = ActionDispatch::Http::ContentDisposition.format(disposition: "attachment", filename: filename)
-    headers["Content-Type"] = "text/csv"
-
-    conn = ActiveRecord::Base.connection.raw_connection
-    self.response_body = Enumerator.new do |yielder|
-      conn.copy_data("COPY (#{query}) TO STDOUT WITH CSV HEADER") do
-        while (row = conn.get_copy_data)
-          yielder << row
-        end
-      end
-    end
-  end
 
   def export_json(export)
     {
--- spec/requests/db_exports_controller_spec.rb
+++ spec/requests/db_exports_controller_spec.rb
@@ -3,10 +3,6 @@
 require "rails_helper"
 
 RSpec.describe DbExportsController do
-  include_context "as admin"
-
-  let(:member) { create(:user) }
-
   before do
     DbExport.create!(name: "posts", file_size: 2048)
     DbExport.create!(name: "tags", file_size: 512)
@@ -38,44 +34,6 @@ RSpec.describe DbExportsController do
       entry = body.find { |export| export["name"] == "posts" }
       expect(entry).to include("file_name" => "posts.csv.gz", "file_size" => 2048)
       expect(entry["url"]).to be_present
-    end
-  end
-
-  describe "GET /db_exports/favorites" do
-    it "rejects anonymous users" do
-      get favorites_db_exports_path
-      expect(response).to have_http_status(:found)
-    end
-
-    it "streams the favorites of a logged-in user as CSV" do
-      post_record = create(:post)
-      FavoriteManager.add!(user: member, post: post_record)
-
-      sign_in_as(member)
-      get favorites_db_exports_path
-
-      expect(response).to have_http_status(:ok)
-      expect(response.media_type).to eq("text/csv")
-      expect(response.body).to include(post_record.id.to_s)
-    end
-  end
-
-  describe "GET /db_exports/votes" do
-    it "rejects anonymous users" do
-      get votes_db_exports_path
-      expect(response).to have_http_status(:found)
-    end
-
-    it "streams the post votes of a logged-in user as CSV" do
-      post_record = create(:post)
-      create(:post_vote, user: member, post: post_record, score: 1)
-
-      sign_in_as(member)
-      get votes_db_exports_path
-
-      expect(response).to have_http_status(:ok)
-      expect(response.media_type).to eq("text/csv")
-      expect(response.body).to include(post_record.id.to_s)
     end
   end
 end
