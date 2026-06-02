69d3a7280b789a93f7785ec79d1685d246518d3f
Cinder <cinder@e621.net>
2026-06-02 08:25:12 -0700

    load webp images

--- app/logical/user_avatar_url_cache.rb
+++ app/logical/user_avatar_url_cache.rb
@@ -14,13 +14,16 @@ module UserAvatarUrlCache
     return nil if user.blank? || user.avatar_id.blank?
 
     if user.has_cropped_avatar?
-      "/data/avatars/#{user.id}.jpg?t=#{user.updated_at.to_i}"
+      [
+        "/data/avatars/#{user.id}.webp?t=#{user.updated_at.to_i}",
+        "/data/avatars/#{user.id}.jpg?t=#{user.updated_at.to_i}",
+      ]
     else
       Cache.fetch(key(user.id), expires_in: CACHE_TTL) do
         post = Post.find_by(id: user.avatar_id)
         # NOTE: For deleted posts, this will cache nil, which is what we want.
         # Deleted posts cannot be used as avatars, and this will prevent repeated lookups.
-        post && !post.is_deleted? ? post.preview_file_url : nil
+        post && !post.is_deleted? ? post.preview_file_url_pair : nil
       end
     end
   end
