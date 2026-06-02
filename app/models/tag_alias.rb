0d21fb37e3c4e7062ae9091e39be6d0ff91339f5
Cinder <cinder@e621.net>
2026-06-02 07:08:24 -0700

    [TagRelationships] Optimize database queries in alias and implication indexes

--- app/models/tag_alias.rb
+++ app/models/tag_alias.rb
@@ -42,6 +42,57 @@ class TagAlias < TagRelationship
   end
 
   module TransitiveChecks
+    extend ActiveSupport::Concern
+
+    class_methods do
+      def preload_transitives(records)
+        records = records.to_a
+        return if records.empty?
+
+        antecedent_names = records.map(&:antecedent_name).uniq
+        antecedent_names_set = antecedent_names.to_set
+
+        bulk_aliases = TagAlias.duplicate_relevant
+                               .where(consequent_name: antecedent_names)
+                               .group_by(&:consequent_name)
+
+        raw_implications = TagImplication.duplicate_relevant
+                                         .where(antecedent_name: antecedent_names)
+                                         .or(TagImplication.duplicate_relevant.where(consequent_name: antecedent_names))
+                                         .to_a
+
+        implications_by_name = Hash.new { |h, k| h[k] = [] }
+        raw_implications.each do |ti|
+          implications_by_name[ti.antecedent_name] << ti if antecedent_names_set.include?(ti.antecedent_name)
+          if ti.consequent_name != ti.antecedent_name && antecedent_names_set.include?(ti.consequent_name)
+            implications_by_name[ti.consequent_name] << ti
+          end
+        end
+
+        records.each do |record|
+          next if record.instance_variable_defined?(:@transitives)
+
+          name = record.antecedent_name
+          transitives = []
+
+          (bulk_aliases[name] || []).each do |ta|
+            transitives << [:alias, ta, ta.antecedent_name, ta.consequent_name, record.consequent_name]
+          end
+
+          (implications_by_name[name] || []).each do |ti|
+            if ti.antecedent_name == name
+              transitives << [:implication, ti, ti.antecedent_name, ti.consequent_name, record.consequent_name, ti.consequent_name]
+            else
+              transitives << [:implication, ti, ti.antecedent_name, ti.consequent_name, ti.antecedent_name, record.consequent_name]
+            end
+          end
+
+          record.instance_variable_set(:@transitives, transitives)
+          record.instance_variable_set(:@has_transitives, transitives.size > 0)
+        end
+      end
+    end
+
     def list_transitives
       return @transitives if @transitives
       @transitives = []
