38daa35aa1f88c2e75256f8d64ccf67b713fba8c
Cinder <cinder@e621.net>
2026-06-09 14:47:59 -0700

    minor fixes

--- app/javascript/src/js/pages/posts/show/MobileTabs.ts
+++ app/javascript/src/js/pages/posts/show/MobileTabs.ts
@@ -9,28 +9,31 @@ function bootstrapTabs () {
     const action = $(event.currentTarget).data("action");
     if (!validActions.includes(action)) return;
     CStorage.postMobileTabState = action;
-
-    container
-      .attr("data-tab-state", action)
-      .find(".post-mobile-tab").each((_, el) => {
-        const $el = $(el);
-        $el.attr("aria-selected", String($el.data("action") === action));
-      });
+    switchToTab(container, action);
   });
 
   // Comment anchor links will not work on mobile unless the user has the comments tab open.
   if (window.innerWidth > 800 || CStorage.postMobileTabState === "comments") return;
   const commentID = window.location.hash.match(/^#?comment-(\d+)/)?.[1];
   if (!commentID) return;
-  const comment = $("article[data-comment-id='" + commentID + "']");
+  const comment = $(`article[data-comment-id="${commentID}"]`);
   if (!comment.length) return;
 
-  container.attr("data-tab-state", "comments");
+  switchToTab(container, "comments");
   // If the tabs get switched early enough, the browser will scroll to the content on its own.
   window.setTimeout(() => {
     const offset = comment.offset()?.top ?? 0;
     if (offset) window.scrollTo({ top: offset, behavior: "smooth" });
   }, 100);
+}
+
+function switchToTab (container: JQuery<HTMLElement>, tab: "comments" | "tags") {
+  container
+    .attr("data-tab-state", tab)
+    .find(".post-mobile-tab").each((_, el) => {
+      const $el = $(el);
+      $el.attr("aria-selected", String($el.data("action") === tab));
+    });
 }
 
 function bootstrapGoUpButton () {
