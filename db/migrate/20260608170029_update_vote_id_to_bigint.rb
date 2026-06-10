8de56b61b53d92004ac51d190e0419bdf8586d0b
Cinder <cinder@e621.net>
2026-06-10 07:31:04 -0700

    [Misc] Fix a typo

--- db/migrate/20260608170029_update_vote_id_to_bigint.rb
+++ db/migrate/20260608170029_update_vote_id_to_bigint.rb
@@ -2,7 +2,7 @@
 
 class UpdateVoteIdToBigint < ActiveRecord::Migration[8.1]
   def up
-    PostVotes.without_timeout do
+    PostVote.without_timeout do
       change_column :post_votes, :id, :bigint
       change_column :comment_votes, :id, :bigint
       execute "ALTER SEQUENCE post_votes_id_seq AS bigint"
@@ -11,7 +11,7 @@ class UpdateVoteIdToBigint < ActiveRecord::Migration[8.1]
   end
 
   def down
-    PostVotes.without_timeout do
+    PostVote.without_timeout do
       execute "ALTER SEQUENCE post_votes_id_seq AS integer"
       execute "ALTER SEQUENCE comment_votes_id_seq AS integer"
       change_column :post_votes, :id, :integer
