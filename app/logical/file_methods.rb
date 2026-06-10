9960147dd41b6fedf1ffb2214b9ab79788bf2cd5
Cinder <cinder@e621.net>
2026-06-10 11:32:36 -0700

    minor tweaks

--- app/logical/file_methods.rb
+++ app/logical/file_methods.rb
@@ -123,12 +123,12 @@ module FileMethods
 
   def calculate_dimensions(file_path)
     if is_video?
-      stdout, _stderr, status = Open3.capture3(Danbooru.config.ffprobe_path, "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", file_path)
-      unless status == 0
-        Rails.logger.warn("[FFPROBE STDOUT] #{stdout.chomp!}")
-        raise "Could not retrieve video dimensions"
+      stdout, stderr, status = Open3.capture3(Danbooru.config.ffprobe_path, "-v", "error", "-select_streams", "v:0", "-show_entries", "stream=width,height", "-of", "csv=p=0:s=x", file_path)
+      unless status.success?
+        Rails.logger.warn("[FFPROBE STDERR] #{stderr.chomp}")
+        raise Upload::Error, "Could not retrieve video dimensions"
       end
-      stdout.chomp!.split("x").map(&:to_i)
+      stdout.chomp.split("x").map(&:to_i)
     elsif is_image?
       image = Vips::Image.new_from_file(file_path)
       [image.width, image.height]
