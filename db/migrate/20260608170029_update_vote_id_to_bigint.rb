232b3b00980d5e8d95f8fb3f1ee567485e0ab89b
Cinder <cinder@e621.net>
2026-06-10 07:29:03 -0700

    [DB] Fix timeout while running the vote migration

--- db/migrate/20260608170029_update_vote_id_to_bigint.rb
+++ db/migrate/20260608170029_update_vote_id_to_bigint.rb
@@ -2,16 +2,20 @@
 
 class UpdateVoteIdToBigint < ActiveRecord::Migration[8.1]
   def up
-    change_column :post_votes, :id, :bigint
-    change_column :comment_votes, :id, :bigint
-    execute "ALTER SEQUENCE post_votes_id_seq AS bigint"
-    execute "ALTER SEQUENCE comment_votes_id_seq AS bigint"
+    PostVotes.without_timeout do
+      change_column :post_votes, :id, :bigint
+      change_column :comment_votes, :id, :bigint
+      execute "ALTER SEQUENCE post_votes_id_seq AS bigint"
+      execute "ALTER SEQUENCE comment_votes_id_seq AS bigint"
+    end
   end
 
   def down
-    execute "ALTER SEQUENCE post_votes_id_seq AS integer"
-    execute "ALTER SEQUENCE comment_votes_id_seq AS integer"
-    change_column :post_votes, :id, :integer
-    change_column :comment_votes, :id, :integer
+    PostVotes.without_timeout do
+      execute "ALTER SEQUENCE post_votes_id_seq AS integer"
+      execute "ALTER SEQUENCE comment_votes_id_seq AS integer"
+      change_column :post_votes, :id, :integer
+      change_column :comment_votes, :id, :integer
+    end
   end
 end
