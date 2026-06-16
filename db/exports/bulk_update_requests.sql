106c1bfbdcd068899c5d233065918d9c09b5f2a9
Donovan_DMC <hewwo@yiff.rocks>
2026-06-16 08:08:51 -0500

    rewrite queries for single join

--- db/exports/bulk_update_requests.sql
+++ db/exports/bulk_update_requests.sql
@@ -13,12 +13,13 @@ SELECT
   COALESCE(votes.meh, 0)  AS meh_votes,
   COALESCE(votes.up, 0)   AS up_votes
 FROM bulk_update_requests
-LEFT JOIN LATERAL (
+LEFT JOIN (
   SELECT
-      COUNT(*) FILTER (WHERE forum_post_votes.score = -1) AS down,
-      COUNT(*) FILTER (WHERE forum_post_votes.score = 0)  AS meh,
-      COUNT(*) FILTER (WHERE forum_post_votes.score = 1)  AS up
+    forum_post_votes.forum_post_id,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = -1) AS down,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = 0)  AS meh,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = 1)  AS up
   FROM forum_post_votes
-  WHERE forum_post_votes.forum_post_id = bulk_update_requests.forum_post_id
-) votes ON true
+  GROUP BY forum_post_votes.forum_post_id
+) votes ON votes.forum_post_id = bulk_update_requests.forum_post_id
 ORDER BY bulk_update_requests.id
--- db/exports/tag_implications.sql
+++ db/exports/tag_implications.sql
@@ -14,12 +14,13 @@ SELECT
   COALESCE(votes.meh, 0)  AS meh_votes,
   COALESCE(votes.up, 0)   AS up_votes
 FROM tag_implications
-LEFT JOIN LATERAL (
+LEFT JOIN (
   SELECT
-      COUNT(*) FILTER (WHERE forum_post_votes.score = -1) AS down,
-      COUNT(*) FILTER (WHERE forum_post_votes.score = 0)  AS meh,
-      COUNT(*) FILTER (WHERE forum_post_votes.score = 1)  AS up
+    forum_post_votes.forum_post_id,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = -1) AS down,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = 0)  AS meh,
+    COUNT(*) FILTER (WHERE forum_post_votes.score = 1)  AS up
   FROM forum_post_votes
-  WHERE forum_post_votes.forum_post_id = tag_implications.forum_post_id
-) votes ON true
+  GROUP BY forum_post_votes.forum_post_id
+) votes ON votes.forum_post_id = tag_implications.forum_post_id
 ORDER BY tag_implications.id
