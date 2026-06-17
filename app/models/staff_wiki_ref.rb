4d3060b26da95ab0641e4c058b9a0bd8f1a42c05
Cinder <cinder@e621.net>
2026-06-17 14:58:44 -0700

    minor tweaks

--- app/models/staff_wiki_ref.rb
+++ app/models/staff_wiki_ref.rb
@@ -7,17 +7,4 @@ class StaffWikiRef < ApplicationRecord
   belongs_to :related, polymorphic: true
 
   validates :related_type, presence: true, inclusion: { in: ALLOWED_TYPES }
-
-  module ValidationMethods
-    def validate_related_exists
-      return if related_type.blank? || related_id.blank?
-      return unless ALLOWED_TYPES.include?(related_type)
-
-      unless related_type.constantize.exists?(related_id)
-        errors.add(:related_id, "must refer to an existing #{related_type}")
-      end
-    end
-  end
-
-  include ValidationMethods
 end
