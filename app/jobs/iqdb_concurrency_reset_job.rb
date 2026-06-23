49c59da23421aefad14e4c2929dc0017062ffe0e
Cinder <cinder@e621.net>
2026-06-23 08:53:31 -0700

    fix tests

--- app/jobs/iqdb_concurrency_reset_job.rb
+++ app/jobs/iqdb_concurrency_reset_job.rb
@@ -5,6 +5,7 @@ class IqdbConcurrencyResetJob < ApplicationJob
 
   def perform
     keys = Cache.redis.smembers("iqdb:concurrent:keys")
+    return if keys.blank?
     Cache.redis.del(*keys) if keys.any?
     Cache.redis.del("iqdb:concurrent:keys")
   end
