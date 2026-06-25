0f0ec8da931eb633f502243a830c7e04e3ba9d52
Cinder <cinder@e621.net>
2026-06-25 10:45:36 -0700

    fix quick blacklist not loading on all pages that includes it

--- app/javascript/entrypoints/v_post-favorites.ts
+++ app/javascript/entrypoints/v_post-favorites.ts
@@ -1,9 +1,8 @@
-// uploads # new
+// post-favorites
 
 import E621Type from "@/interfaces/E621";
 declare const E621: E621Type;
 
 import "@/pages/posts/BlacklistQuickEdit";
-import "@/pages/uploads/new/uploader";
 
-E621.Registry.register("v_uploads_new");
+E621.Registry.register("v_post-favorites");
