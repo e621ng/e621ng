3da02d2dbabb7acdf8b07931c7e58a0c05c34547
Cinder <cinder@e621.net>
2026-06-30 09:39:20 -0700

    wording tweaks

--- config/danbooru_default_config.rb
+++ config/danbooru_default_config.rb
@@ -882,7 +882,7 @@ You can see a list of your deleted posts \"here\":[/deleted_posts?user_id=%UPLOA
 
     def automated_tag_notices
       values = {
-        thumbnail: "This tag is added automatically when the content is smaller than 250 pixels in width or height.",
+        thumbnail: "This tag is added automatically when the content is smaller than 250 pixels in width and height.",
         low_res: "This tag is added automatically when the content has a resolution lower than 500x500 pixels.",
         hi_res: "This tag is added automatically when the content has a width over 1600 pixels or height over 1200 pixels.",
         absurd_res: "This tag is added automatically when the content has a width over 3200 pixels or height over 2400 pixels.",
@@ -890,14 +890,19 @@ You can see a list of your deleted posts \"here\":[/deleted_posts?user_id=%UPLOA
 
         wide_image: "This tag is added automatically when the content is wider than 1024 pixels and the aspect ratio is wider than 4:1.",
         tall_image: "This tag is added automatically when the content is taller than 1024 pixels and the aspect ratio is taller than 1:4.",
+        long_image: "This tag is added automatically when the content is larger than 1024 pixels and the aspect ratio is larger than 4:1.",
 
         huge_filesize: "This tag is added automatically when the content has a file size larger than 30 megabytes.",
         long_playtime: "This tag is added automatically when the content has a video longer than 30 seconds.",
         short_playtime: "This tag is added automatically when the content has a video shorter than 30 seconds.",
 
         video: "This tag is added automatically to posts containing videos.",
-        animated: "This tag is added automatically to posts containing animated images.",
         flash: "This tag is added automatically to posts containing Flash files.",
+
+        animated: "This tag is added automatically to posts containing animated images.",
+        animated_gif: "This tag is added automatically to posts containing animated GIFs.",
+        animated_png: "This tag is added automatically to posts containing animated PNGs.",
+        animated_webp: "This tag is added automatically to posts containing animated WebPs.",
       }.stringify_keys
 
       FileMethods::FILE_TYPE.each_value do |file_type|
