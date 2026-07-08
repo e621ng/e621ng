3e61faa41e17634f086c113c86cc452c8af3a61f
Cinder <cinder@e621.net>
2026-07-08 10:45:35 -0700

    preview in the edit form

---
+++ app/javascript/entrypoints/v_wiki-pages_new.ts
@@ -0,0 +1,8 @@
+// wiki_pages # new
+
+import E621Type from "@/interfaces/E621";
+declare const E621: E621Type;
+
+import "@/pages/wiki_pages/FeaturedPostsPreview";
+
+E621.Registry.register("v_wiki-pages_new");
