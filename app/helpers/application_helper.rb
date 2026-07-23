3b4b5397d9ea85e08ad09d420d04af92577aa19f
Cinder <cinder@e621.net>
2026-07-23 09:13:15 -0700

    Remove `upser-post-uploader` class
    It was not doing anything, and causing N+1 queries

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -132,7 +132,6 @@ module ApplicationHelper
 
     user_class = user.level_css_class
     user_class += " user-post-approver" if user.can_approve_posts?
-    user_class += " user-post-uploader" if user.upload_karma_free?
     user_class += " user-banned" if user.is_restricted?
     user_class += " with-style" if CurrentUser.user.style_usernames?
     html = link_to(user.pretty_name.presence || "<blank>", user_path(user), class: user_class, rel: "nofollow")
