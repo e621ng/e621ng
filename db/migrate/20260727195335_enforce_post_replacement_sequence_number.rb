f4bc55ed4b29d1ad3c12a3d50e8990628776137a
Cinder <cinder@e621.net>
2026-07-27 13:19:44 -0700

    Wrap the entire migration in `without_timeout`

--- db/migrate/20260727195335_enforce_post_replacement_sequence_number.rb
+++ db/migrate/20260727195335_enforce_post_replacement_sequence_number.rb
@@ -2,16 +2,16 @@
 
 class EnforcePostReplacementSequenceNumber < ActiveRecord::Migration[8.1]
   def change
-    change_column_null :post_replacements2, :sequence_number, false
-
     PostReplacement.without_timeout do
+      change_column_null :post_replacements2, :sequence_number, false
+
       add_index :post_replacements2, %i[post_id sequence_number],
                 unique: true,
                 name: :index_post_replacements2_on_post_id_and_sequence_number
-    end
 
-    add_check_constraint :post_replacements2,
-                         "(status = 'original') = (sequence_number = 0)",
-                         name: :post_replacements2_status_original_seq0
+      add_check_constraint :post_replacements2,
+                           "(status = 'original') = (sequence_number = 0)",
+                           name: :post_replacements2_status_original_seq0
+    end
   end
 end
