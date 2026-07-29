14fac9efd79cc11f1ea39edc65c1f61204f4c525
Cinder <cinder@e621.net>
2026-07-29 11:53:23 -0700

    [Users] Fix not being able to search for users with unlimited uploads

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -339,7 +339,7 @@ class UsersController < ApplicationController
   end
 
   def search_params
-    permitted_params = %i[name_matches about_me avatar_id level min_level max_level can_approve_posts order]
+    permitted_params = %i[name_matches about_me avatar_id level min_level max_level can_approve_posts can_upload_free order]
     permitted_params += %i[ip_addr email_matches] if CurrentUser.is_admin?
     permit_search_params permitted_params
   end
