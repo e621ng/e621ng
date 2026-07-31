a70e0a5b678a943898f40ee867d23f8dd14a3d3d
jmortiger <jmortiger@gmail.com>
2026-07-31 11:12:36 -0400

    Add TimestampSwitch and TimestampToggle to E621 interface
    Signed-off-by: jmortiger <jmortiger@gmail.com>

--- app/javascript/src/js/interfaces/E621.ts
+++ app/javascript/src/js/interfaces/E621.ts
@@ -1,6 +1,8 @@
 import Autocomplete from "@/components/autocomplete";
 import DTextFormatter from "@/components/DTextFormatter";
 import ThumbnailEngine from "@/components/ThumbnailEngine";
+import { TimestampSwitch } from "@/components/TimestampSwitch";
+import { TimestampToggle } from "@/components/TimestampToggle";
 import Blacklist from "@/core/blacklist";
 import DeferredPostLoader from "@/core/DeferredPostLoader";
 import Hotkeys from "@/core/hotkeys";
@@ -33,6 +35,8 @@ export default interface E621Type {
   DTextFormatter: typeof DTextFormatter;
   PostCache: typeof PostCache;
   ThumbnailEngine: typeof ThumbnailEngine;
+  TimestampSwitch: typeof TimestampSwitch;
+  TimestampToggle: typeof TimestampToggle;
 
   // compatibility aliases
   notice: typeof ToastManager.notice;
@@ -78,6 +82,8 @@ export function getE621Instance (): E621Type {
     DTextFormatter,
     PostCache,
     ThumbnailEngine,
+    TimestampToggle,
+    TimestampSwitch,
 
     // compatibility aliases
     // TODO: Remove after November 2026
