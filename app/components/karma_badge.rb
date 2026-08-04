eac83d4fc6514cf505a46ccef1dadf1a9b2a21ab
Cinder <cinder@e621.net>
2026-08-04 07:06:02 -0700

    Styling pass

--- app/components/karma_badge.rb
+++ app/components/karma_badge.rb
@@ -17,6 +17,20 @@ class KarmaBadge < ViewComponent::Base
 
   attr_reader :user
 
+  def karma_level
+    return "S" if user.upload_karma_level >= 10
+    user.upload_karma_level
+  end
+
+  def badge_title
+    if user.upload_karma_level >= 10
+      return "Upload Karma: #{user.upload_karma} / #{user.upload_karma_level * 100} (Level S)"
+    end
+
+    next_level = user.upload_karma_level + 1
+    "Upload Karma: #{user.upload_karma} / #{user.required_karma_for_level(next_level)} (Level #{karma_level})"
+  end
+
   def progress_degree
     return 0 if user.upload_karma_level >= 10
     (user.upload_karma_percent * 3.6).round
