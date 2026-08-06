e2026ae3e452adb0b9f8a1cbd03b7942bd3544bb
Cinder <cinder@e621.net>
2026-08-06 08:46:07 -0700

    [Takedowns] Fix being unable to remove posts

--- app/javascript/entrypoints/v_takedowns.ts
+++ app/javascript/entrypoints/v_takedowns.ts
@@ -4,6 +4,6 @@ import E621Type from "@/interfaces/E621";
 declare const E621: E621Type;
 
 import "@/pages/takedowns/takedown_editor";
-import "@/pages/takedowns/takedowns";
+import Takedown from "@/pages/takedowns/takedowns";
 
-E621.Registry.register("v_takedowns");
+E621.Registry.register("v_takedowns", { Takedown });
