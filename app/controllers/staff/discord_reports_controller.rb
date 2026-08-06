21ab24954ae8d41456692b8aa36f22955d1a6ae6
Cinder <cinder@e621.net>
2026-08-05 18:58:17 -0700

    [Misc] Fix discord reports time outs

--- app/controllers/staff/discord_reports_controller.rb
+++ app/controllers/staff/discord_reports_controller.rb
@@ -5,9 +5,11 @@ module Staff
     before_action :admin_only
 
     def index
-      @janitor_report = DiscordReport::JanitorStats.new.report(update_cache: false)
-      @moderator_report = DiscordReport::ModeratorStats.new.report(update_cache: false)
-      @aibur_report = DiscordReport::AiburStats.new.report(update_cache: false)
+      User.without_timeout do
+        @janitor_report = DiscordReport::JanitorStats.new.report(update_cache: false)
+        @moderator_report = DiscordReport::ModeratorStats.new.report(update_cache: false)
+        @aibur_report = DiscordReport::AiburStats.new.report(update_cache: false)
+      end
     end
   end
 end
