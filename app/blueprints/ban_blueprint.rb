9c6e9f88514c339bc9aaf460a8057c3f1237f87b
Cinder <cinder@e621.net>
2026-08-07 12:00:26 -0700

    [Bans] Fix ban flags leaking into JSON output

---
+++ app/blueprints/ban_blueprint.rb
@@ -0,0 +1,14 @@
+# frozen_string_literal: true
+
+class BanBlueprint < Blueprinter::Base
+  identifier :id
+
+  field :user_id
+  field :banner_id
+  field :reason
+  field :duration
+
+  field :expires_at
+  field :created_at
+  field :updated_at
+end
