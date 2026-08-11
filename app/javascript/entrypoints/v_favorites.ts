35d5475b8b03d623236bc5fec6400f7407521577
Cinder <cinder@e621.net>
2026-08-11 08:31:29 -0700

    Fix non-functional buttons on the favorites page

--- app/javascript/entrypoints/v_favorites.ts
+++ app/javascript/entrypoints/v_favorites.ts
@@ -5,7 +5,8 @@ declare const E621: E621Type;
 
 import "@/pages/posts/BlacklistQuickEdit";
 import "@/pages/posts/BlacklistQuickToggle";
-import "@/pages/posts/post_mode_menu";
 import "@/pages/posts/post_search";
+import "@/pages/posts/SearchControls";
+import "@/pages/posts/SearchFilters";
 
 E621.Registry.register("v_favorites");
