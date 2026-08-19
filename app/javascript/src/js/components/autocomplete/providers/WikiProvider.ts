c3f70a35109ff586bb7e3add2d63abf6abd3760b
Cinder <cinder@e621.net>
2026-08-19 12:08:41 -0700

    [Autocomplete] Fix wiki lookup timeouts

--- app/javascript/src/js/components/autocomplete/providers/WikiProvider.ts
+++ app/javascript/src/js/components/autocomplete/providers/WikiProvider.ts
@@ -18,8 +18,11 @@ export default class WikiProvider extends Provider<WikiItem> {
   }
 
   public static async findWikis (term: string): Promise<WikiItem[]> {
+    term = term?.trim() || "";
+    if (!term) return [];
+
     const params = new URLSearchParams({
-      "search[title]": term + "*",
+      "search[title]": term + (term.endsWith("*") ? "" : "*"),
       "search[hide_deleted]": "Yes",
       "search[order]": "post_count",
       "limit": "10",
