4a806180e1b132a2a015ad61bab908df1b60f9a9
Donovan_DMC <hewwo@yiff.rocks>
2026-08-23 17:56:30 -0500

    [PostEvents] Add search & extra data in json

--- app/blueprints/post_event_blueprint.rb
+++ app/blueprints/post_event_blueprint.rb
@@ -3,7 +3,7 @@
 class PostEventBlueprint < Blueprinter::Base
   identifier :id
 
-  fields :post_id, :action, :created_at
+  fields :post_id, :action, :created_at, :extra_data
 
   field :creator_id do |post_event|
     post_event.is_creator_visible?(CurrentUser.user) ? post_event.creator_id : nil
