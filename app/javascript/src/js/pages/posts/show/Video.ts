6a1e08b28071fe47d3a76d5ff98f28c001ae227e
yivarld <yivarld71@gmail.com>
2026-09-15 18:43:59 +0200

    add volume storing in localstorage back

--- app/javascript/src/js/pages/posts/show/Video.ts
+++ app/javascript/src/js/pages/posts/show/Video.ts
@@ -1,15 +1,39 @@
-(async () => {
-	if ($(".video-player").length) {
-		// player must be loaded first
-		await import("@videojs/html/video/player");
+import { VideoPlayerElement } from "@videojs/html/video";
 
-		await Promise.all([
-			import("@videojs/html/icons/element"),
-			import("@videojs/html/ui/container"),
-			import("@videojs/html/ui/controls"),
-			import("@videojs/html/ui/gesture"),
-			import("@videojs/html/ui/play-button"),
-			import("@videojs/html/ui/time"),
-		]);
-	}
+async function importVideoJS() {
+  // player must be loaded first
+  await import("@videojs/html/video/player");
+
+  await Promise.all([
+    import("@videojs/html/icons/element"),
+    import("@videojs/html/ui/container"),
+    import("@videojs/html/ui/controls"),
+    import("@videojs/html/ui/gesture"),
+    import("@videojs/html/ui/play-button"),
+    import("@videojs/html/ui/time"),
+    import("@videojs/html/ui/volume-popover"),
+    import("@videojs/html/ui/mute-button"),
+    import("@videojs/html/ui/time-slider")
+  ]);
+}
+
+class VideoPlayer {
+  private videoElement: HTMLVideoElement;
+
+  public constructor(private container: VideoPlayerElement) {
+    this.videoElement = container.querySelector("video");
+    this.videoElement.volume = parseFloat(localStorage.getItem("video_volume") || "1.0")
+    this.videoElement.addEventListener("volumechange", () => {
+			 localStorage.setItem('video_volume', this.videoElement.volume.toString());
+    })
+  }
+}
+
+(async () => {
+  const videoPlayerContainer = $<VideoPlayerElement>(".video-player");
+	if (!videoPlayerContainer.length) return;
+
+  await importVideoJS();
+
+  new VideoPlayer(videoPlayerContainer[0]);
 })();
