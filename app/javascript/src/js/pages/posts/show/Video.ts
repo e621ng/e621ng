a4213700f62f331f52bb9f10e3e6a30c919aaa8c
yivarld <yivarld71@gmail.com>
2026-09-15 17:39:38 +0200

    fix videojs player not attaching

--- app/javascript/src/js/pages/posts/show/Video.ts
+++ app/javascript/src/js/pages/posts/show/Video.ts
@@ -1,13 +1,15 @@
 (async () => {
-  if ($(".video-player").length) {
-    await Promise.all([
-      import("@videojs/html/video/player"),
-      import("@videojs/html/icons/element"),
-      import("@videojs/html/ui/container"),
-      import("@videojs/html/ui/controls"),
-      import("@videojs/html/ui/gesture"),
-      import("@videojs/html/ui/play-button"),
-      import("@videojs/html/ui/time"),
-    ]);
-  }
+	if ($(".video-player").length) {
+		// player must be loaded first
+		await import("@videojs/html/video/player");
+
+		await Promise.all([
+			import("@videojs/html/icons/element"),
+			import("@videojs/html/ui/container"),
+			import("@videojs/html/ui/controls"),
+			import("@videojs/html/ui/gesture"),
+			import("@videojs/html/ui/play-button"),
+			import("@videojs/html/ui/time"),
+		]);
+	}
 })();
