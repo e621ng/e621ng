dca5a6549edb3aa758e087be3d362a4950ba2bec
yivarld <yivarld71@gmail.com>
2026-09-16 18:07:58 +0200

    more progress: - change icons to use new ones from icon_helper.rb instead. (since videojs was missing some and others sucked) - fix previous volume value not being stored properly if the user muted it instead of sliding it to 0. - improve layout and add more missing behavior.

--- app/javascript/src/js/pages/posts/show/Video.ts
+++ app/javascript/src/js/pages/posts/show/Video.ts
@@ -1,11 +1,10 @@
-import { VideoPlayerElement } from "@videojs/html/video";
+import type { VideoPlayerElement } from "@videojs/html/video";
 
 async function importVideoJS() {
   // player must be loaded first
   await import("@videojs/html/video/player");
 
   await Promise.all([
-    import("@videojs/html/icons/element"),
     import("@videojs/html/ui/container"),
     import("@videojs/html/ui/controls"),
     import("@videojs/html/ui/gesture"),
@@ -13,25 +12,48 @@ async function importVideoJS() {
     import("@videojs/html/ui/time"),
     import("@videojs/html/ui/volume-popover"),
     import("@videojs/html/ui/mute-button"),
-    import("@videojs/html/ui/time-slider")
+    import("@videojs/html/ui/time-slider"),
+    import("@videojs/html/ui/fullscreen-button")
   ]);
 }
 
 class VideoPlayer {
   private videoElement: HTMLVideoElement;
+  private muteButton: HTMLElement;
 
   public constructor(private container: VideoPlayerElement) {
     this.videoElement = container.querySelector("video");
+    this.muteButton = container.querySelector(".mute-button");
+    this.videoElement.addEventListener("volumechange", this.volumeChange)
+    this.loadVolume()
+  }
+
+  private volumeChange = () => {
+    const value = this.videoElement.muted ? 0 : this.videoElement.volume;
+    this.muteButton.querySelectorAll(".icon").forEach(e => e.classList.remove("show"))
+    this.muteButton.querySelector(`.icon.${VideoPlayer.getVolumeIconFromValue(value)}`).classList.add("show")
+    this.storeVolume()
+  }
+
+  public storeVolume() {
+    // muted != volume set to 0. so if we find it being muted, just store it as 0
+    localStorage.setItem('video_volume', this.videoElement.muted ? "0" : this.videoElement.volume.toString());
+  }
+
+  public loadVolume() {
     this.videoElement.volume = parseFloat(localStorage.getItem("video_volume") || "1.0")
-    this.videoElement.addEventListener("volumechange", () => {
-			 localStorage.setItem('video_volume', this.videoElement.volume.toString());
-    })
+  }
+
+  public static getVolumeIconFromValue(value: number) {
+    if (value == 0) return "muted";
+    if (value <= .5) return "low"
+    return "high"
   }
 }
 
 (async () => {
   const videoPlayerContainer = $<VideoPlayerElement>(".video-player");
-	if (!videoPlayerContainer.length) return;
+  if (!videoPlayerContainer.length) return;
 
   await importVideoJS();
 
