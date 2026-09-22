7d010dcccf5d9618f030fbf48ea372fa2c6ebf98
Cinder <cinder@e621.net>
2026-09-22 09:38:08 -0700

    [Uploads] Tweak the "related tags" styles

--- app/logical/related_tag_calculator.rb
+++ app/logical/related_tag_calculator.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class RelatedTagCalculator
-  MAX_RESULTS = 25
+  MAX_RESULTS = 24
 
   def self.calculate_from_sample_to_array(tags, category_constraint = nil)
     convert_hash_to_array(calculate_from_sample(tags, Danbooru.config.post_sample_size, category_constraint))
